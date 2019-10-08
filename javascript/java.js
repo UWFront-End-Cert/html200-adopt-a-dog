82708ef540c196d11420cb0c15efae5ced65bf98
jocjoh <jocjoh@uw.edu>
2019-10-08 13:36:57 -0700

    add second function to javascript

--- javascript/java.js
+++ javascript/java.js
@@ -2,9 +2,9 @@ function dogInfo(name, breed, cost) {
   alert('More on this dog:\n' + name + '\n' + breed + '\n' + cost + ' to adopt');
 }
 
-let result = 0;
+let total = 0;
 
 function totalCost(x) {
-  let result = x;
-  alert('$' + result);
+  let newTotal = total += x;
+  alert('$' + newTotal);
 }
