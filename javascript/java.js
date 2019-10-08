c5028f1479ba8b3f5b9a9ff9577db47cdc80392e
jocjoh <jocjoh@uw.edu>
2019-10-07 22:15:21 -0700

    Start total cost function

--- javascript/java.js
+++ javascript/java.js
@@ -1,3 +1,10 @@
 function dogInfo(name, breed, cost) {
   alert('More on this dog:\n' + name + '\n' + breed + '\n' + cost + ' to adopt');
 }
+
+let result = 0;
+
+function totalCost(x) {
+  let result = x;
+  alert('$' + result);
+}
