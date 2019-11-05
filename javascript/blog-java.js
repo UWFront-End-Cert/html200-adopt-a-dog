8a9ad831b5ba158c2d828b77f44384f65bb468d2
jocjoh <jocjoh@uw.edu>
2019-11-05 13:41:05 -0800

    Complete first blog post with JS

--- javascript/blog-java.js
+++ javascript/blog-java.js
@@ -1,44 +1,38 @@
 /*h2 headline*/
 const blurb = document.createElement("h2");
-blurb.style.marginLeft = "15%";
-blurb.style.marginTop = "2%";
+blurb.className = "intro_blurb";
 blurb.textContent = "Adoptadog blog";
 document.body.appendChild(blurb);
 const footer = document.getElementById("blog-footer");
 document.body.insertBefore(blurb, footer);
 
-/*container
+/*container*/
 const container = document.createElement("div");
-document.body.appendChild(container);
 document.body.insertBefore(container, footer);
 
-*article*
+/*article*/
 const article = document.createElement("article");
-document.body.appendChild(article);
-document.body.insertBefore(article, footer);*/
+article.className = "blog_article";
+container.appendChild(article);
 
 /*first blog image*/
 const image = document.createElement("IMG");
-image.style.width = "20%";
-image.style.marginLeft = "15%";
-image.style.marginTop = "1%";
-image.style.marginBottom = "5%";
+image.className = "blog_photo";
 image.setAttribute("src", "images/blog-1.jpg");
-image.setAttribute("width", "15%");
-image.setAttribute("style.cssFloat", "left");
 image.setAttribute("alt", "Girl hugging her dog as they sit on a cliff overlooking the Grand Canyon.")
-document.body.appendChild(image);
-document.body.insertBefore(image, footer);
+article.appendChild(image);
 
 /*h3 subhead*/
 const header = document.createElement("h3");
-header.style.width = "20%";
 header.textContent = "Traveling With Your Dog";
-document.body.appendChild(header);
-document.body.insertBefore(header, footer);
+article.appendChild(header);
 
-/*paragraph text*/
-const para = document.createElement("p");
-para.textContent = "Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.";
-document.body.appendChild(para);
-document.body.insertBefore(para, footer);
+/*firt paragraph text*/
+const para1 = document.createElement("p");
+para1.textContent = "Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.";
+article.appendChild(para1);
+
+/*second paragraph text*/
+const para2 = document.createElement("p");
+para2.textContent = "Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.";
+article.appendChild(para2);
