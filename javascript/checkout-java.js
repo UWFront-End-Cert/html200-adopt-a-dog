88244239b175ef074e4f3c749e18197ff7e8f94f
jocjoh <jocjoh@uw.edu>
2019-11-05 23:03:33 -0800

    Try console logging the values of the form

--- javascript/checkout-java.js
+++ javascript/checkout-java.js
@@ -1,3 +1,11 @@
-function submitButton() {
+const formInput = [
+  document.getElementById("name"),
+  document.getElementById("email"),
+  document.getElementById("street")
+]
+
+document.querySelector("form.dogForm").addEventListener("submit", function (e) {
+  e.preventDefault();
   alert("Thank you. The form information has been received.");
-}
+  console.log(formInput.value)
+});
