a39c3dc20045b3f57435baa99a73bb8bd38ec5f8
jocjoh <jocjoh@uw.edu>
2019-11-06 16:28:17 -0800

    Add alt text to second and third blog photos

--- javascript/blog-java.js
+++ javascript/blog-java.js
@@ -41,12 +41,14 @@ article.appendChild(para2);
 const blogObjects = [
     {
         image: "images/blog-2.jpg",
+        alt: "A man holding four dogs on a leash in a city park",
         header: "How to Walk Multiple Dogs",
         para1: "Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.",
         para2: "Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam."
     },
     {
         image: "images/blog-3.jpg",
+        alt: "Girl holding a ball up for her large dog against a sunset background",
         header: "Teach Your Dog to Fetch!",
         para1: "Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.",
         para2: "Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam."
@@ -62,6 +64,7 @@ for (let i = 0; i < blogObjects.length; i += 1) {
 
     const image = document.createElement("IMG");
     image.setAttribute("src", object.image);
+    image.setAttribute("alt", object.alt);
     image.className = "blog_photo";
 
     const header = document.createElement("h3");
