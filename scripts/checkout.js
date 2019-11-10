d9a673bb05cba0fb33cc5aae04d78bdd782b09fb
Admin <admin@Admins-MacBook-Pro.local>
2019-11-10 09:13:00 -0800

    Add validation of text fields

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,23 +1,27 @@
 const form = document.getElementById("checkout-form");
-const name = document.getElementById("adopt-name");
-const email = document.getElementById("adopt-email");
-const street = document.getElementById("adopt-street");
-const city = document.getElementById("adopt-city");
-const state = document.getElementById("adopt-state");
-const first = document.getElementById("adopt-first-time");
-const notFirst = document.getElementById("adopt-first-time-no");
-const pickup = document.getElementById("adopt-pickup");
 const submit = document.getElementById("submit-checkout");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const street = document.getElementById("street");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const first = document.getElementById("first-time");
+const notFirst = document.getElementById("first-time-no");
+const pickup = document.getElementById("pickup");
 
 form.addEventListener("submit", function() {
+  if (name.value, email.value, street.value, city.value == "") {
+    alert("Please fill out all information.")
+  }
+  else {
   event.preventDefault();
-  alert("Thank you. The form information has been received.");
-  console.log(name.value);
-  console.log(email.value);
-  console.log(street.value);
-  console.log(city.value);
-  console.log(state.value);
-  console.log('First time? ' + first.checked);
-  console.log('Adopted before? ' + notFirst.checked);
-  console.log(pickup.value);
+  console.log("Name: " + name.value);
+  console.log("Email: " + email.value);
+  console.log("Street: " + street.value);
+  console.log("City: " + city.value);
+  console.log("State: " + state.value);
+  console.log("First time? " + first.checked);
+  console.log("Repeat customer? " + notFirst.checked);
+  console.log("Pickup location: " + pickup.value);
+  }
 });
