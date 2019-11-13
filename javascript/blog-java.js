94c9c2c8f594933f7d3bb6f6501d3cb26826c606
jocjoh <jocjoh@uw.edu>
2019-11-13 09:48:33 -0800

    media queries for alignment and text size on blog page

--- javascript/blog-java.js
+++ javascript/blog-java.js
@@ -25,16 +25,19 @@ article.appendChild(image);
 
 /*h3 subhead*/
 const header = document.createElement("h3");
+header.className = "blog_header";
 header.textContent = "Traveling With Your Dog";
 article.appendChild(header);
 
 /*firt paragraph text*/
 const para1 = document.createElement("p");
+para1.className = "blog_text";
 para1.textContent = "Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.";
 article.appendChild(para1);
 
 /*second paragraph text*/
 const para2 = document.createElement("p");
+para2.className = "blog_text";
 para2.textContent = "Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.";
 article.appendChild(para2);
 
@@ -70,12 +73,15 @@ for (let i = 0; i < blogObjects.length; i += 1) {
 
     const header = document.createElement("h3");
     header.textContent = object.header;
+    header.className = "blog_header";
 
     const para1 = document.createElement("p");
     para1.textContent = object.para1;
+    para1.className = "blog_text";
 
     const para2 = document.createElement("p");
     para2.textContent = object.para2;
+    para2.className = "blog_text";
 
     dogBlog.appendChild(image);
     dogBlog.appendChild(header);
