b7357424d488279a4cf08b3235a11a72dd7d9fa2
jocjoh <jocjoh@uw.edu>
2019-11-15 14:15:28 -0800

    Refactor form submission with jQuery

--- javascript/checkout-java.js
+++ javascript/checkout-java.js
@@ -1,24 +1,13 @@
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const street = document.getElementById("street");
-const city = document.getElementById("city");
-const state = document.getElementById("state");
-const zip = document.getElementById("zip");
-const first = document.getElementById("city");
-const notFirst = document.getElementById("notFirst");
-const pickup = document.getElementById("pickup")
-
-
-document.querySelector("form.dogForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+$('.dogForm').submit(function( event) {
+  event.preventDefault();
   alert("Thank you. The form information has been received.");
-  console.log(name.value);
-  console.log(email.value);
-  console.log(street.value);
-  console.log(city.value);
-  console.log(state.value);
-  console.log(zip.value);
-  console.log(first.value);
-  console.log(notFirst.value);
-  console.log(pickup.value);
+  console.log($('#name').val());
+  console.log($('#email').val());
+  console.log($('#street').val());
+  console.log($('#city').val());
+  console.log($('#state').val());
+  console.log($('#zip').val());
+  console.log($('#first').val());
+  console.log($('#notFirst').val());
+  console.log($('#pickup').val());
 });
