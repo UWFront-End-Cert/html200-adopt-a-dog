798189ecd6c018d2ffdab79b5fbdeb920f2853d1
jocjoh <jocjoh@uw.edu>
2019-11-15 17:07:26 -0800

    Attempt adopt button function with jQuery

--- javascript/java.js
+++ javascript/java.js
@@ -1,14 +1,17 @@
+/*Dog info alert*/
 function dogInfo(name, breed, cost) {
   alert('More on this dog:\n' + name + '\n' + breed + '\n' + cost + ' to adopt');
 }
 
+/*Adopt button cost alert*/
 let total = 0;
 
-function totalCost(x) {
+$('.adopt_button').click(function(x) {
   let newTotal = total += x;
   alert('$' + newTotal);
-}
+});
 
+/*Dog tile hover border on index and dogs pages*/
 $('.box-home').hover(
   function() {
     $( this ).toggleClass('box-border');
