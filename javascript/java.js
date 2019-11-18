b0a5190c32f670f9cfa60a74cfad45d11371edd1
jocjoh <jocjoh@uw.edu>
2019-11-18 09:28:23 -0800

    Update cart total with button clicks

--- javascript/java.js
+++ javascript/java.js
@@ -3,12 +3,13 @@ function dogInfo(name, breed, cost) {
   alert('More on this dog:\n' + name + '\n' + breed + '\n' + cost + ' to adopt');
 }
 
-/*Adopt button cost alert*/
+/*Adopt button cost alert and cart total*/
 let total = 0;
 
 function totalCost(x) {
   let newTotal = total += x;
   alert('$' + newTotal);
+  $("#cart_total").text('$' + Number(newTotal));
 }
 
 /*Dog tile hover border on index and dogs pages*/
@@ -23,3 +24,5 @@ $('.box').hover(
     $( this ).toggleClass('box-border');
 }
 );
+
+/*Cart total*/
