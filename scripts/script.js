4faa632668b8336ef0d60ee7ec877c7ac544bf6b
Admin <admin@Admins-MacBook-Pro.local>
2019-11-18 17:33:06 -0800

    Added text for checkout amount

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@ function dogInfo(name, breed, cost) {
   alert('About this dog:' + '\n' + name + '\n' + breed + '\n' + cost);
 }
 
+$('.dog-box').hover(function(e) {
+  $('img', this).toggleClass('dog-box-effect');
+});
+
 let total = 0;
 
 function adoptFees(x) {
   total += x;
-  alert('Your total: $' + total);
+  $('#total').text('$ ' + Number(total));
 }
-
-$('.dog-box').hover(function(e) {
-  $('img', this).toggleClass('dog-box-effect');
-})
