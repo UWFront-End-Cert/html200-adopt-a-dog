6f4f11a1aa0774e0c24c951361a8e267687f43c5
ahendryx <aubreyhendryx@gmail.com>
2019-12-29 17:59:10 -0800

    adding files while Lesson 4 is in progress

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+function showInfo() {
+  let a = ['Murphy', 'Poppy', 'Jack', 'Duffy', 'Lucas', 'Jake',
+  'Angus', 'Violet', 'Piper', 'Maximus', 'Luna', 'Stella'];
+  let b = ['Labrador', 'Beagle', 'Unknown'];
+  let c = ['$200', '$150'];
+
+  result = (a + b + c);
+
+  alert(result);
+}
