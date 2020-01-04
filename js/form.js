325fe959f5b6f1e8b205e476df70eb6dc0387d70
Keith McDade <kmcdade@uw.edu>
2020-01-03 22:59:43 -0800

    progress check for form js

--- js/form.js
+++ js/form.js
@@ -1,22 +1,20 @@
 //  Form Values  //
-const form = document.getElementById('checkout-form');
-
-const submitButton = document.getElementById('submit');
+const form = document.getElementById("checkout-form");
+const submitButton = document.getElementById("submit");
 
 let formInfo = [];
-let firstTimeAdopter = document.getElementById('');
 
 function submitForm() {
- event.preventDefault();
- alert('Thank you!  Your information has been received.');
- //way to cleanly show only value//
- formInfo.push(document.getElementById('name').value);
- formInfo.push(document.getElementById('address').value);
- formInfo.push(document.getElementById('city').value);
- formInfo.push(document.getElementById('state').value);
- formInfo.push(document.getElementById('zipcode').value);
- formInfo.push(document.getElementById('').value);
- formInfo.push(document.getElementById('pickup-location').value);
+  let formInfo = []; //testing purposes only//
+  event.preventDefault();
+  alert("Thank you!  Your information has been received.");
+  formInfo.push(document.getElementById("name").value);
+  formInfo.push(document.getElementById("address").value);
+  formInfo.push(document.getElementById("city").value);
+  formInfo.push(document.getElementById("state").value);
+  formInfo.push(document.getElementById("zipcode").value);
+  formInfo.push(document.getElementById("first-adopter").value);
+  formInfo.push(document.getElementById("pickup-location").value);
 
- console.log(formInfo);
+  console.log(formInfo);
 }
