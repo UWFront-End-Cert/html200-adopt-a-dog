c671cb28f6727fc8b1d6498ec27f094e15232c1d
Martha Tran <siutsz@uw.edu>
2020-01-30 09:46:29 -0800

    lesson 6 and 7

---
+++ checkout.js
@@ -0,0 +1,22 @@
+function checkOut() {
+  var nameBox = document.getElementById('name').value;
+  var emailBox = document.getElementById('email').value;
+  var streetBox = document.getElementById('street').value;
+  var cityBox = document.getElementById('city').value;
+  var e = document.getElementById("ddlState");
+  var strState = e.options[e.selectedIndex].text;
+
+  var rate_value = '';
+  if (document.getElementById('r1').checked) {
+  rate_value = document.getElementById('r1').value;
+}
+  if (document.getElementById('r2').checked) {
+    rate_value = document.getElementById('r2').value;
+}
+
+  var e = document.getElementById("ddlLocation");
+  var strLocation = e.options[e.selectedIndex].text;
+
+  alert('Thank you. The form information has been received');
+  console.log(nameBox + emailBox + cityBox + strState + rate_value + strLocation);
+}
