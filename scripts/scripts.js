4a60abbadfa4c943620eee77d3d0100aa4142691
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-02-07 10:03:07 -0500

    added event handlers to all html documents

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+//Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
+//You will need to pass in the params in the click handler of the dog’s name, breed and adoption fees.
+//Create click handlers on each dog’s button that will add to the total.
+//Alert this total.
+
+let x = myFunction();
+function myFunction () {
+  alert(//something);
+
+}
