43934844fb5047eaaa0b537880c5bb93d857ff26
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-02-11 15:40:05 -0500

    added JS functions and click event handlers for clicking on images of dogs and clicking adopt me buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,15 @@
-//Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
-//You will need to pass in the params in the click handler of the dog’s name, breed and adoption fees.
-//Create click handlers on each dog’s button that will add to the total.
-//Alert this total.
 
-let x = myFunction();
-function myFunction () {
-  alert(//something);
-
+function dogInfo(dogBio) {
+  alert(dogBio);
 }
+
+
+let total = 0;
+
+function myFee(fee) {
+  total += fee;
+  alert(fee);
+}
+
+
+//myFee only alerts for Murphy and Poppy, will not allow you to click the button again and add to the total
