a5f2c14a653549a5c30e957e0ade090a2d7c5916
Christopher Dela Pena <delapc@gmail.com>
2020-02-19 08:55:18 -0800

    Added updated files per assignment 4

---
+++ js/lesson4Assignment2.js
@@ -0,0 +1,18 @@
+function dogPhotoClick(dogName, dogBreed, adoptionFees) {
+
+  alert(dogName + ' is a(n) ' + dogBreed + '. Adoption cost is $' + adoptionFees + '.');
+
+}
+
+var total = 0;
+
+function dogButtonClick() {
+
+  total = total + 123.45;
+  alert('Your total is $' + total);
+}
+/*  let input1 = prompt('Enter a number');
+  let input2 = prompt('Enter another number');
+  let result = 0;
+
+  total += total*/
