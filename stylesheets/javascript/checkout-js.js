f18bf2a0c7566added46a254da0bf772af4c339d
elena <ejdon@uw.edu>
2020-02-22 17:35:43 -0800

    refactoring checkout javascript

--- stylesheets/javascript/checkout-js.js
+++ stylesheets/javascript/checkout-js.js
@@ -1,27 +1,12 @@
 function submitClick() {
-  var name = document.getElementById("name");
-  console.log(name.value);
-
-  var email = document.getElementById("email");
-  console.log(email.value);
-
-  var street = document.getElementById("street");
-  console.log(street.value);
-
-  var city = document.getElementById("city");
-  console.log(city.value);
-
-  var state = document.getElementById("state");
-  console.log(state.value);
-
-  var zip = document.getElementById("zip");
-  console.log(zip.value);
-
-  var firstTime = document.querySelector('input[name="firstTime"]:checked');
-  console.log(firstTime.value);
-
-  var locations = document.getElementById("locations");
-  console.log(locations.value);
+  console.log($('#name').val());
+  console.log($('#email').val());
+  console.log($('#street').val());
+  console.log($('#city').val());
+  console.log($('#state').val());
+  console.log($('#zip').val());
+  console.log($('input[name="firstTime"]:checked').val());
+  console.log($('#locations').val());
 
   alert("Thank you. The form information has been received");
 
