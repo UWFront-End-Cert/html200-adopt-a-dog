bbb116685a7ee014223e8829bdfa1ac1ff6e239c
cody <tcody2288@gmail.com>
2020-02-27 15:23:25 -0800

    Fixed layout issues and edited css grid containers

--- scripts/dogs.js
+++ scripts/dogs.js
@@ -5,7 +5,7 @@ $('.intro-img').hover(function(e)
   $('.intro-img').removeClass('dog-zoom');
 });
 
-$('.dog-one, .dog-two, .dog-three, .dog-four, .dog-five, .dog-six, .dog-seven, .dog-eight, .dog-nine, .dog-ten, .dog-eleven, .dog-twelve').hover(function(e)
+$('.dog-one, .dog-two, .dog-three, .dog-four, .dog-five, .dog-six, .dog-seven, .dog-eight, .dog-nine, .dog-ten, .dog-eleven, .dog-twelve, .dog-one-front, .dog-two-front, .dog-three-front, .dog-four-front, .dog-five-front, .dog-six-front').hover(function(e)
 {
     $('img', this).addClass('tile-border');
 }, function(e) {
