414dc3e00f570bc8ff7769074fa1d4d8121f8295
Tina Wu <tina710171@gmail.com>
2020-03-06 16:10:04 -0800

    add lesson 4 js

---
+++ dog.js
@@ -0,0 +1,11 @@
+function showInfo(name, cost) {
+  alert(name + ' is cost '+ cost);
+}
+
+
+let total = 0;
+
+function addTotal(cost) {
+  total += cost;
+  alert('Your new total is: $' + cost);
+}
