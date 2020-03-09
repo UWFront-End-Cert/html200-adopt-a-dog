ef36c965b8523068afbdfb9eec7cb02a3e9d3318
Heather Malloy <hmalloy@bluetooth.com>
2020-03-09 10:43:43 -0700

    created js file, updated HTML for click events, added click event to alert dog info

---
+++ script.js
@@ -0,0 +1,7 @@
+// dogInfo
+// addUp
+// #cart-total
+
+function dogInfo(name,breed,cost) {
+  alert('This is ' + name + ', they are a ' + breed + '. It is $' + cost + ' to bring them home with you!');
+}
