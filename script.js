5916acf7f78164d84d52164b3ba529037d0eb3af
Heather Malloy <hmalloy@bluetooth.com>
2020-03-09 11:58:15 -0700

    added js to make it work

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
 function dogInfo(name,breed,cost) {
   alert('This is ' + name + ', they are a ' + breed + '. It is $' + cost + ' to bring them home with you!');
 }
+
+let total = 0;
+
+function addUp(cost) {
+  total += cost;
+  alert(total);
+
+  // a little something extra = updates cart total in header, but only for the page you are on (for now)
+  document.getElementById("cart-total").innerHTML = total;
+};
