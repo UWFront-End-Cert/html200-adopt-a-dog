cb11545fe966aa5b66c53d50087f228f53306165
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-01 10:34:39 -0500

    fixed unresponsive images, continued to work on creating objects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ const containerOne = document.getElementById('travel');
 
 const imageOne = document.createElement('img');
 imageOne.src = 'images/blog-1.jpg';
-imageOne.classList.add('responsive-image');
+imageOne.classList.add('image-responsive');
 containerOne.appendChild(imageOne);
 
 const titleOne = document.createElement('h2');
@@ -32,13 +32,19 @@ containerOne.appendChild(blogOne);
 
 
 //make each post an object
-const containerTwo = {
-  titleTwo: 'How To Walk Multiple Dogs',
-  blogTwo: 'Fat boi doge wow such tempt lotsa pats fat boi he made many woofs, yapper length boy borkf floofs. Fluffer lotsa pats the neighborhood pupper ur givin me a spook, bork much ruin diet. Puggo very hand that feed shibe corgo fluffer, long woofer noodle horse, smol borking doggo with a long snoot for pats such treat. You are doin me a concern ur givin me a spook waggy wags noodle horse sub woofer, shoober clouds you are doing me the shock. Shooberino ur givin me a spook vvv you are doing me the shock boof many pats wrinkler maximum borkdrive, you are doing me a frighten long doggo I am bekom fat blep pupperino shibe. Porgo adorable doggo borkf tungg, doing me a frighten. Smol thicc pupperino clouds long bois fluffer tungg, the neighborhood pupper ruff blop snoot length boy vvv, doggo doge boofers smol borking doggo with a long snoot for pats sub woofer. Mlem blep heckin good boys and girls smol maximum borkdrive, heckin good boys and girls you are doing me the shock adorable doggo. you are doin me a concern long water shoob. Very good spot fluffer long doggo, vvv.',
-};
-
+const containerTwo = document.getElementById('walk');
 const imageTwo = document.createElement('img');
 imageTwo.src = 'images/blog-2.jpg';
+containerTwo.appendChild(imageTwo);
+
+const blogTwo = {
+  titleTwo: 'How To Walk Multiple Dogs',
+  contentTwo: 'Fat boi doge wow such tempt lotsa pats fat boi he made many woofs, yapper length boy borkf floofs. Fluffer lotsa pats the neighborhood pupper ur givin me a spook, bork much ruin diet. Puggo very hand that feed shibe corgo fluffer, long woofer noodle horse, smol borking doggo with a long snoot for pats such treat. You are doin me a concern ur givin me a spook waggy wags noodle horse sub woofer, shoober clouds you are doing me the shock. Shooberino ur givin me a spook vvv you are doing me the shock boof many pats wrinkler maximum borkdrive, you are doing me a frighten long doggo I am bekom fat blep pupperino shibe. Porgo adorable doggo borkf tungg, doing me a frighten. Smol thicc pupperino clouds long bois fluffer tungg, the neighborhood pupper ruff blop snoot length boy vvv, doggo doge boofers smol borking doggo with a long snoot for pats sub woofer. Mlem blep heckin good boys and girls smol maximum borkdrive, heckin good boys and girls you are doing me the shock adorable doggo. you are doin me a concern long water shoob. Very good spot fluffer long doggo, vvv.'
+};
+containerTwo.appendChild(blogTwo);
+
+
+
 
 // document.getElementById('walk');
 // const containerTwo = document.createElement(blogTwo);
