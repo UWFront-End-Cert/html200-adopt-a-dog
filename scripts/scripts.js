60ef4629dbd16d55173945bf9ea2297f5e62b414
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-07 10:22:13 -0500

    added for loop to dynamically generate and style blog posts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,27 +25,6 @@ function myFee(fee) {
 // let str = arr.join(' ');
 //   console.log(str);
 
-// const containerOne = document.getElementById('travel');
-//
-// const imageOne = document.createElement('img');
-// imageOne.src = 'images/blog-1.jpg';
-// imageOne.classList.add('image-responsive');
-// imageOne.alt = "Woman sits on edge of canyon with her arm around a black dog"
-// containerOne.appendChild(imageOne);
-//
-// const titleOne = document.createElement('h2');
-// titleOne.classList.add('blog-header');
-// titleOne.textContent = 'Traveling With Your Dog';
-// containerOne.appendChild(titleOne);
-//
-// const blogOne = document.createElement('p');
-// blogOne.textContent = 'Fat boi doge wow such tempt lotsa pats fat boi he made many woofs, yapper length boy borkf floofs. Fluffer lotsa pats the neighborhood pupper ur givin me a spook, bork much ruin diet. Puggo very hand that feed shibe corgo fluffer, long woofer noodle horse, smol borking doggo with a long snoot for pats such treat. You are doin me a concern ur givin me a spook waggy wags noodle horse sub woofer, shoober clouds you are doing me the shock. Shooberino ur givin me a spook vvv you are doing me the shock boof many pats wrinkler maximum borkdrive, you are doing me a frighten long doggo I am bekom fat blep pupperino shibe. Porgo adorable doggo borkf tungg, doing me a frighten. Smol thicc pupperino clouds long bois fluffer tungg, the neighborhood pupper ruff blop snoot length boy vvv, doggo doge boofers smol borking doggo with a long snoot for pats sub woofer. Mlem blep heckin good boys and girls smol maximum borkdrive, heckin good boys and girls you are doing me the shock adorable doggo. you are doin me a concern long water shoob. Very good spot fluffer long doggo, vvv.';
-// blogOne.classList.add('blog-text');
-// containerOne.appendChild(blogOne);
-
-
-// // leverage this to generate multiple blog posts
-
 
     const blogs = [
       {
@@ -62,7 +41,7 @@ function myFee(fee) {
     },
     {
         img: 'images/blog-3.jpg',
-        alt: 'ilhouetted woman holds ball up for sitting dog during sunset',
+        alt: 'Silhouetted woman holds ball up for sitting dog during sunset',
         h2: 'Teach Your Dog to Fetch!',
         p: 'Fat boi doge wow such tempt lotsa pats fat boi he made many woofs, yapper length boy borkf floofs. Fluffer lotsa pats the neighborhood pupper ur givin me a spook, bork much ruin diet. Puggo very hand that feed shibe corgo fluffer, long woofer noodle horse, smol borking doggo with a long snoot for pats such treat. You are doin me a concern ur givin me a spook waggy wags noodle horse sub woofer, shoober clouds you are doing me the shock. Shooberino ur givin me a spook vvv you are doing me the shock boof many pats wrinkler maximum borkdrive, you are doing me a frighten long doggo I am bekom fat blep pupperino shibe. Porgo adorable doggo borkf tungg, doing me a frighten. Smol thicc pupperino clouds long bois fluffer tungg, the neighborhood pupper ruff blop snoot length boy vvv, doggo doge boofers smol borking doggo with a long snoot for pats sub woofer. Mlem blep heckin good boys and girls smol maximum borkdrive, heckin good boys and girls you are doing me the shock adorable doggo. you are doin me a concern long water shoob. Very good spot fluffer long doggo, vvv.'
     }
@@ -72,22 +51,24 @@ function myFee(fee) {
   for (let i = 0; i < blogs.length; i += 1) {
     const field = blogs[i];
 
-    const containerOne = document.createElement('article');
+    const blogContent = document.createElement('article');
 
     const img = document.createElement('img');
-    img.src = 'images/blog-1.jpg';
-    containerOne.appendChild(img);
+    img.src = field.img;
+    blogContent.appendChild(img);
+    img.setAttribute('alt', field.alt);
+    img.classList.add('image-responsive');
+
 
     const h2 = document.createElement('h2');
     h2.textContent = field.h2;
-    containerOne.appendChild(h2);
+    blogContent.appendChild(h2);
+    h2.classList.add('blog-header');
 
     const text = document.createElement('p');
     text.textContent = field.p;
 
 
-    containerOne.appendChild(text);
-    document.getElementById('blog-content').appendChild(containerOne);
-
-
+    blogContent.appendChild(text);
+    document.getElementById('blog-content').appendChild(blogContent);
   }
