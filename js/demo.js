1a92ec2cdf0df67c1359ae3b00748e3f026a9a59
swati <swatidash2007@gmail.com>
2020-04-12 21:58:45 -0700

    created two javascript functions

---
+++ js/demo.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+ function square() {
+   let input = prompt('Please enter a  number');
+   let x = Number(input);
+   let result = x *  x ;
+
+   alert(result);
+ }
+
+
+function multiplier() {
+  let input1 = prompt("Please enter a number");
+  let x = Number(input1);
+  let input2 = prompt("Please enter another number");
+  let y = Number(input2);
+  let result = x * y;
+
+  alert(result);
+}
