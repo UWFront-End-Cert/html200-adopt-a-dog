5eef8362688d1d627fc56c9786ee0e7bcf2b53f5
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-13 17:15:16 -0500

    added radio button if statement

--- scripts/form.js
+++ scripts/form.js
@@ -5,32 +5,26 @@ submitButton.textContent = 'Submit';
 submitFinal.appendChild(submitButton);
 
 const form = document.getElementById('myForm');
-// const e =  form.elements;
-// for(let i = 0; i < e.length; i++) {
-//   //ACCESS NODE LIST LIKE ARRAY
-//   console.log(e[i]);
-// }
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   alert("Thank you. The form information has been received.");
 })
 
-//radio button console log
-//(x.elements[i].name + ' : ' + x.elements[i].value)
-//if(x.elements[i].type == 'radio') {
-//   if(x.elements[i].checked == false) {
-//     continue;
-//   }
-// }
+
 
 function myAdoption() {
   const x = document.getElementById('myForm');
   let text = ' ';
-  for(let i = 0; i < x.length; i++) {
+  for (let i = 0; i < x.length; i++) {
+    if(x.elements[i].type == 'radio') {
+      if(x.elements[i].checked == false) {
+        continue;
+      }
+    }
     text += x.elements[i].name + ' : ' + x.elements[i].value + '\n';
-    console.log(text);
   }
-
+  console.log(text);
 }
+
 
 // return kicks you out of the fucntion, break kicks you out of the loop, continue skips the rest of the code
