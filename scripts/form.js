30ca9ad50493f76c97630d87551dbf50a583569d
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-13 11:41:03 -0500

    added beginning code to console log radio button, tried different approach to console log values from form

--- scripts/form.js
+++ scripts/form.js
@@ -14,3 +14,15 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
   alert("Thank you. The form information has been received.");
 })
+
+//radio button console log
+//console.log($('.radio:selected').val());
+
+function myAdoption() {
+  const x = document.getElementById('myForm');
+  const text = '';
+  for(i = 0; i < x.length; i++) {
+    text += x.elements[i].value + '<br>';
+  }
+  console.log(document.getElementById('name').value);
+}
