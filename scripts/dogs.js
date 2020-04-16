2851c8b4fb0564dc538009fd935644c9e63a0cea
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-16 10:26:10 -0500

    created separate JS files for each page, added hover effect to dog tile images

---
+++ scripts/dogs.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+$('.card').hover(function() {
+    $(this).css('border', 'solid #81b741 2px');
+  }, function() {
+    $(this).css('border', 'dashed #81b741 2px');
+  });
+});
+
+//
+// function dogInfo(dogBio) {
+//   alert(dogBio);
+// }
+//
+// let total = 0;
+//
+// function myFee(fee) {
+//   total += fee;
+//   alert(total);
+// }
--- scripts/home.js
+++ scripts/home.js
@@ -1,11 +1,19 @@
+//
+// function dogInfo(dogBio) {
+//   alert(dogBio);
+// }
+//
+// let total = 0;
+//
+// function myFee(fee) {
+//   total += fee;
+//   alert(total);
+// }
 
-function dogInfo(dogBio) {
-  alert(dogBio);
-}
-
-let total = 0;
-
-function myFee(fee) {
-  total += fee;
-  alert(total);
-}
+$(document).ready(function() {
+$('.card').hover(function() {
+    $(this).css('border', 'solid #81b741 2px');
+  }, function() {
+    $(this).css('border', 'dashed #81b741 2px');
+  });
+});
