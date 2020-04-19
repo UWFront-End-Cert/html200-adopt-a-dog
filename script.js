0a803eec42dd8fa470d1605842bba4c7b0a95b15
Jak Ashuraliev <ashuraliev@gmail.com>
2020-04-18 23:29:40 -0700

    Fix bug on form submission

--- script.js
+++ script.js
@@ -6,39 +6,35 @@
 //creates an empry array
 let formData = [];
 
-function validate() {
+//add EventListener to the form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-  //add EventListener to the form
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  let formValues = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    address: document.getElementById('address').value,
+    city: document.getElementById('city').value,
+    state: document.getElementById('state').value,
+    zipcode: document.getElementById('zipcode').value,
+    firstAdoptY: document.getElementById('yes').value,
+    location: document.getElementById('location').value,
+    cost: document.getElementById('total-cost').textContent
+  }
 
-    let formValues = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      address: document.getElementById('address').value,
-      city: document.getElementById('city').value,
-      state: document.getElementById('state').value,
-      zipcode: document.getElementById('zipcode').value,
-      firstAdoptY: document.getElementById('yes').value,
-      location: document.getElementById('location').value,
-      cost: document.getElementById('total-cost').textContent
-    }
+  //add formValues to formData an array object
+  formData.push(formValues);
 
-    //add formValues to formData an array object
-    formData.push(formValues);
+  //clear the form after submission
+  document.forms[0].reset();
 
-    //clear the form after submission
-    document.forms[0].reset();
+  //alert user
+  alert('Thank you. The form information has been received');
 
-    //alert user
-    alert('Thank you. The form information has been received');
+  //log formData
+  console.log(formData);
 
-    //log formData
-    console.log(formData);
+  //log formData in JSON format;
+  // console.log(JSON.stringify(formData));
 
-    //log formData in JSON format;
-    // console.log(JSON.stringify(formData));
-
-  })
-
-}
+})
