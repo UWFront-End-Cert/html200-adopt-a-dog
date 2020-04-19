a0293841440b241e5c21696090d75b5511fa3bff
Dane Wallace <danewallace@icloud.com>
2020-04-19 15:45:59 -0700

    Initial js content to populate blog articles programmatically.

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,42 @@
 let feeTotal = 0;
 
+// Alert when clicking on dog picture
 function alertDogInfo(name, breed, adoptionFee) {
   let txt =
     'Name: ' + name + '\nBreed: ' + breed + '\nAdoption fee: ' + adoptionFee;
   alert(txt);
 }
 
+// Alert when clicking adopt button
 function addToTotal(adoptionFee) {
   feeTotal = feeTotal + adoptionFee;
   alert('Adoption fees total: $' + feeTotal);
+}
+
+// Populate blog.html content
+function populateBlog() {
+  //create blog container section
+  const blogContainer = document.createElement('section');
+  blogContainer.setAttribute('class', 'main-blog-container');
+  document.body.insertBefore('blogContainer', 'footer');
+
+  //create article and elements
+  const blogArticle = document.createElement('article');
+  blogArticle.setAttribute('class', 'blog-entry');
+
+  const blogImage = document.createElement('image');
+  blogImage.setAttribute('class', 'blog-image');
+  blogImage.src = './images/blog-1.jpg';
+  blogArticle.appendChild(image);
+
+  const blogTitle = document.createElement('h2');
+  blogTitle.setAttribute('class', 'blog-title');
+  blogTitle.textContent = 'Test title';
+  blogArticle.appendChild(blogTitle);
+
+  const blogContent = document.createElement('p');
+  blogContent.setAttribute('class', 'blog-content');
+  blogContent.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+  blogArticle.appendChild(blogContent);
+
 }
