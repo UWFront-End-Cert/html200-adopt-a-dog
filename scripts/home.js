65fc9ce294557b205046f0bfb0a1f33c3aa8bd73
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-20 10:56:44 -0500

    minor CSS adjustments, working to apply jQuery this

--- scripts/home.js
+++ scripts/home.js
@@ -2,18 +2,44 @@ function dogInfo(dogBio) {
   alert(dogBio);
 }
 
-let total = 0;
-
-function myFee(fee) {
-  total += fee;
-  alert(total);
-}
+// let total = 0;
+// //how do i put this function into jquery?
+// function myFee(fee) {
+//   total += fee;
+//   alert(total);
+// }
 
 $(document).ready(function() {
   $('h3').click(function() {
-    $('.card').fadeOut();
+    $('.card', this).fadeOut();
   });
   $('h3').click(function() {
-    $('.card').fadeIn(1000, 'linear');
+    $('.card', this).fadeIn(2000, 'linear');
   });
 });
+//why isn't THIS working?
+let total = 0;
+$(document).ready(function() {
+  $('.adopt').click(function myFee(fee){
+    total += fee;
+    alert(total);
+  });
+});
+
+$(document).ready(function(){
+  $('.checkout-2')
+})
+
+//$( "input[type=radio][name=baz]:checked" ).val();
+//OR
+// function displayVals() {
+//   var singleValues = $( "#single" ).val();
+//   var multipleValues = $( "#multiple" ).val() || [];
+//   // When using jQuery 3:
+//   // var multipleValues = $( "#multiple" ).val();
+//   $( "p" ).html( "<b>Single:</b> " + singleValues +
+//     " <b>Multiple:</b> " + multipleValues.join( ", " ) );
+// }
+//
+// $( "select" ).change( displayVals );
+// displayVals();
