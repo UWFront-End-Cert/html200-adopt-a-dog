ac2766186278833e9cc41bacb99b0744c306b852
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-04-21 11:04:08 -0500

    updated dog pricing on form.html and jQuery on form.js

--- scripts/form.js
+++ scripts/form.js
@@ -4,26 +4,38 @@ submitButton.setAttribute('type', 'submit');
 submitButton.textContent = 'Submit';
 submitFinal.appendChild(submitButton);
 
-const form = document.getElementById('myForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  myAdoption();
-  alert("Thank you. The form information has been received.");
-})
+let total = 0;
 
-function myAdoption() {
-  const x = document.getElementById('myForm');
-  let text = ' ';
-  for (let i = 0; i < x.length; i++) {
-    if(x.elements[i].type == 'radio') {
-      if(x.elements[i].checked == false) {
+$(document).ready(function () {
+  $('#myForm').click(function myAdoption() {
+  const form = document.getElementById('myForm');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    myAdoption();
+    alert("Thank you. The form information has been received.");
+  });
+  });
+  $('#myForm').click(function() {
+    const x = document.getElementById('myForm');
+    let text = ' ';
+    for (let i = 0; i < x.length; i++) {
+      if(x.elements[i].type == 'radio') {
+        if(x.elements[i].checked == false) {
+          continue;
+        }
+      }
+      if(x.elements[i].value == undefined) {
         continue;
       }
+      text += x.elements[i].name + ' : ' + x.elements[i].value + '\n';
     }
-    if(x.elements[i].value == undefined) {
-      continue;
-    }
-    text += x.elements[i].name + ' : ' + x.elements[i].value + '\n';
-  }
-  console.log(text);
-}
+    console.log(text);
+  });
+});
+
+
+
+// $('.adopt').click(function(){
+//   total += Number($(this).data('price'));
+//   alert('Your current total is $' + total);
+// $('#checkout-2').text('$' + total);
