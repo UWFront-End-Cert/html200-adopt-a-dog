5a804822c7cdb9c005b8c7b21f4b57e36c37bad0
danewallace <danewallace@icloud.com>
2020-04-23 12:53:01 -0700

    Added adoption card hover

--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,11 @@ function addToTotal(adoptionFee) {
   feeTotal = feeTotal + adoptionFee;
   alert('Adoption fees total: $' + feeTotal);
 }
+
+// Add and remove box shadow when overing over dog adoption cards
+$(document).ready(function() {
+    $(".dog-adoption-card").hover(
+        function() { $(this).addClass('card-shadow'); },
+        function() { $(this).removeClass('card-shadow'); }
+    );
+});
