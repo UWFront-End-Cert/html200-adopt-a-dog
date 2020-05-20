835788070927ff6023166e20743e99513f951552
kzvm <kerwin.z.vargas@gmail.com>
2020-05-19 20:31:32 -0400

    Assig2 Lesson4

---
+++ 1st-page/dogs.js
@@ -0,0 +1,129 @@
+function murphyImg() {
+    let murphy = alert('Murphy, Adult breed, $123');
+}
+
+function violetImg() {
+    let violet = alert('Violet, puppy breed, $123');
+}
+
+function piperImg() {
+    let piper = alert('Piper, puppy breed, $123');
+}
+
+function maximusImg() {
+    let maximus = alert('Maximus, Adult breed, $123');
+}
+
+function jackImg() {
+    let jack = alert('Jack, puppy breed, $123');
+}
+
+function jakeImg() {
+    let jake = alert('Jake, Adult breed, $123');
+}
+
+function lucasImg() {
+    let lucas = alert('Lucas, Adult breed, $123');
+}
+
+function duffyImg() {
+    let duffy = alert('Duffy, Adult breed, $123');
+}
+
+function poppyImg() {
+    let poppy = alert('Poppy, Adult breed, $123');
+}
+
+function angusImg() {
+    let angus = alert('Angus, Adult breed, $123');
+}
+
+function lunaImg() {
+    let luna = alert('Luna, Adult breed, $123');
+}
+
+function stellaImg() {
+    let stella = alert('Stella, Adult breed, $123');
+}
+
+
+
+//For Buttons
+
+var mur = 123,
+    vio = 123,
+    pip = 123,
+    max = 123,
+    jac = 123,
+    jak = 123,
+    luc = 123,
+    duf = 123,
+    pop = 123,
+    ang = 123,
+    lun = 123,
+    ste = 123;
+let sum = 0;
+
+
+function onMurphy() {
+    alert(mur)
+    
+}
+
+function onViolet() {
+    alert(vio)
+//    vio++;
+//    document.getElementById("vio").innerHTML = vio;
+//    calculateSum();
+}
+
+function onPiper() {
+    alert(pip);
+}
+
+function onMaximus() {
+    alert(max);
+}
+
+function onJack() {
+    alert(jac);
+}
+
+function onJake() {
+    alert(jak);
+}
+
+function onLucas() {
+    alert(luc);
+}
+
+function onDuffy() {
+    alert(duf);
+}
+
+function onPoppy() {
+    alert(pop);
+}
+
+function onAngus() {
+    alert(ang);
+}
+
+function onLuna() {
+    alert(lun);
+}
+
+function onStella() {
+    alert(ste);
+}
+
+//function calculateSum() {
+//    document.getElementById("sum").innerHTML = mur + vio;
+//    alert(calculateSum)
+//}
+
+function adder() {
+    sum = mur + vio;
+    alert(sum);
+}
+
