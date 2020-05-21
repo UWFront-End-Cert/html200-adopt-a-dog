512f9ee267968095a85770c0055ac2688dca6178
swati <swatidash2007@gmail.com>
2020-05-20 19:41:49 -0700

    created form value js

--- js/form.js
+++ js/form.js
@@ -8,18 +8,51 @@ btn.addEventListener('click', getForm);
 
 
 
-
-
 const form =
 {
   name:'',
-  email: '',
-  streetaddress:'',
+  email:'',
+  street:'',
   city:'',
   state:'',
-  zipcode:0
+  zip:'',
+  firsttimeadopter:'',
+  pickup:''
 }
 
+
+
+
+
 function getInfo() {
-  console.log(form.obj);
+
+
+  form.name = document.getElementById('name').value;
+  form.email = document.getElementById('email').value;
+  form.street = document.getElementById('street').value;
+  form.city = document.getElementById('city').value;
+  form.state = document.getElementById('state-select').value;
+  form.zip = document.getElementById('zip').value;
+  form.pickup = document.getElementById('pickup').value;
+
+  const radio = document.getElementsByName('firsttimeadopter');
+  for(let i=0; i<radio.length; i++ ) {
+    if(radio[i].checked) {
+      form.firsttimeadopter = radio[i].value;
+      break;
+    }
+  }
+  // const radioNo = document.getElementById('radio2').value;
+  // const radioYes = document.getElementById('radio1').value;
+ console.log(form);
+ 
+// if(radio1.checked==true){
+//   console.log(radio1.value);
+//    else if(radio2.checked==true) {
+//     console.log(radio2.value);
+//     // else {
+//     //   console.log("nothing selected");
+//     // }
+//   }
+
 }
