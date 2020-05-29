7b935078ac6befa878bc054e7ed1676292403804
paulfleurdelys <paul.fleurdelys@gmail.com>
2020-05-28 21:27:49 -0700

    Blog post creation script finished

--- scripts/script.js
+++ scripts/script.js
@@ -10,17 +10,47 @@ let blogPosts = [
     {image_url:"images/blog-3.jpg", image_alt: "woman with dog playing fetch at sunset",title: "Teach Your Dog To Fetch!", text: ["Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.", "Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam."]}
 ]
 
+let dogCards = [
+    {image_url:"images/murphy-card.jpg", image_alt:"dog", name:"Murphy", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/poppy-card.jpg", image_alt:"dog", name:"Poppy", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/jack-card.jpg", image_alt:"dog", name:"Jack", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/duffy-card.jpg", image_alt:"dog", name:"Duffy", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/lucas-card.jpg", image_alt:"dog", name:"Lucas", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/jake-card.jpg", image_alt:"dog", name:"Jake", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/angus-card.jpg", image_alt:"dog", name:"Angus", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/violet-card.jpg", image_alt:"dog", name:"Violet", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/piper-card.jpg", image_alt:"dog", name:"Piper", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/maximus-card.jpg", image_alt:"dog", name:"Maximus", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/luna-card.jpg", image_alt:"dog", name:"Luna", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"},
+    {image_url:"images/stella-card.jpg", image_alt:"dog", name:"Stella", cost:"123.45", text:"Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae"}
+
+]
+
 function dogCardPriceOnClick(dogPrice) {
     cartTotal = cartTotal + dogPrice;
     alert ("New price is " + cartTotal);
 }
 
-function javascriptTest() {
-    alert("code reached");
-    let newNode = document.createElement("div");
-    let newNodeText = document.createTextNode("Dog Card Javascript");
-    let sectionLeft = document.getElementById("homepage-section-left")
-    sectionLeft.appendChild(newNode.appendChild(newNodeText));
+function createDogCards() {
+    dogCards.forEach(e => {
+        let article = document.createElement("article");
+        let image = document.createElement("img");
+        let heading = document.createElement("h2");
+        let paragraph = document.createElement("p");
+        let headingText = document.createTextNode(e.name);
+        let dogCardDescription = document.createTextNode(e.text);
+        article.classList.add("dog-card");
+        image.classList.add("dog-card-image");
+        heading.classList.add("dog-card-heading");
+        image.src = e.image_url;
+        image.alt = e.image_alt;
+        heading.appendChild(headingText);
+        paragraph.appendChild(dogCardDescription);
+        article.appendChild(image);
+        article.appendChild(heading);
+        article.appendChild(paragraph)
+        document.querySelector("div.dog-card-display-full").appendChild(article);
+    })
 }
 
 function createBlogPosts() {
@@ -55,3 +85,6 @@ function onSubmit() {
     alert("Thank you. The form information has been received");
 }
 
+function homepageBlogPreview() {
+    
+}
