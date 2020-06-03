350efd51fb509feaa91ac5918e058c5d79e1a8b1
kzvm <kerwin.z.vargas@gmail.com>
2020-06-02 20:33:04 -0400

    adding Assignment2 of lesson4

--- 1st-page/dogs.js
+++ 1st-page/dogs.js
@@ -46,84 +46,16 @@ function stellaImg() {
     let stella = alert('Stella, Adult breed, $123');
 }
 
+let total = 0;
 
-
-//For Buttons
-
-var mur = 123,
-    vio = 123,
-    pip = 123,
-    max = 123,
-    jac = 123,
-    jak = 123,
-    luc = 123,
-    duf = 123,
-    pop = 123,
-    ang = 123,
-    lun = 123,
-    ste = 123;
-let sum = 0;
-
-
-function onMurphy() {
-    alert(mur)
-    
+function onDogPrice(price) {
+    total += price;
+    alert(total);
 }
 
-function onViolet() {
-    alert(vio)
-//    vio++;
-//    document.getElementById("vio").innerHTML = vio;
-//    calculateSum();
-}
 
-function onPiper() {
-    alert(pip);
-}
 
-function onMaximus() {
-    alert(max);
-}
 
-function onJack() {
-    alert(jac);
-}
 
-function onJake() {
-    alert(jak);
-}
 
-function onLucas() {
-    alert(luc);
-}
-
-function onDuffy() {
-    alert(duf);
-}
-
-function onPoppy() {
-    alert(pop);
-}
-
-function onAngus() {
-    alert(ang);
-}
-
-function onLuna() {
-    alert(lun);
-}
-
-function onStella() {
-    alert(ste);
-}
-
-//function calculateSum() {
-//    document.getElementById("sum").innerHTML = mur + vio;
-//    alert(calculateSum)
-//}
-
-function adder() {
-    sum = mur + vio;
-    alert(sum);
-}
 
