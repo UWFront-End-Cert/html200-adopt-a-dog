37a70a422a1b22c71e09791d3a6b23d3bba2ea86
Nichole Godwin <headiehg@mail.com>
2020-06-26 17:03:53 -0700

    fixed blog anchor ids

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,11 +1,11 @@
 /*const foot = document.getElementById("foot");
- 
+
 // Creates title of blog page
 const title = document.createElement('h2');
 title.textContent = "Adoptadog Blog";
 title.className = "blog-title";
 document.body.insertBefore(title, foot);
- 
+
 // Blogs container
 const blogs = document.createElement("section");
 blogs.setAttribute("class", "blogs");
@@ -117,9 +117,9 @@ const blog = document.createElement("article");
 blog.setAttribute("class", "blog");
 
 const allparagraphs = "duciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.";
-const blog_one = ["./images/blog-1.jpg", "Girl sitting with arm around dog in the canyons", "Traveling With Your Dog"];
-const blog_two = ["./images/blog-2.jpg", "Multiple Dogs on a leash going for a walk", "How To Walk Multiple Dogs"];
-const blog_three = ["./images/blog-3.jpg", "Girl holding ball during sunset while dog patiently waits for her to throw it", "Teach Your Dog To Fetch!"];
+const blog_one = ["./images/blog-1.jpg", "Girl sitting with arm around dog in the canyons", "Traveling With Your Dog", "travel"];
+const blog_two = ["./images/blog-2.jpg", "Multiple Dogs on a leash going for a walk", "How To Walk Multiple Dogs", "walk"];
+const blog_three = ["./images/blog-3.jpg", "Girl holding ball during sunset while dog patiently waits for her to throw it", "Teach Your Dog To Fetch!", "fetch"];
 
 const all = [blog_one, blog_two, blog_three];
 
@@ -127,6 +127,7 @@ for (let x = 0; x < all.length; x += 1) {
 	const object = all[x];
 	const blogImage = document.createElement("div");
 	blogImage.setAttribute("class", "c1");
+	blogImage.setAttribute("id", object[3]);
 	const image = document.createElement("img");
 	image.setAttribute("src", object[0]);
 	image.setAttribute("alt", object[1]);
