bcbc7f7e6a88dd5de53fe57f1b8e782c3231bd4f
Ryan Freeland <ryan.freeland94@gmail.com>
2020-07-28 12:07:16 -0700

    js added to website

---
+++ scripts/doginfo.js
@@ -0,0 +1,10 @@
+function dogInfo(dog){
+
+  name = 'x';
+  breed = 'y';
+  fee = 'z';
+
+  clicked_dog = "This dog is named" + " " + name + "." + " " + name + " " + "is a " + " " + breed + "." + " " + name + " " + "costs $" + fee + "."
+
+  alert(clicked_dog);
+}
