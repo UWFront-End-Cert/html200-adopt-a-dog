b519106d8487a8f66722c2405b12b37b085fe658
Ryan Freeland <ryan.freeland94@gmail.com>
2020-07-29 19:26:53 -0700

    click handlers added to images and adopt buttons

--- scripts/doginfo.js
+++ scripts/doginfo.js
@@ -1,10 +1,15 @@
-function dogInfo(dog){
-
-  name = 'x';
-  breed = 'y';
-  fee = 'z';
-
-  clicked_dog = "This dog is named" + " " + name + "." + " " + name + " " + "is a " + " " + breed + "." + " " + name + " " + "costs $" + fee + "."
-
+function dogInfo(name,breed,fee){
+  let clicked_dog = "This dog is named" + " " + name + "." + " " + name + " is a " + breed + "." + " " + name + " costs " + fee + "."
   alert(clicked_dog);
+}
+
+function dogAdopt(cost){
+  var total_items = [];
+  total_items.push(cost);
+  var total = 0;
+  for(let i = 0; i < total_items.length;i++){
+    total += Number(total_items[i]);
+  }
+  total_alert = 'Your total cost of adoption is ' + '$' + total + '.';
+  alert(total_alert);
 }
