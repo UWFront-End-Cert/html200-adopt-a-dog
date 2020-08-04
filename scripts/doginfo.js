70e39d2f2ca3bd4344f551c293c17120beece239
Ryan Freeland <ryan.freeland94@gmail.com>
2020-08-04 11:55:58 -0700

    accumulation of costs in array updated

--- scripts/doginfo.js
+++ scripts/doginfo.js
@@ -3,13 +3,15 @@ function dogInfo(name,breed,fee){
   alert(clicked_dog);
 }
 
+var total_items = []; //this must be placed outside of function in order to have variables logged into the array, as inside the function, the array will constantly reset//
 function dogAdopt(cost){
-  var total_items = [];
   total_items.push(cost);
   var total = 0;
-  for(let i = 0; i < total_items.length;i++){
+  for(let i = 0; i < total_items.length; i++){
     total += Number(total_items[i]);
   }
   var total_alert = 'Your total cost of adoption is ' + '$' + total + '.';
+  console.log(total);
+  alert(total)
   alert(total_alert);
 }
