1008bb5cd21d57afaa70610b56cd2b09ced508cd
Steve-Carlton <stevecarlton@Steves-MBP.lan>
2020-08-10 12:26:56 -0700

    scripting wip

---
+++ scripts/ada-01.js
@@ -0,0 +1,12 @@
+let n = "Murphy";
+
+let b = "hound";
+
+let c = "$123.45";
+
+let dogInfo = n + ', ' + b + ', '+ c+'.'
+
+function displayDogInfo(dogInfo) {
+
+  alert(dogInfo);
+}
---
+++ scripts/ada-02.js
@@ -0,0 +1,22 @@
+
+let customerPrice = total + result;
+let result;
+
+
+function adder(x, y) {
+  result = x + y;
+}
+
+adder(1, .55);
+
+
+
+let total;
+
+function cartTotal() {
+  total = result + 123.45;
+  return total;
+}
+
+
+console.log(cartTotal(result + total));
