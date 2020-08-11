5ff25f77def60608e09f1514c2252e61c1d688fb
Steve-Carlton <stevecarlton@Steves-MBP.lan>
2020-08-11 16:24:10 -0700

    wip

--- scripts/ada-01.js
+++ scripts/ada-01.js
@@ -1,12 +1,51 @@
-let n = "Murphy";
+//Dog specific Arrays
+/*
+let murphy = ['Murphy', 'hound', '$123.45']
 
-let b = "hound";
+let murphyString = String(murphy);
 
-let c = "$123.45";
 
-let dogInfo = n + ', ' + b + ', '+ c+'.'
+let poppy = ['Poppy', 'terrier', '$123.45']
 
-function displayDogInfo(dogInfo) {
+let poppyString = String(poppy);
+*/
+// Arrays by dog info type
 
-  alert(dogInfo);
+let dogName = ['Murphy', 'Poppy', 'Jack']
+let dogBreed = ['hound', 'terrier', 'beagle']
+let dogPrice = [100, 500, 300]
+
+function whichDog(n, b, p) {
+  console.log(dogName[n], dogBreed[b],
+  dogPrice[p]);
 }
+
+// Cost Calculator
+
+let subtotal = 0;
+let itemPrice = 100;
+let total = subtotal + itemPrice;
+
+
+function costCalculator() {
+  subtotal + itemPrice;
+  //return total;
+}
+
+//console.log(total);
+
+/* total = subtotal + itemPrice */
+
+
+/*
+let x = 24;
+let y = 12;
+let result;
+
+//adder function example
+function adder(x, y) {
+  result = x + y;
+}
+
+adder(1, .55);
+*/
