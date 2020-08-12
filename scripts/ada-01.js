f695fc30d1fcc4158a55973cad17c724bb745d4f
Steve-Carlton <stevecarlton@Steves-MBP.lan>
2020-08-11 19:03:54 -0700

    cost calculator funcation added

--- scripts/ada-01.js
+++ scripts/ada-01.js
@@ -1,38 +1,24 @@
-//Dog specific Arrays
-/*
-let murphy = ['Murphy', 'hound', '$123.45']
-
-let murphyString = String(murphy);
-
-
-let poppy = ['Poppy', 'terrier', '$123.45']
-
-let poppyString = String(poppy);
-*/
 // Arrays by dog info type
 let dogString;
 let dogName = ['Murphy', 'Poppy', 'Jack']
 let dogBreed = ['hound', 'terrier', 'beagle']
-let dogPrice = [100, 200, 300]
+let dogPrice = [100, 500, 300]
 
 function whichDog(n, b, p) {
   let dogString = dogName[n] + ', ' + dogBreed[b]  + ', $' + dogPrice[p];
   //console.log(dogString);
   alert(dogString);
-  //return dogString; <-- do i need this line?
+  //return dogString; <-- works w/o a return...? do i need this line?
 }
 
 
 // Cost Calculator
+let total = 0;
 
-let subtotal = 0;
-let itemPrice = 100;
-let total = subtotal + itemPrice;
-
-
-function costCalculator() {
-  subtotal + itemPrice;
-  //return total;
+function costCalculator(x) {
+  total += x;
+  //return total; return not necessary. code stops at return. ok to have a function w/o a return?
+  alert(total);
 }
 
 //console.log(total);
