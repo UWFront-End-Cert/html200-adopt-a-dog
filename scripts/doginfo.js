2ed2f2d46492be0bdcf84154956ee597c9edbb2d
Ryan Freeland <ryan.freeland94@gmail.com>
2020-09-04 13:32:52 -0700

    form final update

--- scripts/doginfo.js
+++ scripts/doginfo.js
@@ -109,7 +109,12 @@ $('form').submit(function(e){
 
     } else {
 
-      console.log(name_data.value, email_data.value, adr_data.value, city_data.value, state_data.value, zip_data.value, firsttimeadopt_data.checked, location_data.value); //better to get form data as key value pairs and to have method on form as POST, so that it isn't appended to website URL//g
+      var values = {};
+      $.each($('form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+      });
+
+      console.log(values); //better to get form data as key value pairs and to have method on form as POST, so that it isn't appended to website URL//g
 
       alert(thanks);
       event.preventDefault();
