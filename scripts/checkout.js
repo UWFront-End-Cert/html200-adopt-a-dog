10afb8761a75dc6c61f2fee8cb2d58ecd7d9bac7
Steve-Carlton <63474298+Steve-Carlton@users.noreply.github.com>
2020-09-29 17:30:52 -0700

    log user inputs WIP

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,30 @@
 //Assignment 8
 
+
+// Console log the values inside the form, you can build a string or build an object.
+//get form. assign variable
+const form = document.getElementsByTagName('form');
+//get input fields. assign variables
+let custmrName = document.getElementById('custmrName');
+let custmrSt = document.getElementById('custmrSt');
+
+
+//create object for form inputs.
+const formValuesObj = {
+  name: custmrName,
+  street: custmrSt
+};
+
+
+function logFormValues() {
+console.log(custmrName.value);
+console.log(custmrSt.value);
+}
+
+logFormValues(formValuesObj);
+
 // Display alert onclick submit button
 let checkoutSubmitForm = document.getElementById('button').addEventListener("click", myFunction);
 function myFunction() {
   alert('Thank you. The form information has been received');
-}
-
-// Console log the values inside the form, you can build a string or build an object.
-
-const form = document.getElementsByTagName('form');
-
-//addEventListener??
-function logFormValues() {
-console.log(form.value);
 }
