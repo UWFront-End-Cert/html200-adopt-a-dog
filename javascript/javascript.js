1ebf5a396bfedd3754eb1673c48a76511c667582
web media <59540946+jem1520@users.noreply.github.com>
2020-10-05 07:30:35 -0400

    updating javescript functions with jquery

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,32 @@
 
 
-function doginfo(name, breed, fee) {
+$('.box5').hover(function(e) {
+  $('img', this).addClass('tile-border');
+}, function(e) {
+  $('img', this).removeClass( 'tile-border' );
+}
+);
 
+$('.box-3').hover(function(e) {
+  $('img', this).addClass('tile-border');
+}, function(e) {
+  $('img', this).removeClass( 'tile-border' );
+}
+);
+
+$(".dogs-button1 , .dogs-button").click(function(e) {
+   {addToCart(123.45);}
+});
+
+let total = 0;
+function addToCart(carttotal) {
+  total += carttotal;
+  alert("Shopping Cart Total: $" + total);
+  document.getElementById ("cart-total").innerHTML = ("$" + total);
+}
+
+
+function doginfo(name, breed, fee) {
 
   result = name + breed + fee;
 
