d968bf391a7e1f90f8538b77a500b1d8cda07edf
jabreu92 <jose.abreu4@hotmail.com>
2020-12-10 20:51:45 -0800

    Working on grab values from form

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -8,3 +8,30 @@ function adoptButton(cost) {
     total = total + Number(cost);
     alert(`Total = $${total.toFixed(2)}`);
 }
+
+function checkoutSubmit(message) {
+    alert(message);
+    let nameValue = document.getElementById("nameValue").value;
+    let emailValue = document.getElementById("emailValue").value;
+    let addressValue = document.getElementById("addressValue").value;
+    let cityValue = document.getElementById("cityValue").value;
+    let stateValue = document.getElementById("stateValue").value;
+    let pickupValue = document.getElementById("pickupValue").value;
+    let zipcodeValue = document.getElementById("zipcodeValue").value;
+    let firstTimeValueYes = document.getElementById("firstTimeValueYes").value;
+    let firstTimeValueNo = document.getElementById("firstTimeValueNo").value;
+    alert(nameValue);
+    alert(emailValue);
+    alert(addressValue);
+    alert(cityValue);
+    alert(stateValue);
+    alert(pickupValue);
+    alert(zipcodeValue);
+    alert(firstTimeValueYes);
+    alert(firstTimeValueNo);
+  
+  
+    
+
+}
+
