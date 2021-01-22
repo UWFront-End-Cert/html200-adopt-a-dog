7023ee2b91282b3bd5cdfea9ef0dc747128ca52a
Dw3ntz <72217128+Dw3ntz@users.noreply.github.com>
2021-01-21 17:29:31 -0800

    adding click handlers and alerts to dog card for dog info and adoptiong fee

--- dog-adopt.js
+++ dog-adopt.js
@@ -1,1 +1,14 @@
-console.log('Javascript is working');
+function adoptAlert(name, breed, adoptionFee) {
+    let result = 'Adopt ' + name + ', a ' + breed + ', adoption fee of ' + adoptionFee;
+    
+    alert(result);
+}
+
+function feeCalc() {
+    const fee = 123.45;
+    let result = 0;
+    
+    result = "Your total is: " + "$" + fee;
+    
+    alert(result);
+}
