2963aab8fc0b3250608ba8f3232ae76b8063930f
Michelle B <michellebochman@gmail.com>
2021-01-27 14:14:02 -0500

    Button Scripts Added

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function dogImageAlert(dog, breed, price) {
+    const description = 'This is ' + dog + '! This ' + breed + ' costs ' + price;
+    alert(description);
+}
+
+let sum = 0;
+function totalAdoptPrice(price) {
+    sum = price + sum;
+    alert('Total Cost $' + sum);
+}
