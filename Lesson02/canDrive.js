728f2f6e996034eb39087b3c8067e437426b1451
jgalber <722751739+jgalber@users.noreply.github.com>
2021-01-28 15:36:51 -0800

    Bonus JS added

---
+++ Lesson02/canDrive.js
@@ -0,0 +1,11 @@
+let y = 15;
+const x = 'Wait!! you will be able to drive a car legally soon';
+let w = 16;
+const z = 'Yep, you are now be able to get your driving license legally';
+
+if (y <= 16) {
+  console.log(x);
+}
+if (w >= 16) {
+  console.log(z);
+}
