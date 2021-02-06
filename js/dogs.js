2b9a61dd598cfe95be1beb770b1a4819d29d2008
Cahillmn <mcahill@seattleu.edu>
2021-02-06 14:28:08 -0800

    started onClick

---
+++ js/dogs.js
@@ -0,0 +1,10 @@
+function pictureInfo(dog, breed, price) {
+    result = 'Name: ' + dog + ' Breed: ' + breed + ' Price: ' + price;
+    alert(result);
+}
+
+let sum = 0;
+function Price(price) {
+    result = price + sum;
+    alert('Total Cost to Adopt $' + result);
+} 
