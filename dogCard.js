4404c121bebf8b3810497390ebedd4a6e78481c2
“Eling <elingv@yahoo.com>
2021-02-06 18:13:17 -0800

    add js to the site

---
+++ dogCard.js
@@ -0,0 +1,36 @@
+let dogName =
+let dogAdoptCost =
+
+
+
+
+let dogInfo = document.getElementsByClassName('dogName dogAdoptCost');
+alert(dogInfo);
+
+
+
+
+function dogInfo(dogName + dogAdoptCost) {
+  document.getElementByClassName("dogName dogAdoptCost").innerHTML = "h3";
+  document.innerHTML = h3
+  return [];
+}
+
+dogCard = ()
+onclick="dogInfo()"
+alert(dogInfo);
+
+//function getElementsByClassName(id) {
+  //return [document.getElementById(id)];
+//
+
+//document.getElementsByClassName("class1 class2");
+
+//document.getElementById returns [object HTMLTableCellElement]
+//You need the content of the element, not the element itself:
+//var newOne = document.getElementById('setCustomer').innerHTML;
+
+
+//document.getElementById('setCustomer').onclick = function(){
+//var newOne = document.getElementById('setCustomer');
+//console.log("var newOne is "+newOne);
