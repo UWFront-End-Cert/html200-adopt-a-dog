06f1f0f0acbf3d41e1ed3f37f82aeb659375aff3
“Eling <elingv@yahoo.com>
2021-02-08 15:30:56 -0800

    editing js dogcard

--- dogCard.js
+++ dogCard.js
@@ -1,36 +1,14 @@
+
+
 function dogInfo() {
-  let content = document.getElementById('adoptPoppy').textContent;
-  alert(content);
+
+  let dogDetail = document.getElementById('card1').textContent;
+  alert(dogDetail);
 }
 
-
-
+  //let dogDetail = document.getElementsByClassName('card1').innerHTML;
+  //dogDetail[0].innerHTML
 //function dogInfo() {
-
-  //alert("hello");
+  //let dogDetail = document.getElementById('card1').innerHTML;
+  //alert(dogDetail);
 //}
-
-//function dogInfo(dogName + dogAdoptCost) {
-  //document.getElementByClassName("dogName dogAdoptCost").innerHTML = "h3";
-  //document.innerHTML = h3
-  //return [];
-//}
-
-//dogCard = ()
-//onclick="dogInfo()"
-//alert(dogInfo);
-
-//function getElementsByClassName(id) {
-  //return [document.getElementById(id)];
-//
-
-//document.getElementsByClassName("class1 class2");
-
-//document.getElementById returns [object HTMLTableCellElement]
-//You need the content of the element, not the element itself:
-//var newOne = document.getElementById('setCustomer').innerHTML;
-
-
-//document.getElementById('setCustomer').onclick = function(){
-//var newOne = document.getElementById('setCustomer');
-//console.log("var newOne is "+newOne);
