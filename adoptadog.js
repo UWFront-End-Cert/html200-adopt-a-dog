8fa40d571db093cc695b4d307367f87928ba243e
Josh Horton <joshhorton2244@gmail.com>
2021-02-08 10:27:40 -0800

    lesson4assignment2

---
+++ adoptadog.js
@@ -0,0 +1,19 @@
+let dogName;
+let dogBreed;
+let adoptFee;
+
+// dogProfile = function() {
+//   alert('I am working');
+// }
+dogProfile = function(dogName,dogBreed,adoptFee) {
+  // console.log(click);
+  alert('Name: ' + dogName + ' Breed: ' + dogBreed + ' Adoption Fee: ' + adoptFee);
+}
+
+let adoptRunningTotal = 0;
+
+let adoptFeeTotal = function(adoptFee) {
+
+  adoptRunningTotal += adoptFee;
+    alert(adoptRunningTotal);
+}
