3903f2de436f727317cbee19e4ab737dd06ad591
“Eling <elingv@yahoo.com>
2021-02-08 19:18:02 -0800

    onClick js

--- dogCard.js
+++ dogCard.js
@@ -6,8 +6,11 @@ function dogInfo() {
   alert(dogDetail);
 }
 
+
+//function dogInfo() {
   //let dogDetail = document.getElementsByClassName('card1').innerHTML;
-  //dogDetail[0].innerHTML
+  //dogDetail[0, 1, 2].innerHTML
+
 //function dogInfo() {
   //let dogDetail = document.getElementById('card1').innerHTML;
   //alert(dogDetail);
