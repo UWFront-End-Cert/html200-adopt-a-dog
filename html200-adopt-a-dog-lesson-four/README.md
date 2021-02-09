c4e82533d9a0e6a7e837977c1f77cab0edff0bfb
jgalber <722751739+jgalber@users.noreply.github.com>
2021-02-09 13:24:24 -0800

    course project - Lesson Four

---
+++ html200-adopt-a-dog-lesson-four/README.md
@@ -0,0 +1,4 @@
+# html200-adopt-a-dog
+
+The CSS of Blog's page is now linked to the main stylesheet page, located in the same folder 'styles', name of the file: 'style.css'.
+ 
---
+++ html200-adopt-a-dog-lesson-four/scripts/script.js
@@ -0,0 +1,53 @@
+ 
+function cost() {
+    alert("$123.45");
+}
+
+function infoMurphy(){
+    alert("Murphy \nBreed: Boxer \n\nAdoption fees: $123.45");
+}
+
+function infoPoppy(){
+    alert("Poppy \nBreed: Asian Terrier \n\nAdoption fees: $123.45");
+}
+
+function infoJack(){
+    alert("Jack \nBreed: Beagle \n\nAdoption fees: $123.45");
+}
+
+function infoDuffy(){
+    alert("Duffy \nBreed: American Bulldog \n\nAdoption fees: $123.45");
+}
+
+function infoLucas(){
+    alert("Lucas \nBreed: Labrador \n\nAdoption fees: $123.45");
+}
+
+function infoJake(){
+    alert("Jake \nBreed: English Pointer/Rhodesian \n\nAdoption fees: $123.45");
+}
+
+function infoAngus(){
+    alert("Angus\nBreed: Bullmastiff \n\nAdoption fees: $123.45");
+}
+
+function infoViolet(){
+    alert("Violet \nBreed: Field Spaniel \n\nAdoption fees: $123.45");
+}
+
+function infoPipper(){
+    alert("Pipper \nBreed: Golden Retriever \n\nAdoption fees: $123.45");
+}
+
+function infoMax(){
+    alert("Maximus\nBreed: Mixed Terrier \n\nAdoption fees: $123.45");
+}
+
+function infoLuna(){
+    alert("Luna\nBreed: Akbash \n\nAdoption fees: $123.45");
+}
+
+function infoStella(){
+    alert("Stella\n Breed: Chihuahua \n\nAdoption fees: $123.45");
+}
+
