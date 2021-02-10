129884b995274ba67adfa4566de096c93e64d77f
Poisonpez <estessm@uw.edu>
2021-02-09 19:24:17 -0500

    updated app

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// click on dog image, alerts name, breed, price
+
 function info(name, breed, price) {
   alert(name + ',' + ' ' + breed + ',' + ' ' + price);
   }
 
 
-// click on Adopt button, alerts total of Adopt button clicks
 let total = 0;
 
 function carttot(price) {
