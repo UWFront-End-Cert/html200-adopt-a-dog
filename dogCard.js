1bfb660405457da49791944b7f603142ba4029ea
“Eling <elingv@yahoo.com>
2021-02-10 10:25:54 -0800

    js for adopt button

--- dogCard.js
+++ dogCard.js
@@ -1,11 +1,21 @@
 
-
 function dogInfo() {
-
-  let dogDetail = document.getElementById('card1').textContent;
-  alert(dogDetail);
+  alert('Breed: Beagle, Name: Murphy, Cost to adopt:$123.45');
 }
 
+
+function adoptCost() {
+  let adoptCost = 0;
+  let adoptTotal =  ('Cost to adopt is:' + ' ' +  '\$' + 123.45)
+alert(adoptTotal);
+}
+
+
+//var x = 1; var y = 2; var result = x + y;
+
+//let dogDetail = document.getElementById('card1').textContent;
+//alert(dogDetail);
+//}
 
 //function dogInfo() {
   //let dogDetail = document.getElementsByClassName('card1').innerHTML;
