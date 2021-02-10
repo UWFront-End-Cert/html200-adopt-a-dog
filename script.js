41c65dd81a1f6090e0f39a9e7efa84a95c3cf640
eddy vazquez <>
2021-02-09 22:31:41 -0800

    Shopping car total reflects cart total

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ let dogArray = document.getElementsByClassName('dog-card-details');
 let adoptionTotalString = document.getElementById('totals').innerText.substr(1);
 let adoptionTotalNumber = parseFloat(adoptionTotalString);
 
-// Get price of the selected card button
+// Get price of the selected card button and set the price
 function priceRipper() {
   let specificPriceString = event.path[1].children[2].innerText;
   let specificPriceNumber = parseFloat(specificPriceString.substr(1));
   adoptionTotalNumber = adoptionTotalNumber + specificPriceNumber;
   alert('Total price of adoption: $' + adoptionTotalNumber);
+  document.getElementById('totals').innerText = '$' + adoptionTotalNumber;
 }
 
-// When 'this' was the parameter it returned the button's elements
-// Maybe I can go to it's siblings?
+// When 'this' was the parameter in the handler it returned the button's elements
+// Maybe I can go to it's siblings? Does it know the context? investigate later.
   // function priceRipper(e) {
   //   console.log(e);
   // }
