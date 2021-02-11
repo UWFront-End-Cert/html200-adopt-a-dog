f3fa8cd519a4026a18a3a439cfabc56a829ace46
Dane Seaberg <>
2021-02-10 21:59:10 -0800

    Alert the dog info when its image is clicked on.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-console.log('connected');
+/* Shows the dog info in an alert. */
+function showDogInfo(dogName, dogBreed, adoptionFee) {
+  let infoMessage = 'Name: ' + dogName + '\nBreed: ' + dogBreed + '\nAdoption Fee: ' + adoptionFee;
+  alert(infoMessage);
+}
