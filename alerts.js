286a959623231072a8b86c4b330952b53f809e6a
Sophia <sophiats@uw.edu>
2021-02-11 12:31:23 -0800

    alerts with dog info

---
+++ alerts.js
@@ -0,0 +1,47 @@
+function imageMurphy() {
+  const murphyInfo = ['Murphy', 'American Staffordshire', '$100'];
+
+  alert(murphyInfo);
+}
+
+function imagePoppy() {
+  const poppyInfo = ['Poppy', 'Bichon', '$200'];
+
+  alert(poppyInfo);
+}
+
+function imageJack() {
+  const jackInfo = ['Jack', 'Beagle', '$300'];
+
+  alert(jackInfo);
+}
+
+function imageDuffy() {
+  const duffyInfo = ['Duffy', 'Pitbull', '$400'];
+
+  alert(duffyInfo);
+}
+
+function imageLucas() {
+  const lucasInfo = ['Lucas', 'Lab Mix', '$500'];
+
+  alert(lucasInfo);
+}
+
+function imageJake() {
+  const jakeInfo = ['Jake', 'Yellow Lab', '$600'];
+
+  alert(jakeInfo);
+}
+
+function imageAngus() {
+  const angusInfo = ['Angus', 'Boxer', '$700'];
+
+  alert(angusInfo);
+}
+
+function imageViolet() {
+  const violetInfo = ['Violet', 'Chocolate Lab', '$800'];
+
+  alert(violetInfo);
+}
