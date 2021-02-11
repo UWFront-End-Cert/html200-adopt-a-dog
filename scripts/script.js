30cb71678e483271f29609db7e99ef423c9da16e
Dane Seaberg <>
2021-02-10 22:18:42 -0800

    Add to and alert the total cost when a dog's adopt button is clicked.

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,10 @@ function showDogInfo(dogName, dogBreed, adoptionFee) {
   let infoMessage = 'Name: ' + dogName + '\nBreed: ' + dogBreed + '\nAdoption Fee: ' + adoptionFee;
   alert(infoMessage);
 }
+
+/* Add the dog's adoption fee to the total price. */
+let totalPrice = 0;
+function addDogToTotal(adoptionFee) {
+  totalPrice += adoptionFee;
+  alert('Total cost: ' + totalPrice);
+}
