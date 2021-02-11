dd566d6def2f686f4473f618426b912a81a795d8
Yuting Lin <72534426+filizlin@users.noreply.github.com>
2021-02-11 23:13:44 +0800

    improve javascript code
    Modified the javascript for the adopt button and image information

--- javascript/adoptbutton.js
+++ javascript/adoptbutton.js
@@ -1,73 +1,10 @@
 let Final = 0;
 
-function Murphy() {
-    alert("Adding Murphy (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
+function AddtoCart(value) {
+    Final = Final + value;
     alert("The total adoption fee is: " + Final);
 };
 
-function Poppy() {
-    alert("Adding Poppy (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Jack() {
-    alert("Adding Jack (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Duffy() {
-    alert("Adding Duffy (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Lucas() {
-    alert("Adding Lucas (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Jake() {
-    alert("Adding Jake (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Angus() {
-    alert("Adding Angus (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Violet() {
-    alert("Adding Violet (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Piper() {
-    alert("Adding Piper (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Maximus() {
-    alert("Adding Maximus (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Luna() {
-    alert("Adding Luna (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
-};
-
-function Stella() {
-    alert("Adding Stella (Breed) to cart, the adoption fee is 123.45");
-    Final = Final + 123.45;
-    alert("The total adoption fee is: " + Final);
+function Dog(Name,Breed,Fee) {
+    alert(Name + "(" + Breed + ") has an adoption fee of " + Fee);
 };
