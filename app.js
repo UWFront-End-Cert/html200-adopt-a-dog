3b521abbc3e4411ea28b3b92cdc9a5202189be83
cluy2020 <chanluy@gmail.com>
2021-02-10 18:00:43 -0600

    add javascript file and updated index.html to add .js source

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function info(name, Dogs, price) {
+  alert(name + ',' + ' ' + Dogs + ',' + ' ' + price);
+  }
+
+let total = 0;
+
+function dogprice(price) {
+  total = price + total;
+  alert('Total:' + ' ' + '$' + total);
+}
