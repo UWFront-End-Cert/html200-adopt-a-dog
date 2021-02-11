c9530bceebc7332aec68c6e095680585b859ddd1
hamediaj <hamediajemal2@gmail.com>
2021-02-11 00:03:00 -0800

    adding lesson4 assignment 2

---
+++ javascript/doginfo.js
@@ -0,0 +1,12 @@
+// console.log('hello  world');
+
+function dogInformation(name, breed, price) {
+  alert(name + ',' + ' ' + breed + ',' + ' ' + price);
+  }
+
+let total = 0;
+
+function adoptionFee(price) {
+  total = price + total;
+  alert('Total:' + ' ' + '$' + total);
+}
