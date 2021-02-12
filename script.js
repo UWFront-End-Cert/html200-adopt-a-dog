e7cdd05c098de4bed32dd5738625b728456363ed
deaddiana <deaddian@uw.edu>
2021-02-11 18:02:17 -0800

    added script

---
+++ script.js
@@ -0,0 +1,57 @@
+function murphy() {
+  alert("Name: Murphy, Breed: Boxer Mix, Adoption Cost: $150.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+
+function poppy() {
+  alert("Name: Poppy, Breed: Shih Tz, Adoption Cost: $150.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+function jack() {
+  alert("Name: Jack, Breed: Beagle, Adoption Cost: $200.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+
+function duffy() {
+  alert("Name: Duffy, Breed: Pitbull, Adoption Cost: $150.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+function lucas() {
+  alert("Name: Lucus, Breed: German Shppard Mix, Adoption Cost: $150.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+
+function jake() {
+  alert("Name: Jake, Breed: German Shppard, Adoption Cost: $150.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+function angus() {
+  alert("Name: Angus, Breed: Boxer, Adoption Cost: $150.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+
+function violet() {
+  alert("Name: Violet, Breed: Lab/Dachsund, Adoption Cost: $200.00");
+  let angus = str.split(',');
+  console.log(angus);
+}
+
+
+//  UNABLE TO GET FUNCTION TO WORK     function dogAdder(){
+            //let a = Number(document.getElementById('a').value);
+            //let b = Number(document.getElementById('b').value);
+            //let c = Number(document.getElementById('c').value);
+            //let d = Number(document.getElementById('d').value);
+
+            //let total = a + b + c + d;
+
+            //document.getElementById("total").value = total;
+
+        //}
