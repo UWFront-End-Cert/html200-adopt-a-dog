a6edfc2cb0b7a340e3fb3edd3e0c6fafaf984ae4
djr206 <myEmail@wherever.com>
2021-02-12 11:48:16 -0800

    L6 submittal

--- js-adoptadog/img-info.js
+++ js-adoptadog/img-info.js
@@ -1,3 +1,20 @@
+// html from index.html that has button click calls to the  js functions 
+            //     <div class="item">
+            //     <img src="images/murphy-card.jpg" onclick="dogInfo('Murphy ','Dog ',123.45)" alt="Descripion of featured dog.">
+            //     <h2>Murphy</h2>
+            //     <p><strong>Cost to Adopt: </strong>$123.45</p>
+            //     <p>Kilsie eitislissi zposesko cha. Abjlkseee jlkt isani stylesheet lked eialse. Lipsseis jkj eiso spiroot, lkjlke kjkeise. Kilsie eitislissi zposesko cha.</p>    
+            //     <p><a onclick="feeTotal(123.45)">Adopt</a></p>
+            // </div>
+            // <div class="item">
+            //     <img src="images/poppy-card.jpg" onclick="dogInfo('Poppy ','Dog ',123.45)" alt="Descripion of featured dog.">
+            //     <h2>Poppy</h2>
+            //     <p><strong>Cost to Adopt: </strong>$123.45</p>
+            //     <p>Kilsie eitislissi zposesko cha. Abjlkseee jlkt isani stylesheet lked eialse. Lipsseis jkj eiso spiroot, lkjlke kjkeise. Kilsie eitislissi zposesko cha.</p>    
+            //     <p><a onclick="feeTotal(123.45)">Adopt</a></p>
+            // </div>
+
+
 // 
 // Link JavaScript to your site.
 // Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
