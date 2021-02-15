12e294c0cc47f7d71a2ace9cbcb3b553d55e648a
HCoulson <HCoulson@users.noreply.github.com>
2021-02-14 22:38:39 -0800

    added click handlers to dog photos

---
+++ javascript/scripts.js
@@ -0,0 +1,8 @@
+//console.log('it is totally working');
+
+function dogInfo(name,breed,price) {
+  //info = name + breed + price;
+  info = `You can adopt ${name}, a ${breed}, for ${price}`;
+  //info = 'Hi there!';
+  alert(info);
+}
