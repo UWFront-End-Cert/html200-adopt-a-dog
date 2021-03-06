555b1abbcd4eb78da2080cc8aaef94b8a1d589e1
JCahill <cahilljm@aol.com>
2021-03-05 16:11:43 -0800

    Add Event Handler to Button on Contact Page

---
+++ js/form.js
@@ -0,0 +1,8 @@
+const el = document.getElementById("contact-button")
+
+el.addEventListener("click", function(event){
+    // event.preventDefault();
+    const result = 'Thank you. The form information has been received.'
+    alert(result);
+    }); 
+
