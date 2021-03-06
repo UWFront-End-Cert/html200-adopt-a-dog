c3521cab7a01d6038c06abb75bf2222bd043c739
DAN POWELL <danpowell@DANs-MacBook-Air.local>
2021-03-05 16:03:39 -0800

    working with array by referring to array location, loop not done yet

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,23 @@
 
 let totalCostArray = [];
-
+const dogBlogData = [
+    {
+    image: './images/blog-1.jpg',
+    title:'How to Walk your Dog',
+    blogText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit imperdiet urna, vitae dignissim enim iaculis eu. Interdum et mlesuada fames ac ante ipsum primis in faucibus. Ut a justo pellentesque, gravida lacus id, rhoncus lorem. Cras aliquam mauris lacus, lacinia posuere tellus blandit id. Morbi consectetur neque ac odio eleifend dictum. Vestibulum tempus velit eu nibh laoreet, vel eleifend nisi ornare. Nulla rhoncus suscipit velit ut suscipit. Maecenas quis consectetur sapien. Maecenas vel fringilla tellus. Cras fringilla nibh sed neque pellentesque accumsan.'
+},
+{
+    image: './images/blog-2.jpg',
+    title: 'How to Walk Multiple Dogs',
+    blogText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit imperdiet urna, vitae dignissim enim iaculis eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut a justo pellentesque, gravida lacus id, rhoncus lorem. Cras aliquam mauris lacus, lacinia posuere tellus blandit id. Morbi consectetur neque ac odi eleifend dictum. Vestibulum tempus velit eu nibh laoreet, vel eleifend nisi ornare. Nulla rhoncus suscipit velit ut suscipit. Maecenas quis consectetur sapien. Maecenas vel fringilla tellus. Cras fringilla nibh sed neque pellentesque accumsan.'
+},
+{
+    image: './images/blog-3.jpg',
+    title: 'How to Teach Your Dog To Fetch',
+    blogText:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit imperdiet urna, vitae dignissim enim iaculis eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut a justo pellentesque, gravida lacus id, rhoncus lorem. Cras aliquam mauris lacus, lacinia posuere tellus blandit id. Morbi consectetur neque ac odio eleifen dictum. Vestibulum tempus velit eu nibh laoreet, vel eleifend nisi ornare. Nulla rhoncus suscipit velit ut suscipit. Maecenas quis consectetur sapien. Maecenas vel fringilla tellus. Cras fringilla nibh sed neque pellentesque accumsan.'
+}
+]  
+;
 
 function arraySummer(dogCost) {
 totalCostArray.push (dogCost);
@@ -18,6 +35,8 @@ function dogInfoSheetReturn(dogCost,dogName, dogBreed){
 function blogFieldsGenerator() {
         console.log('Test');
         gridItemContainer = document.getElementById('gridItemContainer');
+
+        
         const blogMainHolder = document.createElement('article');
         blogMainHolder.className = 'blog-main-holder';
         gridItemContainer.appendChild(blogMainHolder);
@@ -25,23 +44,18 @@ function blogFieldsGenerator() {
         blogImageHolder.className = 'blog-img-holder';
         blogMainHolder.appendChild(blogImageHolder);
         const image = document.createElement('img');
-        image.src = './images/blog-1.jpg';
+        image.src = dogBlogData[0].image;
         blogImageHolder.appendChild(image);  
         const blogTextHolder = document.createElement('div');
         blogTextHolder.className = 'blog-text-holder';
         blogMainHolder.appendChild(blogTextHolder);
-        const blogSubHeader1 = document.createElement('h3');
-        blogSubHeader1.className = 'blog-sub-header';
-        blogSubHeader1.textContent = 'How to Walk your Dog';
-        blogTextHolder.appendChild(blogSubHeader1);
+        const blogSubHeader = document.createElement('h3');
+        blogSubHeader.className = 'blog-sub-header';
+        blogSubHeader.textContent = dogBlogData[0].title;
+        blogTextHolder.appendChild(blogSubHeader);
         const blogSubPara = document.createElement('p');
         blogSubPara.className = 'blog-sub-paragraph';
-        blogSubPara.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer hendrerit imperdiet urna, vitae dignissim enim iaculis eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut a justo pellentesque, gravida lacus id, rhoncus lorem. Cras aliquam mauris lacus, lacinia posuere tellus blandit id. Morbi consectetur neque ac odio eleifend dictum. Vestibulum tempus velit eu nibh laoreet, vel eleifend nisi ornare. Nulla rhoncus suscipit velit ut suscipit. Maecenas quis consectetur sapien. Maecenas vel fringilla tellus. Cras fringilla nibh sed neque pellentesque accumsan.'
-        blogTextHolder.appendChild(blogSubPara);
+        blogSubPara.textContent = dogBlogData[0].blogText;
+        blogTextHolder.appendChild(blogSubPara);    
 
-        
-        
-        
-       
 }
- 
