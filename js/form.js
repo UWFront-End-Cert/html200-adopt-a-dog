08c50ed878468d3d02027ca919c2b630323d77e7
JCahill <cahilljm@aol.com>
2021-03-07 14:50:00 -0800

    Console Log form data and other minor changes

--- js/form.js
+++ js/form.js
@@ -1,8 +1,24 @@
-  
-const el = document.getElementById("contact-button")
+  const el = document.getElementById("contact-button")
 
-el.addEventListener("click", function(event){
-    // event.preventDefault();
-    const result = 'Thank you. The form information has been received.'
-    alert(result);
-    }); 
+  el.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const result = 'Thank you. The form information has been received.'
+      alert(result);
+
+      const formdata = document.getElementById('orderForm').elements;
+      console.log(formdata)
+
+      for (let i = 0; i < formdata.length - 1; i++) {
+
+          const id = document.getElementById('orderForm').elements[i].id;
+          const val = document.getElementById('orderForm').elements[i].value;
+
+          //   console.log("ID: ", id);
+          //   console.log("Value: ", val);
+          const submitdata = ("ID: " + id + " " + "Value: " + val);
+          console.log(submitdata);
+
+      }
+
+  });
