e1142e0a412523548ce9395bc6e7dfa6f7e53874
eddy vazquez <>
2021-03-09 08:12:10 -0800

    Remaining bits are dog cards, total and submit button

--- blog-script.js
+++ blog-script.js
@@ -26,13 +26,27 @@ const checkout = [
     dropDown: true,
     name: 'state',
     label: 'State',
-    states: ['WA', 'CA', 'OR']
+    choices: ['WA', 'CA', 'OR']
   },
   {
     dropDown: false,
     name: 'zip',
     label: 'Zip Code'
   },
+  {
+    dropDown: false,
+    type: 'radio',
+    name: 'returning',
+    answer: ['Yes', 'No'],
+    id: 'yes'
+  },
+  {
+    dropDown: true,
+    name: 'pickup',
+    label: 'Pickup Location',
+    id: 'pickup',
+    choices: ['Seattle', 'Portland', 'San Francisco']
+  }
 ];
 
 const form = document.createElement('form');
@@ -43,6 +57,7 @@ for (let i=0; i< checkout.length; i++){
   const field = checkout[i];
 
   if(field.dropDown === true) {
+    //Makes the dropdown
     const label = document.createElement('label');
     label.textContent = field.label;
     label.setAttribute('for', field.name);
@@ -54,23 +69,51 @@ for (let i=0; i< checkout.length; i++){
     form.appendChild(label);
     form.appendChild(select);
 
-    for (let i=0; i < field['states'].length; i++) {
-      // you were able to do it! now make sure the values work
+    //Creates the options of the dropdown
+    for (let i=0; i < field['choices'].length; i++) {
       const option = document.createElement('option');
-      option.setAttribute('value', field['states'][i]);
-      option.innerText = field['states'][i];
+      option.setAttribute('value', field['choices'][i]);
+      option.innerText = field['choices'][i];
       select.appendChild(option);
     }
 
-  } else {
-    const label = document.createElement('label');
-    label.textContent = field.label;
-    label.setAttribute('for', field.name);
+  } else if (field.type === 'radio') {
+      //Creates the question
+      const pQuestion = document.createElement('p');
+      pQuestion.innerText = 'First time adopter?';
+      form.appendChild(pQuestion);
 
-    const input = document.createElement('input');
-    input.setAttribute('id', field.name);
+      //Creates the radio input buttons and label
+      for (let i=0; i < field['answer'].length; i++){
 
-    form.appendChild(label);
-    form.appendChild(input);
-  };
+        const input = document.createElement('input');
+        input.setAttribute('type', field.type);
+        input.setAttribute('name', field.name);
+        input.setAttribute('id', field['answer'][i]);
+        input.setAttribute('value', field['answer'][i]);
+
+        const label = document.createElement('label');
+        label.textContent = field['answer'][i];
+        label.setAttribute = field['answer'][i];
+
+        form.appendChild(input);
+        form.appendChild(label);
+        }
+
+      } else {
+
+        //Creates text input
+        const label = document.createElement('label');
+        label.textContent = field.label;
+        label.setAttribute('for', field.name);
+
+        const input = document.createElement('input');
+        input.setAttribute('id', field.name);
+
+        form.appendChild(label);
+        form.appendChild(input);
+  }
+
+
+
 }
