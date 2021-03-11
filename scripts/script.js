07a7bca5af55586160eb0eface127b5240212687
Dane Seaberg <>
2021-03-10 21:42:11 -0800

    Add single hard-coded blog.

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,37 @@ function addDogToTotal(adoptionFee) {
   totalPrice += adoptionFee;
   alert('Total cost: ' + totalPrice);
 }
+
+addBlogs();
+
+function addBlogs() {
+  // Create individual elements
+  const blog = document.createElement('article');
+  blog.className = 'blog';
+
+  const blogImage = document.createElement('img');
+  blogImage.src = './images/blog-3.jpg';
+  blogImage.alt = 'another dog picture';
+
+  const blogContents = document.createElement('div');
+  blogContents.className = 'blog-text';
+  const blogTitle = document.createElement('h3');
+  blogTitle.textContent = 'Another blog post title';
+  const blogText1 = document.createElement('p');
+  blogText1.textContent = 'Lorem ipsum one';
+  const blogText2 = document.createElement('p');
+  blogText2.textContent = 'Lorem ipsum two';
+
+  // Link the elements together
+  blogContents.appendChild(blogTitle);
+  blogContents.appendChild(blogText1);
+  blogContents.appendChild(blogText2);
+
+  blog.appendChild(blogImage);
+  blog.appendChild(blogContents);
+
+  const section = document.getElementsByTagName('section')[0];
+  section.appendChild(blog);
+
+  console.log();
+}
