013de115d5454f588e0a3855a1f716ca2fe74e37
Dane Seaberg <>
2021-03-10 21:57:22 -0800

    Put hard-coded dummy blog into an object. Put blog element creation into a helper method.

--- scripts/script.js
+++ scripts/script.js
@@ -11,27 +11,48 @@ function addDogToTotal(adoptionFee) {
   alert('Total cost: ' + totalPrice);
 }
 
+const allBlogData = {
+  blogs: [
+    {
+      imageSource: './images/blog-3.jpg',
+      imageAltText: 'another dog picture',
+      title: 'Another Blog post Title',
+      paragraphs: [
+        'Lorem ipsum one',
+        'Lorem ipsum two'
+      ]
+    }
+  ]
+}
+
 addBlogs();
 
 function addBlogs() {
-  // Create individual elements
+  const blog = createBlog(allBlogData.blogs[0]);
+
+  const section = document.getElementsByTagName('section')[0];
+  section.appendChild(blog);
+}
+
+function createBlog(blogData) {
+  // 1. Create individual elements
   const blog = document.createElement('article');
   blog.className = 'blog';
 
   const blogImage = document.createElement('img');
-  blogImage.src = './images/blog-3.jpg';
-  blogImage.alt = 'another dog picture';
+  blogImage.src = blogData.imageSource;
+  blogImage.alt = blogData.imageAltText;
 
   const blogContents = document.createElement('div');
   blogContents.className = 'blog-text';
   const blogTitle = document.createElement('h3');
-  blogTitle.textContent = 'Another blog post title';
+  blogTitle.textContent = blogData.title;
   const blogText1 = document.createElement('p');
-  blogText1.textContent = 'Lorem ipsum one';
+  blogText1.textContent = blogData.paragraphs[0];
   const blogText2 = document.createElement('p');
-  blogText2.textContent = 'Lorem ipsum two';
+  blogText2.textContent = blogData.paragraphs[1];
 
-  // Link the elements together
+  // 2. Link the elements together
   blogContents.appendChild(blogTitle);
   blogContents.appendChild(blogText1);
   blogContents.appendChild(blogText2);
@@ -39,8 +60,5 @@ function addBlogs() {
   blog.appendChild(blogImage);
   blog.appendChild(blogContents);
 
-  const section = document.getElementsByTagName('section')[0];
-  section.appendChild(blog);
-
-  console.log();
+  return blog;
 }
