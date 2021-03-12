be43c542f58c5390823fb0d973a31fc592e01415
Valerie Mehring <vmehring@gmail.com>
2021-03-11 17:52:32 -0800

    added js button to checkout page

---
+++ scripts/checkoutForm.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('checkout-form');
+//form.setAttribute('id', 'checkout-form');
+//const fields = ['Name', 'Email', 'Street Address', 'City', 'State', 'Zip code', 'First time adopter?', 'Pickup location'];
+
+//for (let i = 0; i < fields.length; i++) {
+  //const label = document.createElement('label');
+  //label.textContent = fields[i];
+  //label.setAttribute('for', fields[i]);
+
+  //const input = document.createElement('input');
+  //input.setAttribute('type', 'text');
+  //input.setAttribute('id', fields[i]);
+
+  //form.appendChild(label);
+  //form.appendChild(input);
+//}
+
+const submitButton = document.createElement('button');
+submitButton.setAttribute('type', 'submit');
+submitButton.textContent = 'submit';
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const formData = document.getElementById('checkout-form').elements;
+  const submitData = {};
+
+  for (let i = 0; i < formData.length - 1; i++) {
+    const val = document.getElementById('checkout-form').elements[i].value;
+    const id = document.getElementById('checkout-form').elements[i].id;
+    console.log('Value: ', val);
+    console.log('Id: ', id);
+    submitData[id] = val;
+  }
+
+  alert('Thank you. Your info has been submitted.')
+  console.log(submitData);
+
+});
+
+form.appendChild(submitButton);
