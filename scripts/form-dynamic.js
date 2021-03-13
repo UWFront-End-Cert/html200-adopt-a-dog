700733abf220c85fb730027a793887c6313f6fcd
“Eling <>
2021-03-13 11:30:46 -0800

    remove test pages

--- scripts/form-dynamic.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('formSubmitButton').addEventListener('click', function (e) {
-  event.preventDefault();
-  alert("Thank you. The form information has been received.");
-
-  const formData = document.getElementById('formContainter');
-  console.log(formData);
-});
