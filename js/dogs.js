e407334e21f5127ead619f7b1e9773fb8a764ff0
Cahillmn <mcahill@seattleu.edu>
2021-03-13 16:46:27 -0800

    finished assignment 9

--- js/dogs.js
+++ js/dogs.js
@@ -7,4 +7,9 @@ let total = 0;
 function Price(price) {
     total = price + total;
     alert('Total Cost to Adopt $' + total);
+
+    $('.adopt-button').click(function(){
+        $('.price').text('$' + total);
+      });
 } 
+
