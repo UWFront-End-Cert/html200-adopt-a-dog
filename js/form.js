5053b6fcb68222e0c940edc0055722edcc3d5c04
AKliz <lizotcweb@gmail.com>
2021-03-14 09:13:29 -0800

    updating with submitData working

--- js/form.js
+++ js/form.js
@@ -6,25 +6,20 @@ formAlertSubmitt.textContent = 'Submit';
 
 
 form.addEventListener('submit', function(e){
-  //On the form page, display an alert that says
-  //"Thank you. The form information has been received" when the form has been submitted.
+  //On the form page, display an alert that says "Thank you. The form information has been received" when the form has been submitted.
   event.preventDefault();
   alert('Thank you. The form information has been received.');
 
-  //Console log the values inside the form,
-  //you can build a string or build an object.
-
+  //store data in an array or object
   const checkoutFormData = form.elements;
   console.log(checkoutFormData);
-  // minus one because we don't want to include submit button
   const submitData = {};
 
   for(i = 0; i < checkoutFormData.length-1; i++){
-    const fieldData = form[i].value;
+    const fieldData = form.element[i].value;
     const fieldName = form[i].id;
-
     console.log(`${fieldName}: ${fieldData}`);
-    submitData[fieldName] = fieldData;
+    submitData[fieldName] = fieldData;   //Console log the values inside the form, you can build a string or build an object.
   }
 console.log(submitData);
 });
