31301cf31e8c264690332523d5059957c10acce0
AKliz <lizotcweb@gmail.com>
2021-03-13 15:01:14 -0900

    fixing js error because forgot to create variable for form

--- js/form.js
+++ js/form.js
@@ -1,12 +1,21 @@
 //On the form page, display an alert that says "Thank you. The form information has been received" when the form has been submitted.
+const form = document.getElementById('dogdataForm')
 const formAlertSubmitt = document.getElementById('submitForm');
+formAlertSubmitt.setAttribute('type','submit');
+formAlertSubmitt.textContent = 'Submit';
 
-formAlertSubmitt.addEventListener('click', {
-  handleEvent: function (event) {
-    event.preventDefault();
-    alert('Thank you. The form information has been received.');
-  }
-});
+
+form.addEventListener('submit', function(e){
+  event.preventDefault();
+  alert('Thank you. The form information has been received.');
+})
+
+//formAlertSubmitt.addEventListener('click', {
+//  handleEvent: function (event) {
+//    event.preventDefault();
+//    alert('Thank you. The form information has been received.');
+//  }
+//});
 
 //formAlertSubmitt.addEventListener('submit', function(){
 //  event.preventDefault(); //prevents it from refreshing the page
@@ -16,3 +25,10 @@ formAlertSubmitt.addEventListener('click', {
 //  const dogData = document.getElementById('dogdataForm').elements;
 //  const submitDogData = {};
 //});
+
+//Console log the values inside the form,
+//you can build a string or build an object.
+
+
+let checkoutFrom = [];
+document.getElementById
