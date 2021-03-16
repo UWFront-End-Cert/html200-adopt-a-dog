96ac1f648cb50ea958afc2efa29f760a016e35a6
JCahill <cahilljm@aol.com>
2021-03-15 18:19:42 -0700

    Push initial attempt to console log form data

--- js/form.js
+++ js/form.js
@@ -6,16 +6,64 @@ el.addEventListener("click", function (event) {
     const result = 'Thank you. The form information has been received.'
     alert(result);
 
-    // console log form input using vanilla javascript
-    const formdata = document.getElementById('orderForm').elements;
+//     // console log form input using vanilla javascript
+//     const formdata = document.getElementById('orderForm').elements;
 
-    // console.log(formdata)
+//     // console.log(formdata)
+
+//     for (let i = 0; i < formdata.length - 1; i++) {
+
+//         const id = document.getElementById('orderForm').elements[i].id;
+//         const value = document.getElementById('orderForm').elements[i].value;
+//         const checked = document.getElementById('orderForm').elements[i].checked;
+//         const submitdata = (id + ": " + value);;
+//         const selection = ("first time adopter?: " + value)
+
+
+//         // console.log(id);
+//         // console.log(value);
+//         // console.log(checked);
+
+//         if (id === 'resultY' && checked === true, id === 'resultN' && checked === true) {
+//             console.log(selection);
+//         } else if (id === 'resultY' && checked === false, id === 'resultN' && checked === false) {
+//           function log(message) {
+//             if (debug) { 
+//               console.log(submitdata);
+//             }
+//           }
+//         } else if (id === 'fieldset') {
+//           function log(message) {
+//             if (debug) { 
+//               console.log(submitdata);
+//             }
+//           }
+//         } else if (id !== 'resultY', id !== 'resultN',id !== 'fieldset') {
+//             console.log(submitdata);
+//         } 
+//     }
+
+
+// });
+
+// console log the form input using jQuery
+$('button').on('click', function (event) {
+    event.preventDefault();
+    let values = $('form').serialize();
+    console.log(values);
+});
+
+
+//    console log form input details using jquery
+    const formdata = $('input,select');
+
+    console.log(formdata)
 
     for (let i = 0; i < formdata.length - 1; i++) {
 
-        const id = document.getElementById('orderForm').elements[i].id;
-        const value = document.getElementById('orderForm').elements[i].value;
-        const checked = document.getElementById('orderForm').elements[i].checked;
+        const id = $('input,select').elements[i].id;
+        const value = $('input,select').elements[i].value;
+        const checked = $('input,select ').elements[i].checked;
         const submitdata = (id + ": " + value);;
         const selection = ("first time adopter?: " + value)
 
@@ -44,11 +92,4 @@ el.addEventListener("click", function (event) {
     }
 
 
-});
-
-// console log the form input using jQuery
-$('button').on('click', function (event) {
-    event.preventDefault();
-    let values = $('form').serialize();
-    console.log(values);
 });
