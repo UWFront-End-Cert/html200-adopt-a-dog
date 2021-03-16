2fb4bf47055ce9922585e29e2b77653b86bc713a
Poisonpez <estessm@uw.edu>
2021-03-16 19:53:04 -0400

    updated code again because I didnt get it...

--- dogs.js
+++ dogs.js
@@ -9,38 +9,3 @@ $(function(){
     $(this).fadeTo('fast', 1)
   })
 })
-
-
-// function carttot(price) {
-  // total = price + '#total-amt';
-  // $('#total-amt').text('$' + price);
-// }
-
-
-
-// function carttot(price) {
-//     totalCostArray.push (price);
-//
-//     let summer = (cost, totalCost) => cost + totalCost;
-//     totalCostArray.push(0);
-//     let total = (totalCostArray.reduce(summer));
-//     // alert('Your total cost is $' + total);
-//     $('#checkout-value').html('$' + total);
-// }
-
-// <h2 id="total-amt" onload="ckttot()">$0.00</h2>
-//
-// <a href="#link" onclick="carttot(price = 135)" class="adopt-link">Adopt</a>
-// // $(function(){
-// //   console.log("DOM is still working")
-// //
-// //   $('carttot(price)').click(function(e) {
-// //
-// //   })
-// //
-// // })
-//
-// function carttot(price) {
-//   total = price + total;
-//   $('#total-amt').text('$' + price)
-// }
