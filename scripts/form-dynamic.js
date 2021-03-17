43dd7552b1b3956ae39bce4d345a2cb26546fa1a
“Eling <>
2021-03-17 16:20:32 -0700

    form console log

--- scripts/form-dynamic.js
+++ scripts/form-dynamic.js
@@ -1,19 +1,10 @@
 document.getElementById('formSubmitButton').addEventListener('click', function (e) {
   event.preventDefault();
   alert("Thank you. The form information has been received.");
-
-// const formData = (i = 0; i < fromData.length; i++)
-// let queryString = $('#checkoutform').serialize();
-  console.log(document.getElementById('name').value);
-
 });
 
-
-
-// const formData = document.getElementById('formContainter');
-// console.log(formData);
-//
-// function submitform()
-// {
-//   document.checkoutform.submit();
-// }
+  document.body
+  .querySelectorAll('form [name]')
+  .forEach(element => {
+  console.log('name:', element.name, 'value:', element.value);
+})
