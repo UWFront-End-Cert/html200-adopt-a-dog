f15893a230ba7cdae8f117a4436126c135747ea7
Suzanne DePoe <suzannedepoe@Suzannes-MacBook-Air.local>
2021-03-17 22:33:33 -0700

    Lesson 9 completed adopt-a-dog.js

--- scripts/adopt-a-dog.js
+++ scripts/adopt-a-dog.js
@@ -1,37 +1,51 @@
 function clickHandler(str) {
     alert(str);
-}
+};
 
-const blogPosts = ['blog1', 'blog2', 'blog3'];
+//A Home effect that gives the tile an outline, or give it a box shadow
+$(function () {
+    console.log('The page has loaded');
+    $('h3').hover(function () {
+        $(this).css('box-shadow', '3px 3px 3px #A52A2A');
+    });
+});
 
-for (i = 0, i < blogPosts.length; i++) {
+//Add a ripple effect to your button.
+$('button').click(function () {
+    $(this).animate({
+        left: '20px',
+        fontSize: '14px',
+    }, function () {
+        //annimation complete
+    });
+});
 
-//Top blog for blog page
+//Zooms in on the dog photo.
+$(function () {
+    $('img').mouseover(function () {
+        $(this).animate({ width: '300px', height: '150px' }, 100);
+    }); $('img').mouseout(function () {
+        $(this).animate({ width: '100%', height: '6rem' }, 100);
+    });
+});
 
-const blogs = document.createElement('article');
-blogs.setAttribute('class', 'blog-item');
 
-document.body.append(blogs);
 
-const blogImage = document.createElement('img');
-blogImage.src = "images/blog-1.jpg";
-blogImage.alt = "A woman sitting looking out over the Grand Canyon with a dog";
 
-const blogTitle = document.createElement('h2');
-blogImage.title.textContent = 'Traveling With Your Dog';
 
-const blogInfo1 = document.createElement('p');
-blogInfo1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
 
-// Center blog for blog page
-const blogImage = document.createElement('img');
-blogImage.src = 'images/blog-2.jpg';
-blogImage.alt = 'Four dogs sitting resting';
 
-const blogTitle = document.createElement('h2');
-blogImage.title.textContent = 'How To Walk Multiple Dogs';
 
-const blogInfo1 = document.createElement('p');
-blogInfo1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
