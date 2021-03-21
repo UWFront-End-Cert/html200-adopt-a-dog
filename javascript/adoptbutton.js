866071cb28d333769521d0f008da95549a775bad
Yuting Lin <72534426+filizlin@users.noreply.github.com>
2021-03-22 04:37:38 +0800

    jquery edits for Assignment 09

--- javascript/adoptbutton.js
+++ javascript/adoptbutton.js
@@ -2,9 +2,13 @@ let Final = 0;
 
 function AddtoCart(value) {
     Final = Final + value;
-    alert("Adding " + value + "to cart.  The total adoption fee is now: " + Final);
+    console.log("Adding " + value + "to cart.  The total adoption fee is now: " + Final);
+    $("#total").text("$ " + Final);
 };
 
 function Dog(Name,Breed,Fee) {
-    alert(Name + "(" + Breed + ") has an adoption fee of " + Fee);
+    console.log(Name + "(" + Breed + ") has an adoption fee of " + Fee);
 };
+
+
+
--- javascript/checkout.js
+++ javascript/checkout.js
@@ -4,31 +4,13 @@ submitButton.setAttribute('type', 'submit');
 submitButton.textContent='Submit';
 form.appendChild(submitButton);
 
-form.addEventListener('submit', function(run) {
-   //Console log the values inside the form, you can build a string or build an object.
+//Use jQuery and refactor the form submission. Simply console log the form data as before.
 
-
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var address = document.getElementById('address').value;
-    var city = document.getElementById('city').value;
-    var state = document.getElementById('zipcode').value;
-    var location = document.getElementById('location').value;
-
-    const UserInfo = {
-        UserName: name,
-        Useremail: email,
-        Useraddress: address,
-        Usercity: city,
-        Userstate: state,
-        PickUpLocation: location
-    };
-
-    console.log(UserInfo);
-
+$( "form" ).submit(function(run) {
+    console.log($(this).serializeArray());
+    run.preventDefault();
 
 //On the form page, display an alert that says "Thank you. The form information has been received" when the form has been submitted.
-
     alert("Thank you. The form information has been received");
 })
 
