3c82d9c9d870ba29d1767f1cdb2682ea9e69cee7
Valerie Mehring <vmehring@gmail.com>
2021-03-22 10:32:42 -0700

    updated checkout js file

--- scripts/jQueryCheckout.js
+++ scripts/jQueryCheckout.js
@@ -2,9 +2,12 @@ $('<input>').appendTo('form')
   .attr({type:'submit', name:'submit', value:'submit'});
 
 $('form').submit(function(event) {
+  console.log( $( this ).serializeArray() );
   event.preventDefault();
+
   alert('Thank you. Your info has been submitted.');
-})
+  
+});
 
 $('<input>').appendTo('form')
   .attr({type:'reset', name:'reset', value:'reset'});
