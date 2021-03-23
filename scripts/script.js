df64290ace8282ffbbaa8df498715d07863a1e9b
deaddiana <deaddian@uw.edu>
2021-03-22 18:12:20 -0700

    completed checkout

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
 
 function dogInfo(name, breed, cost) {
-    alert(name, breed, cost);
+    alert(name + ',' + ' ' + breed + ',' + ' ' + price);
 }
 
 
 let totalCost = 0;
-function total(cost) {
+
+function dogCost(cost) {
     totalCost = cost + totalCost;
-    alert('TOTAL: $' + totalCost);
-}
+    alert('Total:' + ' ' + '$' + totalCost);
+
+    $('#main-total').text(totalCost.toFixed(2));
+};
+
+
+// Hover effect on dog tiles
+       $('.dog-card').hover(function(e){
+           $(this).toggleClass('border');
+       });
