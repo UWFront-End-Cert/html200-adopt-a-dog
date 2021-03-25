cfe81fd638054dab2be272adaa887ea9015f63df
cluy2020 <>
2021-03-24 22:58:23 -0500

    added box shadow in blue

--- adopteddog.js
+++ adopteddog.js
@@ -24,3 +24,17 @@ $('.dogimages').hover(function(){
     $(this).fadeTo('fast', 1)
     $(this).text('Adopt')
   })
+
+
+  $(function(){
+    console.log('DOM is ready, javascript loading!')
+    $('.ulnav').hide().delay(100).show('slow')
+  })
+
+  $('.dogs').mouseenter(function(){
+    console.log('mouse has entered')
+    $(this).css('box-shadow', 'inset 2px 2px 2px 2px blue');
+  }).mouseleave(function(){
+    console.log('mouse has left')
+    $(this).css('box-shadow', 'none');
+  })
