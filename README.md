16c9ca6d9828a03d5518fb3470a31b77ca561d38
djr206 <myEmail@wherever.com>
2021-03-31 15:15:23 -0700

    L10 Submit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # html200-adopt-a-dog
-Initial fork of L01 for html5200.
+
+Description:
+
+This site is designed to facilitate the adoption of dogs in need of a forever home. We feature pictures and details of dogs available for adoption. 
+
+User Persona: 
+
+According to https://www.packagedfacts.com/Pet-Population-Ownership-10858348/ the primary demographics most likely to adopt:
+* 18-34 years old
+* +70 
+* 2nd & 3rd genteration Lations
+
+From this information we should conclude that the younger cohort will be using a variety of hand held devices. 
+The aging boomers are probably using desktop and laptop devices with a good potential for out-of-date browsers.
+The Latino-Americans could possibly require translated pages.
+
+It is also evident that at the very least we will want to include a link or links to good information about what is required
+as a pet owner. The following link is a good example  https://news.orvis.com/dogs/pet-adoption-statistics-the-numbers-behind-the-need.
+
+Testing Log:
+
+https://djr206.github.io/html200-adopt-a-dog/testing.txt
+
+
+Licensing:
+
+The site is unlicensed. 
+It is hosted currently on GitHub Pages. 
+https://djr206.github.io/html200-adopt-a-dog/index.html
---
+++ js-adoptadog/jqJosh.js
@@ -0,0 +1,77 @@
+
+const fields = [
+  {
+    name: 'applicant',
+    label: 'Name'
+  },
+  {
+    name: 'applicantEmail',
+    label: 'Email Address'
+  },
+  {
+    name: 'applicantAddress',
+    label: 'Address'
+  },
+  {
+    name: 'applicantCity',
+    label: 'City'
+  },
+  {
+    name: 'applicantState',
+    label: 'State'
+  },
+  {
+    name: 'applicantZip',
+    label: 'Zip Code'
+  },
+];
+
+// const h1 = document.createElement('h1');
+// document.body.appendChild(h1);
+// h1.textContent = "Checkout";
+// h1.classList.add('blogheading');
+const h2 = document.createElement('h2');
+document.body.appendChild(h2);
+h2.textContent = "Checkout";
+h2.classList.add('contact');
+
+const form = document.createElement('form');
+document.body.appendChild(form);
+form.classList.add('form');
+
+for (let i = 0; i < fields.length; i ++) {
+  const field = fields[i];
+
+  const label = document.createElement('label');
+  label.textContent = field.label;
+  label.setAttribute('for', field.name);
+
+  const input = document.createElement('input');
+  input.setAttribute('id', field.name);
+
+  form.appendChild(label);
+  form.appendChild(input);
+}
+
+const submitButton = document.createElement('button');
+submitButton.setAttribute('type', 'submit');
+submitButton.innerHTML = 'Submit';
+
+form.appendChild(submitButton);
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Form submitted');}
+);
+
+$(document).ready(function(){
+  $('form').submit(function(){
+    let formInput = document.getElementsByTagName('input');
+    let formLabel = document.getElementsByTagName('label');
+    for(let j = 1,  l = 0; j < formInput.length, l < formLabel.length-1; j++, l++) {
+    console.log(formLabel[l].innerHTML + ': ' + formInput[j].value);
+    };
+    alert('working now');
+  });
+  
+});
