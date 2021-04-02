6657537623dac5d55ed875de3e92ffad1b3815c6
HCoulson <HCoulson@users.noreply.github.com>
2021-04-01 18:56:31 -0700

    updated comments for clarity

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -31,47 +31,19 @@ function cartSummary(name) {
   alert(result);
 }
 
+//~ASSIGNMENT 8 code~
 
-//only run this code for the blog page
-// if (blogPageTitle == document.title) {
-//   const top = document.createElement('div');
-//   top.setAttribute('class', 'grid-top');
-//   top.setAttribute('id', 'main-content');
-//   document.body.appendChild(top);
-//
-//   mainPageTitle = document.createElement('h2');
-//   mainPageTitle.textContent = 'Adoptadog Blog';
-//   top.appendChild(mainPageTitle);
-//
-//   blogEntry = document.createElement('article');
-//   blogEntry.setAttribute('class', 'blog');
-//   top.appendChild(blogEntry);
-//
-//   blogImg = document.createElement('img');
-//   blogImg.setAttribute('src', 'images/blog-1.jpg');
-//   blogImg.setAttribute('class', 'left-img');
-//   blogImg.setAttribute('alt', 'a girl looks out over a canyon with her arm around a black dog');
-//   blogEntry.appendChild(blogImg);
-//
-//   blurb = document.createElement('div');
-//   blurb.setAttribute('class', 'blurb-text');
-//   blogEntry.appendChild(blurb);
-//
-//   blogTitle = document.createElement('h3');
-//   blogTitle.textContent = 'Traveling With Your Dog';
-//   blurb.appendChild(blogTitle);
-//
-//   blogP1 = document.createElement('p');
-//   blogP1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
-//   blurb.appendChild(blogP1);
-//
-//   blogP2 = document.createElement('p');
-//   blogP2.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
-//   blurb.appendChild(blogP2);
-// }
+// THE BLOG PAGE
+// Pseudo-code plan:
+// create an array of page sections
+// each section will contain the components of a blog entry, i.e. <article>
+// store content and attributes for blog entries in arrays, e.g. imgSrc, alt text, blog title, etc.
+// step through loops that append elements of blog entries to parent element
+// go back and set style attributes so they display correctly
 
 //setting up necessary variables/objects
 const pageSections = ['top','middle','bottom'];
+//To add subsequent blog posts, add data to these arrays:
 let imgSrcArr = ['blog-1.jpg','blog-2.jpg','blog-3.jpg'];
 let imgAltArr = ['a girl looks out over a canyon with her arm around a black dog','four dogs on leads sit in a city square looking eagerly at someone holding the other end of the leash out of the frame','the silhouette of a girl raising a ball to toss for a sitting, smiling dog in front of a deep orange twilight sky'];
 let titleArr = ['Traveling With Your Dog','How To Walk Multiple Dogs','Teach Your Dog to Fetch!'];
@@ -85,14 +57,15 @@ if (blogPageTitle == document.title) {
     //console.log(pageComponents[i]);
     let section = document.createElement('div');
     section.setAttribute('class', `grid-${pageSections[i]}`);
-    //top.setAttribute('id', 'main-content');
-    //console.log(section.classList);
+    section.setAttribute('id',`blog${i+1}`);
+
     if (pageSections[i] == 'top') {
       section.setAttribute('id', 'main-content');
       mainPageTitle = document.createElement('h2');
       mainPageTitle.textContent = 'Adoptadog Blog';
       section.appendChild(mainPageTitle);
     }
+    console.log(section.getAttribute('id'));
 
     let entry = document.createElement('article');
     entry.setAttribute('class', 'blog');
@@ -110,6 +83,7 @@ if (blogPageTitle == document.title) {
     let blogTitle = document.createElement('h3');
     blogTitle.textContent = titleArr[i];
     blurb.appendChild(blogTitle);
+
     for (j = 0; j < textContentArr[i].length; j+=1) {
       //console.log(`i = ${i}`);
       //console.log(`j = ${j}`);
@@ -125,7 +99,73 @@ if (blogPageTitle == document.title) {
   }
 }
 
-// create an array of blog entries (blogList)
-// each blog entry will contain an array of components: title, image, content
-// step through loops that append blog elements for each component and entry
-// set style attributes so they display correctly
+// THE CHECKOUT PAGE
+// Pseudo-code plan:
+// create object for form fields
+// create array of dogs in shopping cart and their info
+// step through a loop collecting input and console logging it
+// display dogs in cart
+// add event listener to alert when form is submitted
+
+const fields = [
+  {
+    name: 'name',
+    label: 'Name'
+  },
+  {
+    name: 'email',
+    label: 'Email Address'
+  },
+  {
+    name: 'street-address',
+    label: 'Street Address'
+  },
+  {
+    name: 'city',
+    label: 'City'
+  },
+  {
+    name: 'state',
+    label: 'State'
+  },
+  {
+    name: 'zipcode',
+    label: 'Zip Code'
+  },
+  {
+    name: 'first-adopt',
+    value: 'yes',
+    label: 'Yes'
+  },
+  {
+    name: 'first-adopt',
+    value: 'no',
+    label: 'No'
+  },
+  {
+    name: 'location',
+    label: 'Pickup Location'
+  },
+];
+
+cartDogsArr = ['Lucas','Duffy'];
+const dogPrice = 123.45;
+totalAdoptionCost = 0;
+
+const checkoutForm = document.createElement('form');
+document.body.appendChild(checkoutForm);
+
+for (let i = 0; i < fields.length; i+=1) {
+  let field = fields[i];
+
+  const label = document.createElement('label');
+  label.textContent = field.label;
+  label.setAttribute('for', field.name);
+
+  const input = document.createElement('input');
+  input.setAttribute('id', field.name);
+
+  checkoutForm.appendChild(label);
+  checkoutForm.appendChild(input);
+  console.log(input);
+}
