34b8add0d2eaf7347f719a22b72b6f2a0acb24a6
Jacqueline-Leung <73628793+Jacqueline-Leung@users.noreply.github.com>
2021-04-18 16:55:15 -0700

    edit styles

---
+++ script/script.js
@@ -0,0 +1,36 @@
+function murphy(){
+    alert('Name: Murphy, Breed: Alien, Adoption:$123.45');
+}
+function poppy(){
+    alert('Name: Poppy, Breed: Alien, Adoption:$123.45');
+}
+function jack(){
+    alert('Name: Jack, Breed: Alien, Adoption:$123.45');
+}
+function duffy(){
+    alert('Name: Duffy, Breed: Alien, Adoption:$123.45');
+}
+function lucas(){
+    alert('Name: Lucas, Breed: Alien, Adoption:$123.45');
+}
+function jake(){
+    alert('Name: Jake, Breed: Alien, Adoption:$123.45');
+}
+function angus(){
+    alert('Name: Angus, Breed: Alien, Adoption:$123.45');
+}
+function violet(){
+    alert('Name: Violet, Breed: Alien, Adoption:$123.45');
+}
+function piper(){
+    alert('Name: Piper, Breed: Alien, Adoption:$123.45');
+}
+function maximus(){
+    alert('Name: Maximus, Breed: Alien, Adoption:$123.45');
+}
+function luna(){
+    alert('Name: Luna, Breed: Alien, Adoption:$123.45');
+}
+function stella(){
+    alert('Name: Stella, Breed: Alien, Adoption:$123.45');
+}
