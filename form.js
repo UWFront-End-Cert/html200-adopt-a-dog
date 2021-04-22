dbd378254540c6a8ab739217a5482c79d12d3509
Dw3ntz <72217128+Dw3ntz@users.noreply.github.com>
2021-04-22 10:31:49 -0700

    recreating blogPage js and form js files, went missing on local machine after submitting assignment?

---
+++ form.js
@@ -0,0 +1,19 @@
+form.addEventListener('submit', function(event) {
+     alert('Thank you, the form information has been received');
+ });
+
+ //add variables for input areas and call on variables in console.log//
+
+ function formInput () {
+   const nameInput = document.getElementById("applicantName");
+   const emailInput = document.getElementById('applicantEmail');
+   const addyInput = document.getElementById('applicantAddress');
+   const cityInput = document.getElementById('applicantCity');
+   const stateInput = document.getElementById('applicantState');
+
+   console.log(nameInput.value);
+   console.log(emailInput.value);
+   console.log(addyInput.value);
+   console.log(cityInput.value);
+   console.log(stateInput.value);
+ }
