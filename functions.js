9687c71c1f0f90910e3439c5281e26d9e62a0354
matthew <leematt5@uw.edu>
2021-04-22 21:18:29 -0700

    fixed js

---
+++ functions.js
@@ -0,0 +1,11 @@
+function info(name, breed, fees) {
+    alert(String(name + " is a " + breed + ". Their cost is " + fees + "."));
+}
+
+let total = 0;
+
+
+function totalCalc(cost) {
+    total = total + Number(cost);
+    alert(total);
+}
