8969ef0b6c6e41120071dedeb3e3b3998fc00707
pkbh <no-reply>
2021-04-23 00:05:33 -0400

    create first javascript file for course project

---
+++ bio.js
@@ -0,0 +1,17 @@
+function dogBio() {
+  let Name = ["Murphy", "Poppy", "Jack", "Duffy", "Lucas", "Jake", "Angus", "Violet"];
+  let Breed = ["Mixed Breed", "Purebred"];
+  let Cost = 123.45;
+  let result = 0;
+
+  result = Cost + Cost;
+
+  console.log('works!');
+
+  alert((Name[0]) + ", " + Breed[1] + ", " + Cost);
+  alert(result);
+}
+
+//1. name, breed, adoption fee
+//2. add click handlers to each button that will add to total
+//3. alert total
