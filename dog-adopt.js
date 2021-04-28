bc8a6d7932b51e4b9d84b4970c77713ad0586942
Dw3ntz <72217128+Dw3ntz@users.noreply.github.com>
2021-04-28 11:20:58 -0700

    adding jquery to files for assingment 9

--- dog-adopt.js
+++ dog-adopt.js
@@ -1,18 +1,37 @@
 function adoptAlert(name, breed, adoptionFee) {
     let result = 'Adopt ' + name + ', a ' + breed + ', adoption fee of ' + adoptionFee;
-    
+
     alert(result);
 }
-
 
 let total = 0;
 let fee = 123.45;
 
 function feeCalc(fee) {
         total += fee;
-        
+
     alert("Added adoption fee of " + fee + " to your cart! Your cart total is $" + total);
     console.log(total);
 }
 
- 
+$(".adopt-button-border").mouseenter(function(e){
+      $(this).css("border:hover", "solid 3px #375218");
+    }).mouseleave(function(){
+      $(this).css("border:hover", "0");
+    });
+
+
+
+$(".adopt-button").first().click(function() {
+  update( $("span").first() );
+});
+
+$(".adopt-button").last().click(function() {
+  $(".adopt-button").first().trigger("click");
+  update( $("span").last() );
+});
+
+function update(j) {
+  let n=parseInt(j.text(), 1000);
+  j.text(n + total);
+}
--- form.js
+++ form.js
@@ -2,18 +2,7 @@ form.addEventListener('submit', function(event) {
      alert('Thank you, the form information has been received');
  });
 
- //add variables for input areas and call on variables in console.log//
-
- function formInput () {
-   const nameInput = document.getElementById("applicantName");
-   const emailInput = document.getElementById('applicantEmail');
-   const addyInput = document.getElementById('applicantAddress');
-   const cityInput = document.getElementById('applicantCity');
-   const stateInput = document.getElementById('applicantState');
-
-   console.log(nameInput.value);
-   console.log(emailInput.value);
-   console.log(addyInput.value);
-   console.log(cityInput.value);
-   console.log(stateInput.value);
- }
+$("form").on("submit", function (event){
+  event.preventDefault();
+  console.log($(this).serialize());
+});
