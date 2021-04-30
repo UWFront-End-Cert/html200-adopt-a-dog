e563b9a3723e51abd57f4c392ef4b5d256a2f487
Dw3ntz <72217128+Dw3ntz@users.noreply.github.com>
2021-04-29 19:02:15 -0700

    final jquery touches to reflect cart total

--- dog-adopt.js
+++ dog-adopt.js
@@ -1,17 +1,19 @@
+let total = 0;
+let fee = 123.45;
+
 function adoptAlert(name, breed, adoptionFee) {
     let result = 'Adopt ' + name + ', a ' + breed + ', adoption fee of ' + adoptionFee;
 
     alert(result);
 }
 
-let total = 0;
-let fee = 123.45;
-
 function feeCalc(fee) {
         total += fee;
 
     alert("Added adoption fee of " + fee + " to your cart! Your cart total is $" + total);
-    console.log(total);
+    //console.log(total);
+
+    $('#cartTotal').html(total);
 }
 
 $(".adopt-button-border").mouseenter(function(e){
@@ -19,19 +21,3 @@ $(".adopt-button-border").mouseenter(function(e){
     }).mouseleave(function(){
       $(this).css("border:hover", "0");
     });
-
-
-
-$(".adopt-button").first().click(function() {
-  update( $("span").first() );
-});
-
-$(".adopt-button").last().click(function() {
-  $(".adopt-button").first().trigger("click");
-  update( $("span").last() );
-});
-
-function update(j) {
-  let n=parseInt(j.text(), 1000);
-  j.text(n + total);
-}
