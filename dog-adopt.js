980df0a4970216ff7e0e6405a88c09976bde4e57
Dw3ntz <72217128+Dw3ntz@users.noreply.github.com>
2021-05-07 11:31:09 -0700

    updating broken code from lesson 9 push

--- dog-adopt.js
+++ dog-adopt.js
@@ -1,18 +1,23 @@
-function adoptAlert(name, breed, adoptionFee) {
-    let result = 'Adopt ' + name + ', a ' + breed + ', adoption fee of ' + adoptionFee;
-    
-    alert(result);
-}
-
-
 let total = 0;
 let fee = 123.45;
 
-function feeCalc(fee) {
-        total += fee;
-        
-    alert("Added adoption fee of " + fee + " to your cart! Your cart total is $" + total);
-    console.log(total);
+function adoptAlert(name, breed, adoptionFee) {
+    let result = 'Adopt ' + name + ', a ' + breed + ', adoption fee of ' + adoptionFee;
+
+    alert(result);
 }
 
- 
+function feeCalc(fee) {
+        total += fee;
+
+    alert("Added adoption fee of " + fee + " to your cart! Your cart total is $" + total);
+    //console.log(total);
+
+    $('#cartTotal').html(total);
+}
+
+$(".adopt-button-border").mouseenter(function(e){
+      $(this).css("border:hover", "solid 3px #375218");
+    }).mouseleave(function(){
+      $(this).css("border:hover", "0");
+    });
