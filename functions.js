40aa46d387b102b681abb839e969ab70212939ca
matthew <leematt5@uw.edu>
2021-05-23 13:57:29 -0700

    modified submit js

--- functions.js
+++ functions.js
@@ -13,20 +13,18 @@ function totalCalc(cost) {
 
 document.getElementById("submit").addEventListener("click", submit);
 
-var formVals;
+var formVals = [];
 
 function submit() {
     alert("Thank you. The form information has been received");
-    console.log(document.getElementById("fname").value);
-
-    formVals.push(document.getElementById("fname").value)
-    formVals.push(document.getElementById("eaddress").value)
-    formVals.push(document.getElementById("address").value)
-    formVals.push(document.getElementById("city").value)
-    formVals.push(document.getElementById("state").value)
-    formVals.push(document.getElementById("zcode").value)
-    formVals.push(document.getElementById("yes").value)
-    formVals.push(document.getElementById("no").value)
-    formVals.push(document.getElementById("loc").value)
+    formVals.push(document.getElementById("fname").value);
+    formVals.push(document.getElementById("eaddress").value);
+    formVals.push(document.getElementById("address").value);
+    formVals.push(document.getElementById("city").value);
+    formVals.push(document.getElementById("state").value);
+    formVals.push(document.getElementById("zcode").value);
+    formVals.push(document.getElementById("yes").value);
+    formVals.push(document.getElementById("no").value);
+    formVals.push(document.getElementById("loc").value);
     console.log(formVals);
 }
