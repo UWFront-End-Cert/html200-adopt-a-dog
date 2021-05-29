e6ea4954412f04d85b2c33bdceeafb1a0bf96915
DBalint88 <dbalint88@gmail.com>
2021-05-29 11:55:58 -0400

    Moved my JS link up to the head, changed dog prices for validation purposes, set inital Total amount to a string

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,22 +12,21 @@ $(document).ready(function(){
 })
 
 const Murphy = ["Murphy", "Hound", "123.45"]
-const Poppy = ["Poppy", "Bichon", "123.45"]
-const Jack = ["Jack", "Terrier", "123.45"]
-const Duffy = ["Duffy", "Bull Terrier", "123.45"]
-const Lucas = ["Lucas", "Shepherd", "123.45"]
-const Jake = ["Jake", "Yellow Lab", "123.45"]
-const Angus = ["Angus", "Bulldog", "123.45"]
-const Violet = ["Violet", "Chocolate Lab", "123.45"]
-const Piper = ["Piper", "Shepherd", "123.45"]
-const Maximus = ["Maximus", "Terrier", "123.45"]
-const Luna = ["Luna", "Lab", "123.45", "123.45"]
-const Stella = ["Stella", "Chihuahua", "123.45"]
+const Poppy = ["Poppy", "Bichon", "123.46"]
+const Jack = ["Jack", "Terrier", "123.47"]
+const Duffy = ["Duffy", "Bull Terrier", "123.48"]
+const Lucas = ["Lucas", "Shepherd", "123.49"]
+const Jake = ["Jake", "Yellow Lab", "123.51"]
+const Angus = ["Angus", "Bulldog", "123.52"]
+const Violet = ["Violet", "Chocolate Lab", "123.53"]
+const Piper = ["Piper", "Shepherd", "123.54"]
+const Maximus = ["Maximus", "Terrier", "123.55"]
+const Luna = ["Luna", "Lab", "123.45", "123.56"]
+const Stella = ["Stella", "Chihuahua", "123.57"]
 
 // For initially setting the cart total:
 
 let Total = 0;
-document.getElementById('total').innerHTML = "$" + Total.toFixed(2);
 
 // For the dog info alerts:
 function dogInfo(dog) {
