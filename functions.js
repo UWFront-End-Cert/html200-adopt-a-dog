40b17a46de4a3e5478fdf821a1ecadfdddf56ceb
matthew <leematt5@uw.edu>
2021-06-01 20:24:54 -0700

    added jquery

--- functions.js
+++ functions.js
@@ -9,3 +9,17 @@ function totalCalc(cost) {
     total = total + Number(cost);
     alert(total);
 }
+
+$(".grid-item").hover(onHoverInEffect, onHoverOutEffect)
+
+$(".dogs-grid-item").hover(onHoverInEffect, onHoverOutEffect)
+
+function onHoverInEffect() {
+	// on hover add the highlight effect
+	$(this).addClass("highlight")
+}
+
+function onHoverOutEffect() {
+  // when they hover away remove the highlight
+	$(this).removeClass("highlight")
+}
