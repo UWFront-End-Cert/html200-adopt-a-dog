5d83592e7dca536c8b077ca113f500743be6e912
GITlamaria <48498661+GITlamaria@users.noreply.github.com>
2021-06-07 12:15:49 -0700

    Delete arrayChallenge3.js
    will add to the latest branch to get the assignment graded

--- Lesson03/arrayChallenge3.js
+++
@@ -1,8 +0,0 @@
-//5 element array
-let colorsArray = ['white', 'blue', 'green', 'red', 'yellow'];
-
-//print array elements, including the index and a comma, to console uwing a for loop
-for (i=0; i<colorsArray.length; i++)
-{
-    console.log(i + ',' + colorsArray[i]);
-}
