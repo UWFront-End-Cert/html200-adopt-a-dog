f482bb601f089b4544cb193b12298055a28e2ed8
unknown <maria.p.k@hotmail.com>
2021-06-07 10:48:55 -0700

    added array code to arrayChallenge2.js

---
+++ Lesson03/arrayChallenge2.js
@@ -0,0 +1,8 @@
+//5+ words string
+const todayForecast = 'The forecast for today is RAIN';
+
+//put todayForecast string into todayForecastArr array
+let todayForecastArr = todayForecast.split(' ');
+
+//print 3rd word of array to console - expect: for
+console.log (todayForecastArr[2]);
