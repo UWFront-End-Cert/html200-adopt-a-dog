5b348fde05ebb2b39fe38333265708fa50f7b714
djackson29 <danika.c.jackson@gmail.com>
2021-06-16 18:58:22 -0700

    added javascript file

---
+++ dogs.js
@@ -0,0 +1,58 @@
+// math equation to add adoption fee to stored total
+function adoptTotal() {
+  alert("$123.45");
+}
+
+
+// display subtotal in top right corner
+document.getElementById("subtotal").innerHTML = "Total: $";
+
+
+// click dog image and display name, breed, and fee
+function murphyInfo() {
+  alert("Murphy\nDog Breed\n$123.45");
+}
+
+function poppyInfo() {
+  alert("Poppy\nDog Breed\n$123.45");
+}
+
+function jackInfo() {
+  alert("Jack\nDog Breed\n$123.45");
+}
+
+function duffyInfo() {
+  alert("Duffy\nDog Breed\n$123.45");
+}
+
+function lucasInfo() {
+  alert("Lucas\nDog Breed\n$123.45");
+}
+
+function jakeInfo() {
+  alert("Jake\nDog Breed\n$123.45");
+}
+
+function angusInfo() {
+  alert("Angus\nDog Breed\n$123.45");
+}
+
+function violetInfo() {
+  alert("Violet\nDog Breed\n$123.45");
+}
+
+function piperInfo() {
+  alert("Piper\nDog Breed\n$123.45");
+}
+
+function maxInfo() {
+  alert("Maximus\nDog Breed\n$123.45");
+}
+
+function lunaInfo() {
+  alert("Luna\nDog Breed\n$123.45");
+}
+
+function stellaInfo() {
+  alert("Stella\nDog Breed\n$123.45");
+}
