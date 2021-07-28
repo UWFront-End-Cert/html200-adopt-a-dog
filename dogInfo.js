c31cd2b57ceabb0a593c18dec7da1782e5a589c0
pkbh <no-reply>
2021-07-28 10:47:21 -0400

    update cart total when adopt buttons are clicked

--- dogInfo.js
+++ dogInfo.js
@@ -1,9 +1,10 @@
+let totalCost = 0;
+
 function dogInfo(name, breed, fee) {
   alert(name + " (" + breed + ")" + "\n\nAdoption Fee: $" + fee);
 }
 
-let totalCost = 0;
-
 function adoptTotal(fee) {
   alert('Your Adoption Total is: $' + (totalCost += fee));
+  $("#cartCost").html("$ " + totalCost);
 }
