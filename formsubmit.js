561d0d5047a5817e1a478ec514f8af426a4332b2
pkbh <no-reply>
2021-07-28 21:38:38 -0400

    add confirmation alert

--- formsubmit.js
+++ formsubmit.js
@@ -1,19 +1,5 @@
-function myFunction() {
-  let name = $("#name").val()
-  let email = $("#email").val()
-  let address = $("#address").val() + ", " + $("#city").val() + ", " + $("#state").val()
-+ " " + $("#zip").val()
+// Console log whether or not this is the first adoption
 
-  let pickup = $("#location").val()
-
-  console.log("Name: " + name);
-  console.log("Email: " + email);
-  console.log("Address: " + address);
-  console.log("Pickup At: " + pickup);
-
-}
-
-$("button").click(myFunction)
 $(document).ready(function(){
     $('input[id="yes"]').click(function(){
         if($(this).prop("checked") == true){
@@ -28,3 +14,26 @@ $(document).ready(function(){
         }
     });
 });
+
+// Console log other inputs
+
+function myFunction() {
+  let name = $("#name").val()
+  let email = $("#email").val()
+  let address = $("#address").val() + ", " + $("#city").val() + ", " + $("#state").val()
++ " " + $("#zip").val()
+
+  let pickup = $("#location").val()
+
+  console.log("Name: " + name);
+  console.log("Email: " + email);
+  console.log("Address: " + address);
+  console.log("Pickup At: " + pickup);
+
+  alert("Thank you. The form information has been received.");
+
+}
+
+// Execute function on submit
+
+$("button").click(myFunction)
