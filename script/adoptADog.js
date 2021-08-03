46793612af653d8b85baca18848faa0ba87e7fc0
Gitting better <StevenM82@users.noreply.github.com>
2021-08-02 23:05:22 -0700

    the blog posts auto generate correctly, now I need to get them inserted above the footer

--- script/adoptADog.js
+++ script/adoptADog.js
@@ -1,130 +1,170 @@
 function infoAboutDogs(name, breed, fee) {
-    alert(`name: ${name} \nbreed: ${breed} \nfee: ${fee}`); 
+  alert(`name: ${name} \nbreed: ${breed} \nfee: ${fee}`);
 }
 
-let currentTotal = 0; 
+let currentTotal = 0;
 
 function runningTotal(fee) {
-    currentTotal = currentTotal + fee;
-    alert(`Your current total is $${currentTotal}`);
-} 
+  currentTotal = currentTotal + fee;
+  alert(`Your current total is $${currentTotal}`);
+}
 
 
 let blogsArray = [
-    {
-      image: 'images/blog-1.jpg',
-      heading: 'Traveling with your dog',
-      p0: 'Do be do be do',
-      p1: 'Cha cha cha'
-    },
-    {
-      image: 'images/blog-2.jpg',
-      heading: 'How to walk multiple dogs',
-      p0: 'Blah dee blah blah',
-      p1: 'Cannot sleep. Clowns will eat me.'
-    },
-    {
-      image: 'images/blog-3.jpg',
-      heading: 'Teach your dog to fetch!',
-      p0: 'Something not too redundant',
-      p1: 'Blah dee blah blah blah'
-    }
-  ];
-  
-  const foot = document.body.getElementsByClassName('bottom-container');
-  console.log(foot);
-  
-  /*------------First blog post----------*/
-  function blogBlog0(blogginOut) {
-    const section0 = document.createElement('section');
-    section0.setAttribute('id', 'blogPost0');
-    document.body.appendChild(section0);
-    section0.classList.add("blog-grid-1");
-  
-    const blog0 = blogsArray[0];
-  
-    const image0 = document.createElement('img');
-    image0.src = blog0.image;
-  
-    const heading0 = document.createElement('h4');
-    heading0.textContent = blog0.heading;
-    heading0.classList.add('h4-blog');
-  
-    const p00 = document.createElement('p');
-    p00.textContent = blog0.p0;
-    p00.classList.add('blog-p');
-  
-    const p10 = document.createElement('p');
-    p10.textContent = blog0.p1;
-    p10.classList.add('blog-p');
-  
-    section0.appendChild(image0);
-    section0.appendChild(heading0);
-    section0.appendChild(p00);
-    section0.appendChild(p10);
-  }
-  
-  /*------------Second blog post----------*/
-  function blogBlog1(blogginOut) {
-    const section1 = document.createElement('section');
-    section1.setAttribute('id', 'blogPost1');
-    document.body.appendChild(section1);
-    section1.classList.add("blog-grid-2");
-  
-    const blog1 = blogsArray[1];
-  
-    const image1 = document.createElement('img');
-    image1.src = blog1.image;
-  
-    const heading1 = document.createElement('h4');
-    heading1.textContent = blog1.heading;
-    heading1.classList.add('h4-blog');
-  
-    const p01 = document.createElement('p');
-    p01.textContent = blog1.p0;
-    p01.classList.add('blog-p');
-  
-    const p11 = document.createElement('p');
-    p11.textContent = blog1.p1;
-    p11.classList.add('blog-p');
-  
-    section1.appendChild(image1);
-    section1.appendChild(heading1);
-    section1.appendChild(p01);
-    section1.appendChild(p11);
-  }
-  
-  /*------------Third blog post----------*/
-  function blogBlog2(blogginOut) {
-    const section2 = document.createElement('section');
-    section2.setAttribute('id', 'blogPost2');
-    document.body.appendChild(section2);
-    section2.classList.add("blog-grid-3");
-  
-    const blog2 = blogsArray[2];
-  
-    const image2 = document.createElement('img');
-    image2.src = blog2.image;
-  
-    const heading2 = document.createElement('h4');
-    heading2.textContent = blog2.heading;
-    heading2.classList.add('h4-blog');
-  
-    const p02 = document.createElement('p');
-    p02.textContent = blog2.p0;
-    p02.classList.add('blog-p');
-  
-    const p12 = document.createElement('p');
-    p12.textContent = blog2.p1;
-    p12.classList.add('blog-p');
-  
-    section2.appendChild(image2);
-    section2.appendChild(heading2);
-    section2.appendChild(p02);
-    section2.appendChild(p12);
-  }
-  
-  /*--------------Checkout--------------*/
-  function thankYou(click) {
-    alert("Thank you. The form information has been received");
-  }
+{
+  image: 'images/blog-1.jpg',
+  heading: 'Traveling with your dog',
+  p0: 'How far can you walk into the woods? 1/2 way',
+  p1: 'One, two, cha cha cha'
+},
+{
+  image: 'images/blog-2.jpg',
+  heading: 'How to walk multiple dogs',
+  p0: 'I am worried about the calendar. Its days are numbered.',
+  p1: 'Cannot sleep. Clowns will eat me.'
+},
+{
+  image: 'images/blog-3.jpg',
+  heading: 'Teach your dog to fetch!',
+  p0: 'Two drums and a cymbal fell off a cliff. Buh dum ching',
+  p1: 'Blah dee blah blah blah'
+}
+];
+
+const footer = document.body.getElementsByTagName('footer');
+
+/*------------First blog post----------*/
+function blogBlog0(blogginOut) {
+const blog0 = blogsArray[0];
+
+const section0 = document.createElement('section');
+const image0 = document.createElement('img');
+const div0 = document.createElement('div');
+const heading0 = document.createElement('h4');
+const p00 = document.createElement('p');
+const p10 = document.createElement('p');
+
+section0.setAttribute('id', 'blogPost0');
+heading0.setAttribute('id', 'heading0');
+p00.setAttribute('id', 'p00');
+p10.setAttribute('id', 'p10');
+
+image0.src = blog0.image;
+
+heading0.textContent = blog0.heading;
+p00.textContent = blog0.p0;
+p10.textContent = blog0.p1;
+
+section0.classList.add("blog-grid-1");
+div0.classList.add('blog');
+heading0.classList.add('h4-blog');
+p00.classList.add('blog-p');
+p10.classList.add('blog-p');
+
+document.body.appendChild(section0);
+section0.appendChild(image0);
+section0.appendChild(div0);
+div0.appendChild(heading0);
+div0.appendChild(p00);
+div0.appendChild(p10);
+}
+
+/*------------Second blog post----------*/
+function blogBlog1(blogginOut) {
+const blog1 = blogsArray[1];
+
+const section1 = document.createElement('section');
+const image1 = document.createElement('img');
+const div1 = document.createElement('div');
+const heading1 = document.createElement('h4');
+const p01 = document.createElement('p');
+const p11 = document.createElement('p');
+
+section1.setAttribute('id', 'blogPost1');
+heading1.setAttribute('id', 'heading1');
+p01.setAttribute('id', 'p01');
+p11.setAttribute('id', 'p11');
+
+image1.src = blog1.image;
+
+heading1.textContent = blog1.heading;
+p01.textContent = blog1.p0;
+p11.textContent = blog1.p1;
+
+section1.classList.add("blog-grid-2");
+div1.classList.add('blog');
+heading1.classList.add('h4-blog');
+p01.classList.add('blog-p');
+p11.classList.add('blog-p');
+
+document.body.appendChild(section1);
+section1.appendChild(image1);
+section1.appendChild(div1);
+div1.appendChild(heading1);
+div1.appendChild(p01);
+div1.appendChild(p11);
+}
+
+/*------------Third blog post----------*/
+function blogBlog2(blogginOut) {
+const blog2 = blogsArray[2];
+
+const section2 = document.createElement('section');
+const image2 = document.createElement('img');
+const div2 = document.createElement('div');
+const heading2 = document.createElement('h4');
+const p02 = document.createElement('p');
+const p12 = document.createElement('p');
+
+section2.setAttribute('id', 'blogPost2');
+heading2.setAttribute('id', 'heading2');
+p02.setAttribute('id', 'p02');
+p12.setAttribute('id', 'p12');
+
+image2.src = blog2.image;
+
+heading2.textContent = blog2.heading;
+p02.textContent = blog2.p0;
+p12.textContent = blog2.p1;
+
+section2.classList.add("blog-grid-3");
+div2.classList.add('blog');
+heading2.classList.add('h4-blog');
+p02.classList.add('blog-p');
+p12.classList.add('blog-p');
+
+document.body.appendChild(section2);
+section2.appendChild(image2);
+section2.appendChild(div2);
+div2.appendChild(heading2);
+div2.appendChild(p02);
+div2.appendChild(p12);
+}
+
+/*--------------Checkout--------------*/
+
+function thankYou(thanks) {
+const adopter = {
+  name: document.getElementById('name').value,
+  email: document.getElementById('email').value,
+  street: document.getElementById('street').value,
+  city: document.getElementById('city').value,
+  state: document.getElementById('state').value,
+  // firstAdopt: document.getElementById('firstAdoptYes').value,
+  zipcode: document.getElementById('zipCode').value,
+  pickupLocation: document.getElementById('pickup').value
+};
+console.log(adopter);
+
+alert(`Thank you. ${adopter.name} \nThe form information has been received`);
+}
+
+
+
+
+
+
+
+
+
+//
