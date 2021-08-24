5fe3e42e9d3d695869ec6d79cbd83b967b4ffdb4
Waruggy <cwang8413@yahoo.com>
2021-08-24 16:06:05 -0700

    Have added an alert and console log for the form

---
+++ checkout.js
@@ -0,0 +1,17 @@
+document.getElementById("submit").addEventListener("click", submit);
+
+var formVals = [];
+
+ function submit() {
+     alert("Thank you. The form information has been received");
+     formVals.push(document.getElementById("name").value);
+     formVals.push(document.getElementById("email").value);
+     formVals.push(document.getElementById("address").value);
+     formVals.push(document.getElementById("city").value);
+     formVals.push(document.getElementById("state").value);
+     formVals.push(document.getElementById("zcode").value);
+     formVals.push(document.getElementById("yes").value);
+     formVals.push(document.getElementById("no").value);
+     formVals.push(document.getElementById("location").value);
+     console.log("Javascript garthered this info -> " + formVals);
+ }
