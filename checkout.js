f324402d4e07ba5fa3a11a5f38d7f56ec90905f1
Waruggy <cwang8413@yahoo.com>
2021-08-31 15:43:29 -0700

    Added jQuery

--- checkout.js
+++ checkout.js
@@ -1,4 +1,4 @@
-document.getElementById("submit").addEventListener("click", submit);
+/*document.getElementById("submit").addEventListener("click", submit);
 
 var formVals = [];
 
@@ -15,3 +15,34 @@ var formVals = [];
      console.log("Javascript garthered this info -> " + formVals);
      alert("Thank you. The form information has been received");
  }
+*/
+
+// set handler for counter button
+$(".btn2").click(onClickIncrementCount)
+
+function handleFormSubmit() {
+    // get text for first and last name inputs then console.log the output
+    // note - have to use .val() to get text content of input fields
+      let name = $('#name').val()
+      let email = $('#email').val()
+      let address = $('#address').val()
+      let city = $('#city').val()
+      let state = $('#state').val()
+      let zip = $('#zcode').val()
+      let firstTimeAdopterYes = $("#yes").prop("checked")
+      let firstTimeAdopterNo = $("#no").prop("checked")
+      let pickUpLoc = $('#location').val()
+
+      console.log('firstName = ' + name)
+      console.log('email address = ' + email)
+      console.log('Street address = ' + address)
+      console.log('city = ' + city)
+      console.log('state = ' + state)
+      console.log('zip = ' + zip)
+      console.log('firstTimeAdopterYes = ' + firstTimeAdopterYes)
+      console.log('firstTimeAdopterNo = ' + firstTimeAdopterNo)
+      console.log('Pickup Location = ' + pickUpLoc)
+  }
+
+  // set handler for form data
+  $('#submit').click(handleFormSubmit)
