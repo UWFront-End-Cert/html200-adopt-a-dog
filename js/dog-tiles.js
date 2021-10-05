84b789b1e8a201054d296b32f84905928b16788c
carolyngreeno <greeno.carolyn@gmail.com>
2021-10-04 17:58:46 -0700

    add bounce effect

--- js/dog-tiles.js
+++ js/dog-tiles.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
+  // apply hover state to dog tiles
   const dogCard = $(".card");
-
   dogCard
     .mouseover(function () {
       $(this).addClass("slow").addClass("hover-state");
@@ -9,9 +9,15 @@ $(document).ready(function () {
       $(this).removeClass("hover-state");
     });
 
+  // card bounces after it's been clicked
+  dogCard.click(function () {
+    $(this).effect("bounce", { times: 1 }, 1000);
+  });
+
+  // apply ripple effect on buttons
   $("button").ripples({
     resolution: 500,
     dropRadius: 10,
-    perturbance: 0.02,
+    perturbance: 0.2,
   });
 });
