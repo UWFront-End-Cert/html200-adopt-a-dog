12eab8f2102cd2bdec96d95ad8f3dcd6e642f370
tkwest2014 <thomaswest@thomass-air.lan>
2021-10-28 17:25:00 -0500

    Assignment 9

--- javascript.js
+++ javascript.js
@@ -9,16 +9,18 @@ function total(fee){
   alert('Your current total is '+currentTotal);
 }
 
-function submitAlert(){
-  var nameValue = document.getElementById('formName').value;
-  var emailValue = document.getElementById('formEmail').value;
-  var addressValue = document.getElementById('formAddress').value;
-  var cityValue = document.getElementById('formCity').value;
-  var stateValue = document.getElementById('formState').value;
-  var zipValue = document.getElementById('formZip').value;
+$(document).ready(function(){
+  $('.submit').click(function(){
+    var nameValue = document.getElementById('formName').value;
+    var emailValue = document.getElementById('formEmail').value;
+    var addressValue = document.getElementById('formAddress').value;
+    var cityValue = document.getElementById('formCity').value;
+    var stateValue = document.getElementById('formState').value;
+    var zipValue = document.getElementById('formZip').value;
 
-  console.log(nameValue + ", " + emailValue + ", " + addressValue + ", " + cityValue + ", " + stateValue + ", " + zipValue);
+    console.log(nameValue + ", " + emailValue + ", " + addressValue + ", " + cityValue + ", " + stateValue + ", " + zipValue);
 
-  alert('Thank you.  The form information has been received.');
+    alert('Thank you.  The form information has been received.');
 
-}
+  })
+})
