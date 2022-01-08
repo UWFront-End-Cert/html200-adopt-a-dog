3a6bb1d2a724e92adf47902d02aaade147ca4337
Gitting better <StevenM82@users.noreply.github.com>
2022-01-07 22:51:25 -0800

    changed some image files

--- script/adoptADog.js
+++ script/adoptADog.js
@@ -159,19 +159,19 @@ $(function(){
 /*--------------Blogs---------------*/
 let blogsArray = [
   {
-    image: 'images/blog-1.jpg',
+    image: 'images/blog-1.jpeg',
     heading: 'Traveling with your dog',
     p0: 'But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.',
     p1: 'Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?'
   },
   {
-    image: 'images/blog-2.jpg',
+    image: 'images/blog-2.jpeg',
     heading: 'How to walk multiple dogs',
     p0: 'But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?',
     p1: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.'
   },
   {
-    image: 'images/blog-3.jpg',
+    image: 'images/blog-3.jpeg',
     heading: 'Teach your dog to fetch!',
     p0: 'These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammeled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided.',
     p1: 'But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.'
