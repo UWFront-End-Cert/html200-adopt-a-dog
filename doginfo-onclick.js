3030c4eb43c64761e5a65f73f46a3da38db50a1e
Kennedy Caldwell <kaldwell95@gmail.com>
2022-01-09 10:41:59 -0800

    linked javascript to website for dog info and onclick

---
+++ doginfo-onclick.js
@@ -0,0 +1,10 @@
+let adoptionCost = 0;
+
+function info(name, breed, adoptionFee) {
+  alert(name + "\nBreed: " + breed + "\nAdoption Fee: $" + adoptionFee);
+}
+
+function cost(fee){
+  adoptionCost += fee;
+  alert(`Total adoption fee is ${adoptionCost}`);
+}
