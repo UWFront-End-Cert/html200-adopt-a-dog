86a54132f584696a3bca9a8f7805f4df4d101b1c
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-01-20 21:18:55 -0800

    Added some JS.

---
+++ scripts/script.js
@@ -0,0 +1,83 @@
+let checkoutTotal = document.getElementById("checkout-total");
+let cost = document.getElementById("cost").textContent;
+let total = 0;
+
+function addToCheckOutTotal(){
+  total = total + Number(cost);
+  checkoutTotal.textContent = "$" + total;
+  alert("Your total is now: $" + total);
+};
+
+
+
+function murphy(){
+  let name = document.getElementsByTagName("h4")[0].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function poppy(){
+  let name = document.getElementsByTagName("h4")[1].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function jack(){
+  let name = document.getElementsByTagName("h4")[2].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function duffy(){
+  let name = document.getElementsByTagName("h4")[3].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function lucas(){
+  let name = document.getElementsByTagName("h4")[4].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function jake(){
+  let name = document.getElementsByTagName("h4")[5].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function angus(){
+  let name = document.getElementsByTagName("h4")[6].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function violet(){
+  let name = document.getElementsByTagName("h4")[7].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function piper(){
+  let name = document.getElementsByTagName("h4")[8].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function maximus(){
+  let name = document.getElementsByTagName("h4")[9].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function luna(){
+  let name = document.getElementsByTagName("h4")[10].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
+
+function stella(){
+  let name = document.getElementsByTagName("h4")[11].textContent;
+  let breed = "Good Dog";
+  alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
+};
