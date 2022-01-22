f5affa9041f08ae4d65081fdc05ad4ebf29d1d09
Arthur Rodrigues <artrodrig3@outlook.com>
2022-01-22 10:03:11 -0800

    Fix, static-files/jquery-activity.js line.

--- static-files/jquery-activity.js
+++ static-files/jquery-activity.js
@@ -33,7 +33,7 @@ function calc_total(operation) {
 $(".add-to-cart").on("click", function(e){
         e.preventDefault();
 
-        $(this).toggleClass("added-to-cart");
+        $(".added-to-cart").css("background-color", "blue");
 
         if($(this).text() == "Adopt"){
             $(this).text("In Cart");
