d610d797e84a6dfaa179bb82735f70bfc7cbeff4
Arthur Rodrigues <artrodrig3@outlook.com>
2022-01-22 09:43:10 -0800

    Refactor code to be jQuery.

--- static-files/form-completion.js
+++ static-files/form-completion.js
@@ -1,36 +1,53 @@
 
 function get_form_info() {
-    let name = document.getElementsByName("adopter-name")[0].value;
-    console.log("Name of adopter: ", name);
-    
-    let email = document.getElementsByName("adopter-email")[0].value;
-    console.log("Email of adopter: ", email);
-    
-    let street_address = document.getElementsByName("adopter-street-address")[0].value;
-    console.log("Street Address of adopter: ", street_address);
-    
-    let city = document.getElementsByName("adopter-city")[0].value;
-    console.log("City of adopter: ", city);
-    
-    let state = document.getElementsByName("adopter-state")[0].value;
-    console.log("State of adopter: ", state);
-    
-    let zip = document.getElementsByName("adopter-zip")[0].value;
-    console.log("ZIP of adopter: ", zip);
-    
-    // If the first value is checked, for "Yes"
-    if (document.getElementsByName("first-time-adopter")[0].checked == true){
-        console.log("First Time adopter? ", "Yes");
-    } else {
-        console.log("First Time adopter? ", "No");
+    // let name = document.getElementsByName("adopter-name")[0].value;
+    // console.log("Name of adopter: ", name);
+
+    let search_list = [
+        ['First Time adopter? ', 'first-time-adopter'],
+    ];
+
+    console.log(
+        'Name of adopter: ',
+        $("input[name='adopter-name']").val()
+    )
+
+    console.log(
+        'Email of adopter: ',
+        $("input[name='adopter-email']").val()
+    )
+    console.log(
+        'Street Address of adopter: ',
+        $("input[name='adopter-street-address']").val()
+    )
+    console.log(
+        'City of adopter: ',
+        $("input[name='adopter-city']").val()
+    )
+    console.log(
+        'State of adopter: ',
+        $("#adopter-state").children("option:selected").val()
+    )
+    console.log(
+        'ZIP of adopter: ',
+        $("input[name='adopter-zip']").val()
+    )
+
+    console.log(
+        'First Time adopter? ',
+        $("input[name='first-time-adopter']:checked").val()
+    )
+
+    console.log(
+        'Picking up at',
+        $("#pickup-location").children("option:selected").val()
+    )
+}
+
+
+$(".add-to-cart").on("click", function(e) {
+        e.preventDefault();
+        alert("Thank you. The form information has been received.");
+        get_form_info();
     }
-    
-    let pickup = document.getElementsByName("pickup-location")[0].value;
-    console.log("Street Address of adopter: ", pickup);
-}
-
-function acknowledge_submission() {
-    alert("Thank you. The form information has been received.");
-
-    get_form_info();
-}
+)
