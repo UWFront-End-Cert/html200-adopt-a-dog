dae152d80b1b5d20f11354c57c0a0294e2e364c8
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-02-19 15:07:13 -0800

    Finished blog assignment for lessong 8.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 let checkoutTotal = document.getElementById("checkout-total");
-let cost = document.getElementById("cost").textContent;
+let cost = document.getElementById("cost");
 let total = 0;
 
 // Function to add total dollars to check out. Figure out how to truncate the decimals to 2 places.
 function addToCheckOutTotal(){
-  total = total + Number(cost);
+  total = total + Number(cost.value);
   checkoutTotal.textContent = "$" + total;
   alert("Your total is now: $" + total);
 };
@@ -82,3 +82,43 @@ function stella(){
   let breed = "Good Dog";
   alert("This dog is named " + name + " and their breed is " + breed + ". Their adoption cost is $" + cost + ".");
 };
+
+
+
+
+
+// Form submit for new blog post
+const articleContainer = document.getElementById("blog-posts-container");
+const postForm = document.getElementById("new-post-form");
+
+postForm.addEventListener("submit", function(event){
+
+  event.preventDefault();
+
+  let blogTitle = document.getElementById("blog-title");
+  let post = document.getElementById("blog-words");
+
+  let articleImg = document.createElement("img");
+  articleImg.setAttribute('src', 'images/blog-2.jpg');
+  articleImg.setAttribute('class', 'blog-img');
+  let artDiv = document.createElement("div");
+  artDiv.setAttribute('class', 'blog-text');
+  let articleTitle = document.createElement("h3");
+  articleTitle.innerHTML = blogTitle.value;
+  console.log(articleTitle);
+  let articleContent = document.createElement("p");
+  articleContent.innerHTML = post.value;
+  console.log(articleContent);
+
+  let newArticle = document.createElement('article');
+  newArticle.appendChild(articleImg);
+  newArticle.appendChild(artDiv);
+  artDiv.appendChild(articleTitle);
+  artDiv.appendChild(articleContent);
+  articleContainer.insertBefore(newArticle, articleContainer.firstChild);
+
+  alert("Thank you. The form information has been received.");
+  blogTitle.value = '';
+  post.value = '';
+
+});
