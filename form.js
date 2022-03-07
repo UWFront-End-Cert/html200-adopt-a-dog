2ff11f835efd800f662958d2742c1eb36b17e7fa
Anna Dzhurko <annadzhurko@Annas-iMac.local>
2022-03-07 14:44:29 -0800

    assignment9

--- form.js
+++ form.js
@@ -1,35 +1,17 @@
-// var form = document.createElement('form');
-// document.body.appendChild(form);
+
+
 //
+// var myName = document.getElementById("name").value;
+// console.log("The value in the name section is " + myName);
 //
-// const submitButton = document.createElement('button');
-// submitButton.setAttribute('type', 'submit');
-// submitButton.textContent = 'Submit';
+// var myEmail = document.getElementById("email").value;
+// console.log("The value in the email address section is " + myEmail);
 //
-// submitButton.classList = "button-two";
+// var myAddress = document.getElementById("address").value;
+// console.log("The value in the address section is " + myAddress);
 //
-//   form.appendChild(submitButton);
+// var myCity = document.getElementById("city").value;
+// console.log("The value in the city section is " + myCity);
 //
-// form.addEventListener('submit', function(event) {
-//   event.preventDefault();
-//   alert ('Thank you. The form information has been received')
-//
-// });
-
-
-// console.log(document.getElementById('name').value);
-
-var myName = document.getElementById("name").value;
-console.log("The value in the name section is " + myName);
-
-var myEmail = document.getElementById("email").value;
-console.log("The value in the email address section is " + myEmail);
-
-var myAddress = document.getElementById("address").value;
-console.log("The value in the address section is " + myAddress);
-
-var myCity = document.getElementById("city").value;
-console.log("The value in the city section is " + myCity);
-
-var myZipcode = document.getElementById("zip").value;
-console.log("The value in the zip code section is " + myZipcode);
+// var myZipcode = document.getElementById("zip").value;
+// console.log("The value in the zip code section is " + myZipcode);
