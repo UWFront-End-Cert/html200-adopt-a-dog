492895c1964040cc1e7e72316d0e911817562f55
allisonann007 <87877879+allisonann007@users.noreply.github.com>
2022-03-17 21:24:43 -0700

    Adding JS files to course project.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function totalCost(x, y) {
+  let numberOne = 123.45;
+  let numberTwo = 1234.56;
+  let result = 0;
+  result = numberOne + numberTwo;
+  alert(result);
+}
+
+
+function showDogInfo(x,y,z) {
+  let dogName = x;
+  let dogBreed = y;
+  let adoptionFee = Number(z);
+  let result = 0;
+  result = dogName + " " + dogBreed + " " + adoptionFee;
+  alert(result);
+}
