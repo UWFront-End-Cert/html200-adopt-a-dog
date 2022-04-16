b1566f8bc2bdd599e07daa9153b48f6984a784f3
allisonann007 <87877879+allisonann007@users.noreply.github.com>
2022-04-15 18:43:32 -0700

    Adding JavaScript accomplishments.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ function totalCost(x, y) {
   alert(result);
 }
 
-
 function showDogInfo(x,y,z) {
   let dogName = x;
   let dogBreed = y;
@@ -14,4 +13,24 @@ function showDogInfo(x,y,z) {
   let result = 0;
   result = dogName + " " + dogBreed + " " + adoptionFee;
   alert(result);
+}
+
+function clickAlert() {
+  alert("Thank you. The form information has been received.")
+}
+
+const section = document.createElement('section');
+document.body.appendChild(section);
+
+let i = 7;
+for(let i = 0; i < 7; i++) {
+  const newDiv = document.createElement('div');
+  section.appendChild(newDiv);
+}
+
+let arr = ['gridContainer', 'grid-item-4', 'grid-item-5', 'grid-item-6', 'grid-item-7',
+'grid-item-8', 'grid-item-9']
+
+for(let i = 0; i < arr.length; i++) {
+  
 }
