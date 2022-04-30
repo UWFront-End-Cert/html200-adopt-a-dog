fe5a2740793be2aa9ed56cb505ace52947a52676
janene-smith <j.smith.peachy.bit@gmail.com>
2022-04-30 02:30:55 -0700

    Lesson 6 redo - media changes

--- newClickHandler.js
+++ newClickHandler.js
@@ -1,30 +1,16 @@
-/* Lesson 4 Assignment 2 */
+/* Lesson 4 Assignment 2 - redo */
+/* Saved in clickHandlers.js this is just until I merge pull request. */
 
-/* Click Handlers that alert the dog's info. when clicking on the dog's photo. */
-/* Images can have same click handler */
-/* Different arguments are passed into them in the html for each dog ex. dogInfo('Murphy', 123.45) */
-/* Then handle data in JS function with parameters for ea. argument */
-//addEventListener
+let balance = 0;
 
-const dogName = ["Murphy", "Poppy", "Jack", "Duffy", "Lucas", "Jake", "Angus", "Violet", "Piper", "Maximus", "Luna", "Stella"];
-
-const breed = ["Bulldog", "Bichon Frise", "Beagle", "Terrier Mix", "Lab Mix", "Labrador Retriever", "Boxer", "Chocolate Lab", "Golden Retriever", "Irish Terrier", "Yellow Lab", "Chihuahua"];
-
-const fee = [123.45, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45, 123.45];
-
-let arr = [["Murphy"], ["Bulldog"], [123.45]];
-
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr[i].length; j++) {
-    for (let k = 0; k < arr[j].length; k++)
-    console.log(arr[i][j][k])
-  }
+function dogBark(dogName, dogBreed, fee) {
+  alert("Dog name: " + dogName + "\nDog breed: " + dogBreed + "\nAdoption fee: $" + fee);
 }
 
+/* Click handlers on each dog's button that will add to the total. Alert total. */
 
-// Click handler (HTML element is img, click handler is onclick="dogInfo(result)")
-
-function dogInfo() {
-  let dogs = ["dogName", "breed", "fee"];
+// Adopt button
+function btnAdder(fee) {
+  balance += parseInt(fee);
+  alert(balance);
 }
-console.log(result);
