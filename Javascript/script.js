2c9d9856f9adad118e42a990f589480e1f5bce4a
Laurenrowe95 <95656430+Laurenrowe95@users.noreply.github.com>
2022-05-05 14:53:56 -0700

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,17 @@
+function dogPhotoClick(dogName, dogBreed, adoptionFees) {
+
+  alert(dogName + ' is a ' + dogBreed + '. Adoption cost is $' + adoptionFees + '.');
+
+}
+
+var total = 0;
+
+function dogButtonClick() {
+
+  total = total + 123.45;
+  alert('Your total is $' + total);
+}
+/*  let input1 = prompt('Enter a number');
+  let input2 = prompt('Enter another number');
+  let result = 0;
+  total += total*/
