25612067f8976a9324d1a09fac1528fac49bfa8f
janene-smith <j.smith.peachy.bit@gmail.com>
2022-05-07 00:43:55 -0700

    Updated header image to original size

--- clickHandlers.js
+++ clickHandlers.js
@@ -13,3 +13,36 @@ function btnAdder(fee) {
   balance += parseInt(fee);
   alert(balance);
 }
+
+/* Lesson 8 Assignment */
+/* Array of blog objects & loop */
+const container = document.createElement('container');
+const image = document.createElement('img');
+image.src = './blog-1.jpg';
+
+container.appendChild(image);
+
+const headline = document.createElement('h2');
+headline = 'Traveling With Your Dog';
+
+container.appendChild(headline);
+
+const blogtext = document.createElement('p');
+blogtext.p = 'Iduciendisite quo magnatem iuntum quid quaest ea';
+container.appendChild(blogtext);
+
+for (let i = 0; i < image.length; i += 1) {
+  const image = img[i];
+}
+
+for (let )
+const container = document.getElementById('flex-container-blog');
+const image = document.createElement('img');
+image.src = './blog1.jpg';
+
+container.appendChild(image);
+
+const title = document.createElement('h2');
+title.textContent = 'Traveling With Your Dog';
+
+container.insertBefore(image, title);
