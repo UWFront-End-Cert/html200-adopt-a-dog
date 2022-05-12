db7f80705df009dde893f66933407d1803638154
janene-smith <j.smith.peachy.bit@gmail.com>
2022-05-12 13:30:42 -0700

    Added lesson 8 assignment dynamic blog javascript content

---
+++ lesson8.js
@@ -0,0 +1,75 @@
+/* Lesson 8 Assignment */
+/* Array of blog objects & loop */
+// Code used in DOM to create first blog post
+
+const container = document.createElement('container');
+container
+
+const title = document.createElement('h1');
+title.textContent = 'Adoptadog Blog';
+title
+document.body.appendChild(title);
+
+const headline = document.createElement('h2');
+headline.textContent = 'Traveling With Your Dog';
+headline
+document.body.appendChild(headline);
+
+const blogInfo = document.createElement('p');
+blogInfo.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+blogInfo
+document.body.appendChild(blogInfo);
+
+const blogText = document.createElement('p');
+blogText.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, conet labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea adqui con nes cus esere dolut hicto tenisolutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
+blogText
+document.body.appendChild(blogText);
+
+const image = document.createElement('img');
+image.src = './images/blog-1.jpg';
+container.appendChild(image);
+document.body.appendChild(image);
+
+// Blog 2
+
+const headline1 = document.createElement('h2');
+headline1.textContent = 'How To Walk Multiple Dogs';
+headline1
+document.body.appendChild(headline1);
+
+const blogInfo1 = document.createElement('p');
+blogInfo1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+blogInfo1
+document.body.appendChild(blogInfo1);
+
+const blogText1 = document.createElement('p');
+blogText1.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, conet labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea adqui con nes cus esere dolut hicto tenisolutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
+blogText1
+document.body.appendChild(blogText1);
+
+const image1 = document.createElement('img');
+image1.src = './images/blog-2.jpg';
+container.appendChild(image1);
+document.body.appendChild(image1);
+
+// Blog 3
+
+const headline2 = document.createElement('h2');
+headline2.textContent = 'Teach Your Dog To Fetch!';
+headline2
+document.body.appendChild(headline2);
+
+const blogInfo2 = document.createElement('p');
+blogInfo2.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+blogInfo2
+document.body.appendChild(blogInfo2);
+
+const blogText2 = document.createElement('p');
+blogText2.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, conet labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea adqui con nes cus esere dolut hicto tenisolutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
+blogText2
+document.body.appendChild(blogText2);
+
+const image2 = document.createElement('img');
+image2.src = './images/blog-3.jpg';
+container.appendChild(image2);
+document.body.appendChild(image2);
