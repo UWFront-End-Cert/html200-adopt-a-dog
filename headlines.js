2ff7c18a68a6ba50575fbec93ec8250cc2b034c5
janene-smith <j.smith.peachy.bit@gmail.com>
2022-05-13 01:41:03 -0700

    cleanup

--- headlines.js
+++
@@ -1,7 +0,0 @@
-let headline = ['Traveling With Your Dog', 'How To Walk Multiple Dogs', 'Teach Your Dog To Fetch!'];
-
-for (let h = 0; h < headline.length; h += 1) {
-  let headline = [h];
-}
-
-console.log(headline);
--- imageLink.js
+++
@@ -1,9 +0,0 @@
-let imageLink = [
-  './images/blog-1.jpg',
-  './images/blog-2.jpg',
-  './images/blog-3.jpg'
-];
-for (let i = 0; i < imageLink.length; i +=1) {
-  let imageLink = [i];
-}
-console.log(imageLink);
