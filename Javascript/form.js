417b3477816672535cfe4fda259fb5ec5e59f4ac
Laurenrowe95 <95656430+Laurenrowe95@users.noreply.github.com>
2022-05-31 12:42:36 -0700

    Add files via upload

---
+++ Javascript/form.js
@@ -0,0 +1,20 @@
+
+let formElement = document.querySelector('form');
+
+
+formElement.addEventListener('submit', function(e) {
+e.preventDefault();
+const inputValue = formElement.elements[0].value;
+const inputValue2 = formElement.elements[1].value;
+const inputValue3 = formElement.elements[2].value;
+
+const dataToSubmit = {
+   firstName: inputValue,
+   lastName: inputValue2,
+   email: inputValue3,
+}
+
+   alert('Thank you. The form information has been received');
+
+   console.log(dataToSubmit);
+})
