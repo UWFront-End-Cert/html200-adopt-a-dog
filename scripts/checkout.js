12a6bb5fbe497488a6451a635f827761e860f9f1
Mark L Taylor <taylormarkld@gmail.com>
2022-06-05 12:53:52 -0700

    Added Form alert and console log inputs

---
+++ scripts/checkout.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('checkout_form');
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Thank you. The form information has been received');
+
+    for (i = 0; i < form.elements.length; i++) {
+        console.log(form.elements[i].name + ': ' + form.elements[i].value);
+    }
+});
