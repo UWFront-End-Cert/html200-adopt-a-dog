bfac0fa7db5e44cc3938a43554b42a1095be10f0
Mark L Taylor <taylormarkld@gmail.com>
2022-06-14 10:58:00 -0700

    Update the checkout total on the page with each adoption

--- scripts/adoption.js
+++ scripts/adoption.js
@@ -1,10 +1,17 @@
-function dogStats(name, breed, fees) {
-    alert('Name: ' + name + '\nBreed: ' + breed + '\nAdoption Fees: $' + fees);
-}
-
 let total = 0.00;
+// Format the to a currency
+let dollarUS = Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+});
+
+function dogStats(name, breed, fees) {
+    alert('Name: ' + name + '\nBreed: ' + breed + '\nAdoption Fees: ' + dollarUS.format(fees));
+}
 
 function addTotal(fee) {
     total += fee;
-    alert('Adoption Fee: ' + fee + ' has been added to cart.' + '\nTotal: $' + total);
+    alert('Adoption Fee: ' + dollarUS.format(fee) + ' has been added to cart.' + '\nTotal: ' + dollarUS.format(total));
+
+    $('#cart-total').text(dollarUS.format(total))
 }
