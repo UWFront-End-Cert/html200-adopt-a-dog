df9d3a69632e5f86d7a8cd758bc3e4e90cf7c3a1
Emmett Keeton <emmaleek9@yahoo.com>
2022-06-16 19:54:34 -0700

    Completed lesson 9 assignment

---
+++ javascripts/blog-scripts.js
@@ -0,0 +1,56 @@
+//inserting title
+document.createElement('h1');
+const title = document.createElement('h1');
+title.textContent = 'Adoptadog Blog';
+
+const blog = document.getElementById('blogs');
+document.body.insertBefore(title, blog);
+
+//inserting blog posts
+const articleItems = [
+  {
+    articleImage: './images/blog-1.jpg',
+    articleTitle: 'Traveling With Your Dog',
+    articleContents: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.'
+  },
+  {
+    articleImage: './images/blog-2.jpg',
+    articleTitle: 'How To Walk Multiple Dogs',
+    articleContents: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem //quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut //alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus //explam sus am aut amet ant fugiatum, utem non reptat.'
+  },
+  {
+    articleImage: './images/blog-3.jpg',
+    articleTitle: 'Teach Your Dog To Fetch!',
+    articleContents: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.'
+  }
+];
+
+document.createElement('article');
+const articles = document.createElement('article');
+document.body.insertBefore(articles, blog);
+
+for (let i = 0; i < articleItems.length; i += 1) {
+  const items = articleItems[i];
+
+  const articleImage = document.createElement('img');
+  articleImage["src"] = items.articleImage;
+
+  const articleTitle = document.createElement('h3');
+  articleTitle.textContent = items.articleTitle;
+
+  const articleContents = document.createElement('content');
+  articleContents.textContent = items.articleContents;
+
+  articles.appendChild(articleTitle);
+  articles.appendChild(articleImage);
+  articles.appendChild(articleContents);
+}
+
+const form = document.getElementById('adoptForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Thank you. The form information has been received.');
+  for (i = 0; i < form.length; i++) {
+    console.log(form.elements[i].value);
+  }
+});
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -10,59 +10,32 @@ function addFee() {
   alert("Your total is $" + lastTotal.toFixed(2));
 }
 
-//inserting title
-document.createElement('h1');
-const title = document.createElement('h1');
-title.textContent = 'Adoptadog Blog';
+//$('.dog-img').mouseenter(function() {
+//  console.log("hover over")
+//  $(this).width(125%)
+//})
 
-const blog = document.getElementById('blogs');
-document.body.insertBefore(title, blog);
+$(function() {
+  $('.adopt').hover(
+    function() {
+    console.log("mouse over")
+    $(this).fadeTo('slow', .75)
+    }, function() {
+      console.log("mouse off")
+      $(this).fadeTo('fast', 1)
+    }
+  );
+  $(".dog-card").hover(
+    function() {
+      console.log("mouse in")
+      $(this).css("box-shadow", "0 0 5px #00a3cc")
+    }, function() {
+      console.log("mouse out")
+      $(this).css("box-shadow", "none")
+    }
+  );
+})
 
-//inserting blog posts
-const articleItems = [
-  {
-    articleImage: './images/blog-1.jpg',
-    articleTitle: 'Traveling With Your Dog',
-    articleContents: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.'
-  },
-  {
-    articleImage: './images/blog-2.jpg',
-    articleTitle: 'How To Walk Multiple Dogs',
-    articleContents: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem //quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut //alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus //explam sus am aut amet ant fugiatum, utem non reptat.'
-  },
-  {
-    articleImage: './images/blog-3.jpg',
-    articleTitle: 'Teach Your Dog To Fetch!',
-    articleContents: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.'
-  }
-];
-
-document.createElement('article');
-const articles = document.createElement('article');
-document.body.insertBefore(articles, blog);
-
-for (let i = 0; i < articleItems.length; i += 1) {
-  const items = articleItems[i];
-
-  const articleImage = document.createElement('img');
-  articleImage["src"] = items.articleImage;
-
-  const articleTitle = document.createElement('h3');
-  articleTitle.textContent = items.articleTitle;
-
-  const articleContents = document.createElement('content');
-  articleContents.textContent = items.articleContents;
-
-  articles.appendChild(articleTitle);
-  articles.appendChild(articleImage);
-  articles.appendChild(articleContents);
-}
-
-const form = document.getElementById('adoptForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Thank you. The form information has been received.');
-  for (i = 0; i < form.length; i++) {
-    console.log(form.elements[i].value);
-  }
-});
+//$('.dog-img').hover(function() {
+//  $(this).animate({width: 125%,}, 'slow');
+//});
