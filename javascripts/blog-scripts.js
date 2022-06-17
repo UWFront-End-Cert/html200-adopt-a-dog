47b443afedeedee6dc62a93cdba050719f8921f0
Emmett Keeton <emmaleek9@yahoo.com>
2022-06-17 16:04:00 -0700

    Lesson 9

--- javascripts/blog-scripts.js
+++ javascripts/blog-scripts.js
@@ -45,12 +45,3 @@ for (let i = 0; i < articleItems.length; i += 1) {
   articles.appendChild(articleImage);
   articles.appendChild(articleContents);
 }
-
-const form = document.getElementById('adoptForm');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  alert('Thank you. The form information has been received.');
-  for (i = 0; i < form.length; i++) {
-    console.log(form.elements[i].value);
-  }
-});
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -7,35 +7,31 @@ function dogInfo(x, y, z) {
 let lastTotal = 0;
 function addFee() {
   lastTotal = lastTotal + 123.45;
-  alert("Your total is $" + lastTotal.toFixed(2));
 }
 
-//$('.dog-img').mouseenter(function() {
-//  console.log("hover over")
-//  $(this).width(125%)
-//})
+$(function() {
+  $('.adopt').on("click", function() {
+    alert("Your total is $" + lastTotal.toFixed(2));
+  });
+});
+
+$(function () {
+  $('#total')
+})
 
 $(function() {
   $('.adopt').hover(
     function() {
-    console.log("mouse over")
     $(this).fadeTo('slow', .75)
     }, function() {
-      console.log("mouse off")
       $(this).fadeTo('fast', 1)
     }
   );
   $(".dog-card").hover(
     function() {
-      console.log("mouse in")
       $(this).css("box-shadow", "0 0 5px #00a3cc")
     }, function() {
-      console.log("mouse out")
       $(this).css("box-shadow", "none")
     }
   );
 })
-
-//$('.dog-img').hover(function() {
-//  $(this).animate({width: 125%,}, 'slow');
-//});
