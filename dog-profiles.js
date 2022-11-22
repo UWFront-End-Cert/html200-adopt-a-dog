2093618c2c2f0e68ff97ffd51af05f0c3b4c331e
Mary Sennello <marysenn@uw.edu>
2022-11-22 14:45:01 -0800

    added js and jq language for mousein/out on dog cards

--- dog-profiles.js
+++ dog-profiles.js
@@ -1,17 +1,6 @@
-function dogInfo(dog, breed) {
-  alert(dog + '\r\n' + breed + '\r\n' + '$40.00');
-}
-
-let fees1 = 40.00;
-let fees = 0.00;
-
-function adoptionFees() {
-  fees = fees + fees1
-  alert (fees)
-}
 $(function(){
   console.log("DOM is READY, javascript a go")
-  $('.homepage').mouseenter(function(){
+  $('.profile-card').mouseenter(function(){
     console.log("adopt me!")
     $(this).addClass('profile-card-hover')
     $('.profile-card-hover > div').removeClass('adopt-button').addClass('adopt-button-hover')
