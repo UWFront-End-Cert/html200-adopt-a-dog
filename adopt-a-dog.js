158fdffb31d5e242ab38f097f74d3ba78e69abfd
Mary Sennello <marysenn@uw.edu>
2022-11-29 13:39:03 -0800

    clicking adopt button adds to total and alerts total w jquery

--- adopt-a-dog.js
+++ adopt-a-dog.js
@@ -1,16 +1,7 @@
-function dogInfo(dog, breed) {
-  alert(dog + '\r\n' + breed + '\r\n' + '$40.00');
-}
-
-let fees1 = 40.00;
-let fees = 0.00;
-
-function adoptionFees() {
-  fees = fees + fees1
-  alert (fees)
-}
 $(function(){
   console.log("DOM is READY, javascript a go")
+
+  //dog card hover changes
   $('.homepage').mouseenter(function(){
     console.log("adopt me!")
     $(this).addClass('profile-card-hover')
@@ -20,4 +11,27 @@ $(function(){
     $(this).removeClass('profile-card-hover')
     $('.adopt-button-hover').addClass('adopt-button').removeClass('adopt-button-hover')
   })
+//alert clicking dog pic
+  function dogInfo(dog, breed) {
+    alert(dog + '\r\n' + breed + '\r\n' + '$40.00');
+  }
+
+//clicking Adopt button adds total to cart and alerts total
+  let adoptButton = $('.adopt-button')
+  for (let i = 0; i < adoptButton.length; i++){
+    let button = adoptButton[i]
+    button.addEventListener('click', adoptionFees)
+  }
+    let fees1 = 40.00;
+    let fees = 0.00;
+    let cartTotal =$('.cart-total')[0]
+    function adoptionFees() {
+        fees = Number(fees) + Number(fees1);
+        console.log(fees);
+        $('.cart-total')[0].textContent = "$" + fees + ".00"
+        alert("Cart Total is $" + fees + ".00")
+    }
+
+
+
 })
--- dog-profiles.js
+++ dog-profiles.js
@@ -10,3 +10,19 @@ $(function(){
     $('.adopt-button-hover').addClass('adopt-button').removeClass('adopt-button-hover')
   })
 })
+
+//clicking Adopt button adds total to cart and alerts total
+  let adoptButton = $('.adopt-button')
+  for (let i = 0; i < adoptButton.length; i++){
+    let button = adoptButton[i]
+    button.addEventListener('click', adoptionFees)
+  }
+    let fees1 = 40.00;
+    let fees = 0.00;
+    let cartTotal =$('.cart-total')[0]
+    function adoptionFees() {
+        fees = Number(fees) + Number(fees1);
+        console.log(fees);
+        $('.cart-total')[0].textContent = "$" + fees + ".00"
+        alert("Cart Total is $" + fees + ".00")
+    }
