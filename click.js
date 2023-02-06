e495e541c848dd734778f13e3f7398c7c904a95d
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2023-02-05 21:51:29 -0800

    jQuery refactoring

--- click.js
+++ click.js
@@ -1,11 +1,30 @@
-function CalculateTotal(x){
-  let previousTotal =0;
-  total = previousTotal + x;
-  previousTotal = total;
-  alert('New total: $'+total);
+//pre-jQuery
+// function CalculateTotal(x){
+//   total = previousTotal + x;
+//   previousTotal = total;
+//   alert('New total: $'+total);
+// }
+
+//jQuery Total
+let total =0;
+function calculateTotal(x){
+  total +=x;
+  $('#total').text('$ ' + Number(total));
 }
 
 function dogInfo(x, y, z){
   let info = (x+": "+y+" breed, adoption fees: "+z);
   alert(info);
 }
+
+$('.dog-tile').hover(function() {
+  $('img', this).toggleClass('dog-tile-opacity');
+  $( this ).toggleClass('dog-tile-border');
+});
+
+// let total = 0;
+
+// function adoptFees(x) {
+//   total += x;
+//   $('#total').text('$ ' + Number(total));
+// }
