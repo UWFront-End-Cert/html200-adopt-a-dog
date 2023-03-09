c2431cb4845cf1f157fe08c402a7d7b2f5004453
Michael Palmer <michael.palmer@docusign.com>
2023-03-09 15:42:29 -0800

    Lesson 8
    lesson 8 stuff

--- code.js
+++ code.js
@@ -55,10 +55,72 @@ card.hover(function () {
   $(this).removeClass('hover');
 });
 
-$(document).ready(function () {
-  $('form').submit(function (e) {
-    e.preventDefault();
-    var formData = $(this).serialize();
-    console.log(formData);
-  });
+
+
+const blogPosts = [
+  {
+    imgSrc: 'images/blog-1.jpg',
+    imgAlt: 'woman sits with a dog',
+    title: "Eating Your Dog's Food",
+    content: "ASADA!! Tacos al pastor made with adobada meat. I think I’ve overdosed on tacos. Fish tacos with cabbage slaw and a side of chips and guac. Josh’s taco shack is the best taco shack. Give me tacos, or give me death. Can you put some peppers and onions on that? Say taco one more time. CARNE ASADA!! Carne asada on corn tortillas. Tacos, again? This will be 5 times this week and it’s only Tuesday. CARNITAS!!",
+  },
+  {
+    imgSrc: 'images/blog-2.jpg',
+    imgAlt: 'woman sits with a dog',
+    title: "Eating Your Dog's Food",
+    content: "ASADA!! Tacos al pastor made with adobada meat. I think I’ve overdosed on tacos. Fish tacos with cabbage slaw and a side of chips and guac. Josh’s taco shack is the best taco shack. Give me tacos, or give me death. Can you put some peppers and onions on that? Say taco one more time. CARNE ASADA!! Carne asada on corn tortillas. Tacos, again? This will be 5 times this week and it’s only Tuesday. CARNITAS!!",
+  },
+  {
+    imgSrc: 'images/blog-3.jpg',
+    imgAlt: 'dog yawning',
+    title: "Eating Your Dog's Food",
+    content: "ASADA!! Tacos al pastor made with adobada meat. I think I’ve overdosed on tacos. Fish tacos with cabbage slaw and a side of chips and guac. Josh’s taco shack is the best taco shack. Give me tacos, or give me death. Can you put some peppers and onions on that? Say taco one more time. CARNE ASADA!! Carne asada on corn tortillas. Tacos, again? This will be 5 times this week and it’s only Tuesday. CARNITAS!!",
+  },
+];
+
+const blogPostsContainer = document.getElementById('blog-posts');
+if (blogPostsContainer !== null) {
+
+const title = document.createElement('h2');
+title.textContent = "Bob Loblog's Dog Blog";
+blogPostsContainer.appendChild(title);
+
+blogPosts.forEach((post) => {
+  const postDiv = document.createElement('div');
+  postDiv.classList.add('row');
+
+  const imgColumn = document.createElement('div');
+  imgColumn.classList.add('column');
+
+  const img = document.createElement('img');
+  img.src = post.imgSrc;
+  img.alt = post.imgAlt;
+
+  imgColumn.appendChild(img);
+  postDiv.appendChild(imgColumn);
+
+  const textColumn = document.createElement('div');
+  textColumn.classList.add('column');
+
+  const title = document.createElement('h3');
+  title.textContent = post.title;
+
+  const content = document.createElement('p');
+  content.textContent = post.content;
+
+  textColumn.appendChild(title);
+  textColumn.appendChild(content);
+  postDiv.appendChild(textColumn);
+
+  blogPostsContainer.appendChild(postDiv);
+});
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+  alert(`Thank you. The form information has been received.`);
 });
