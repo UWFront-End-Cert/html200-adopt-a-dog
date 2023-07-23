78fbcd4756b8930c039715717b6654fbe7e8aa9a
mahsieh <michaelhsieh.alt@gmail.com>
2023-07-23 01:35:47 -0700

    blog page now dynamically generates posts from JS, rather than HTML; checkout now alerts upon submission, and outputs submitted values to console. form completion is now required for submission, and valid email is enforced.

---
+++ javascript/blog.js
@@ -0,0 +1,47 @@
+const blogs = [
+    {
+        headline: 'Traveling With Your Dog',
+        imageUrl: 'images/blog-1.jpg',
+        imageAlt: 'young woman hugging her black dog while overlooking desert canyons and mesas',
+        blogText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.'
+    },
+
+    {
+        headline: 'How To Walk Multiple Dogs',
+        imageUrl: 'images/blog-2.jpg',
+        imageAlt: '4 dogs on leashes with a dogwalker offcamera, sitting on a brick sidewalk in the city',
+        blogText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.'
+    },
+
+    {
+        headline: 'Teach Your Dog To Fetch!',
+        imageUrl: 'images/blog-3.jpg',
+        imageAlt: 'picture at sunset with the silhouettes of a young woman holding a ball in her right arm, while looking down at her dog, who is looking back at her and the ball while sitting down',
+        blogText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. \n'
+    },
+
+];
+
+for(let i=0; i < blogs.length; i++) {
+    const blogEntry = blogs[i];
+    const container = document.createElement('article');
+    container.setAttribute('class', 'post')
+    const image = document.createElement('img')
+    image.setAttribute('src', blogEntry.imageUrl);
+    image.setAttribute('alt', blogEntry.imageAlt);
+    const textHolder = document.createElement('div')
+    textHolder.setAttribute('class', 'postText')
+    const blogTitle = document.createElement('h2');
+    blogTitle.textContent = blogEntry.headline;
+    const blogContent = document.createElement('p');
+    const blogContent2 = document.createElement('p');
+    blogContent.textContent = blogEntry.blogText;
+    blogContent2.textContent = blogContent.textContent;
+    textHolder.appendChild(blogTitle);
+    textHolder.appendChild(blogContent);
+    textHolder.appendChild(blogContent2);
+
+    document.getElementsByTagName('h1')[0].appendChild(container);
+    container.appendChild(image);
+    container.appendChild(textHolder);
+}
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -11,3 +11,43 @@ function updateCartBalance(additionalCost) {
     runningTotal = runningTotal + additionalCost;
     alert('Your current total adoption cost will be: $' + runningTotal);
 }
+
+const submitButton = document.createElement('input');
+submitButton.setAttribute('id', 'final');
+submitButton.setAttribute('type', 'submit');
+submitButton.setAttribute('value', 'Submit');
+
+document.getElementsByTagName('form')[0].appendChild(submitButton);
+
+document.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formValues = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            city: '',
+            state: '',
+            zip: ''
+        },
+        experience: '',
+        pickup: '',
+        dogs: []
+    };
+
+    formValues.name = document.querySelector('input[name=applicantName').value;
+    formValues.email = document.querySelector('input[name=applicantEmail]').value;
+    formValues.address.street = document.querySelector('input[name=applicantStreetNumber').value;
+    formValues.address.city = document.querySelector('input[name=addressCity]').value;
+    formValues.address.state = document.querySelector('select[name=state]').value;
+    formValues.address.zip = document.querySelector('input[name=addressZip]').value;
+    formValues.experience = document.querySelector('input[name=exp]:checked').value;
+    formValues.pickup = document.querySelector('select[name=location]').value;
+    for (let j = 0; j < document.getElementsByClassName('review')[0].childElementCount; j++) {
+        formValues.dogs[j] = document.getElementsByClassName('review')[0].getElementsByTagName('h2')[j].textContent;
+    }
+    console.log('displaying values submitted');
+    console.log(formValues);
+    alert('Thank you. The form information has been received.!');
+});
+
