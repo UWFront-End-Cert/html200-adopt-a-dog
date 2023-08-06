aeb725e7d40fd2ba0ef04f204a37dcaf732321be
mahsieh <michaelhsieh.alt@gmail.com>
2023-08-05 17:49:52 -0700

    refactored checkout page to generate from jQuery, as well as form submission

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,13 +1,43 @@
 $(function(){
     console.log("DOM ready, load remaining javascript")
 
-    const submitButton = document.createElement('input');
-    submitButton.setAttribute('id', 'final');
-    submitButton.setAttribute('type', 'submit');
-    submitButton.setAttribute('value', 'Submit');
+    /* build a form */
+    if($('h1').outerText = 'Checkout')
+    {
+        console.log("it's the checkout page, build, build, build");
 
-    if(document.getElementsByTagName('form').length > 0){
-        document.getElementsByTagName('form')[0].appendChild(submitButton);
+        $form = $('<form method="GET"></form>');
+        $name = $('<label for="name">Name<input type="text" name="applicantName" required></label>')
+        $form.append($name);
+        
+        $email = $('<label for="email">Email Address<input type="email" name="applicantEmail" required></label>')
+        $form.append($email);
+        
+        $streetNumber = $('<label for="streetNumber">Street Address<input id="streetNumber" type="text" name="applicantStreetNumber" required></label>');
+        $city = $('<label for="city">City<input id="city" type="text" name="addressCity" required></label>');
+        $state = $('<label for="state">State<select name="state" id="state" required><option value="">Please select a state.</option><option value="stateA">State A</option><option value="stateB">State B</option><option value="stateC">State C</option></select></label>');
+        $zip = $('<label for="zip">Zip Code<input id="zip" type="number" name="addressZip" required></label>');
+        $form.append($streetNumber);
+        $form.append($city);
+        $form.append($state);
+        $form.append($zip);
+
+        $experience = $('<fieldset class="experience"><legend>First Time Adopter?</legend><label for="expYes">Yes<input id="expYes" type="radio" name="exp" value="yes" required></label><label for="expNo">No<input id="expNo" type="radio" name="exp" value="no"></label>');
+        $form.append($experience);
+
+        $pickup = $('<label>Pickup Location <select name="location" required><option value="">Please select a location.</option><option value="locOne"> Location 1</option><option value="locTwo">Location 2</option><option value="locThree">Location 3</option></select></label>');
+        $form.append($pickup);
+
+        $cartReview = $('<h2>Dogs Adopted</h2><div class="review"><div class="doggoCart"><img src="images/lucas-card.jpg" alt="picture of Lucas on a leash and smiling at the photographer"><h2>Lucas</h2><p class="cost"><strong>Cost to Adopt:</strong> $123.45</p></div><div class="doggoCart"><img src="images/duffy-card.jpg" alt="picture of Lucas on a leash and smiling at the photographer"><h2>Duffy</h2><p class="cost"><strong>Cost to Adopt:</strong> $123.45</p></div></div><h2>Total Adoption Cost</h2><p>$1234.56</p>');
+        $form.append($cartReview);
+
+        $submit = ('<input type="submit" value="Submit" id="final">');
+        $form.append($submit);
+
+        $('footer').before($form);
+    }
+
+    $('form').on('submit', function(event ) {
         document.addEventListener('submit', function(event) {
             event.preventDefault();
             const formValues = {
@@ -24,14 +54,14 @@ $(function(){
                 dogs: []
             };
 
-            formValues.name = document.querySelector('input[name=applicantName').value;
-            formValues.email = document.querySelector('input[name=applicantEmail]').value;
-            formValues.address.street = document.querySelector('input[name=applicantStreetNumber').value;
-            formValues.address.city = document.querySelector('input[name=addressCity]').value;
-            formValues.address.state = document.querySelector('select[name=state]').value;
-            formValues.address.zip = document.querySelector('input[name=addressZip]').value;
-            formValues.experience = document.querySelector('input[name=exp]:checked').value;
-            formValues.pickup = document.querySelector('select[name=location]').value;
+            formValues.name = $('input[name="applicantName"]').val();
+            formValues.email = $('input[name="applicantEmail"]').val();
+            formValues.address.street = $('input[name="applicantStreetNumber"]').val();
+            formValues.address.city = $('input[name="addressCity"]').val();
+            formValues.address.state = $('select[name="state"]').val();
+            formValues.address.zip = $('input[name="addressZip"]').val();
+            formValues.experience = $('input[name="exp"]:checked').val();
+            formValues.pickup = $('select[name="location"]').val();
             for (let j = 0; j < document.getElementsByClassName('review')[0].childElementCount; j++) {
                 formValues.dogs[j] = document.getElementsByClassName('review')[0].getElementsByTagName('h2')[j].textContent;
             }
@@ -39,7 +69,7 @@ $(function(){
             console.log(formValues);
             alert('Thank you. The form information has been received.!');
         })
-    }
+    })
 
 })
 
