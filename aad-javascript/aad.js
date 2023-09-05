9a1db8de7b97d9773ab9ea08169ca82698371e3e
Christopher Pierce <c.pierce2@pm.me>
2023-09-04 21:20:44 -0700

    made changes to html, sourced .js to it

---
+++ aad-javascript/aad.js
@@ -0,0 +1,10 @@
+function dogProfilePic() {
+    let dogName = '';
+    let dogBreed = '';
+    let adoptionFee = '';
+    let result = 0;
+
+    result = ('Dog Name: ' + dogName, 'Dog Breed: ' + dogBreed, 'Adoption Fee: ' + adoptionFee);
+
+    alert(result);
+}
