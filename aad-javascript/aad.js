bf5437fa1dd5496af1800c78bfb7b6858a382a5a
Christopher Pierce <c.pierce2@pm.me>
2023-09-05 17:56:51 -0700

    added some js and linked js file to html

--- aad-javascript/aad.js
+++ aad-javascript/aad.js
@@ -1,10 +1,25 @@
-function dogProfilePic() {
-    let dogName = '';
-    let dogBreed = '';
-    let adoptionFee = '';
-    let result = 0;
+let adoptionTotal = '$' + Number();
 
-    result = ('Dog Name: ' + dogName, 'Dog Breed: ' + dogBreed, 'Adoption Fee: ' + adoptionFee);
+const breed = "Canis lupus familiaris";
+const fee = Number(123.45);
 
-    alert(result);
+const d1Name = "Murphy";
+const d2Name = "Poppy";
+const d3Name = "Jack";
+const d4Name = "Duffy";
+const d5Name = "Lucas";
+const d6Name = "Jake";
+const d7Name = "Angus";
+const d8Name = "Violet";
+const d9Name = "Piper";
+const d10Name = "Maximus";
+const d11Name = "Luna";
+const d12Name = "Stella";
+
+function dogProfile(a, b, c) {
+    return "Dog Selection:    " + a + '\nBreed:    ' + b + '\nAdoption Fee:   $' + c;
+}
+
+function adoptionPrice(c) {
+    return 'Adoption Fee: $' + c;
 }
