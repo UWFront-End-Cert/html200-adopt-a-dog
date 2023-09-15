b569bf88480e91c64c4aa2aa5f53b3c0ae624661
Christopher Pierce <c.pierce2@pm.me>
2023-09-14 21:06:56 -0700

    made changes to branch with clone, and added .js

---
+++ aad-javascript/aad.js
@@ -0,0 +1,23 @@
+const d1Name = "Murphy";
+const d2Name = "Poppy";
+const d3Name = "Jack";
+const d4Name = "Duffy";
+const d5Name = "Lucas";
+const d6Name = "Jake";
+const d7Name = "Angus";
+const d8Name = "Violet";
+const d9Name = "Piper";
+const d10Name = "Maximus";
+const d11Name = "Luna";
+const d12Name = "Stella";
+
+const breed = "Canis lupus familiaris";
+const fee = Number(123.45);
+
+function dogProfile(a, b, c) {
+    return "Dog Selection:    " + a + '\nBreed:    ' + b + '\nAdoption Fee:   $' + c;
+}
+
+function adoptionPrice(c) {
+    return 'Adoption Fee:\n\n$' + c;
+}
