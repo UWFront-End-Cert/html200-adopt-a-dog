6f45da870a2a15d6a76324540ccc078c9910c845
Christopher Pierce <c.pierce2@pm.me>
2023-10-06 19:44:59 -0700

    README didn't take, re-commiting

--- aad-javascript/aad-form.js
+++ aad-javascript/aad-form.js
@@ -6,24 +6,26 @@ function submitAlert() {
 
 function getVal() {
     let data = [];
-    
+
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const address = document.getElementById('address');
     const city = document.getElementById('city');
     const state = document.getElementById('state');
     const zip = document.getElementById('zip');
-    const firstTimeAdopter = document.getElementById('yes', 'no');
+    const firstTimeAdopter = document.get;
+    const notFirstTime = document.getElementById('no');
     const pickUpLocation = document.getElementById('pu-location');
 
-    data.unshift(name.value);
-    data.push(email.value);
-    data.push(address.value);
-    data.push(city.value);
-    data.push(state.value);
-    data.push(zip.value);
-    data.push(firstTimeAdopter.value);
-    data.push(pickUpLocation.value);
+    data.unshift('Name: ' + name.value);
+    data.push(' Email: ' + email.value);
+    data.push(' Address: ' + address.value);
+    data.push(' City: ' + city.value);
+    data.push(' State: ' + state.value);
+    data.push(' Zip: ' + zip.value);
+    data.push('First-time Adopter: ' + firstTimeAdopter.value);
+    data.push('First-time Adopter: ' + notFirstTime.value);
+    data.push('Pickup Location: ' + pickUpLocation.value);
 
     let values = data.toString();
     console.log(values);
