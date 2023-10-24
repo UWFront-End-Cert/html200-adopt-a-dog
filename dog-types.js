7f387322fcf92bf5f326d895e6cc006fb2784908
KAnderson <kylaa2@uw.edu>
2023-10-23 19:01:39 -0700

    created javascript functions and click handlers that alert the names and breeds and adoption fees of the dogs

---
+++ dog-types.js
@@ -0,0 +1,8 @@
+function MurphyDog() {alert("Name: Murphy\nBreed: American FoxHound\nAdoption Fees: $123.45");}
+function PoppyDog() {alert("Name: Poppy\nBreed: Bichon Frise Mix\nAdoption Fees: $123.45");}
+function JackDog() {alert("Name: Jack\nBreed: Beagle \nAdoption Fees: $123.45");}
+function DuffyDog() {alert("Name: Duffy\nBreed: Staffordshire Bull Terrier Mix \nAdoption Fees: $123.45");}
+function LucasDog() {alert("Name: Lucas\nBreed: German Shepherd Mix \nAdoption Fees: $123.45");}
+function JakeDog() {alert("Name: Jake\nBreed: Golden Labrador Retriever  \nAdoption Fees: $123.45");}
+function AngusDog() {alert("Name: Angus\nBreed: Boxer  \nAdoption Fees: $123.45");}
+function VioletDog() {alert("Name: Violet \nBreed: Chocolate Labrador Retriever  \nAdoption Fees: $123.45");}
