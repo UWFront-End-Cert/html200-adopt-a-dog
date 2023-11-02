af9fa5fa9f966da9549bfafd6497d75406b18ff3
KAnderson <kylaa2@uw.edu>
2023-11-02 15:38:53 -0700

    improved html and javascript coding. Elminated individual functions for each dog adoption button. Used input onclick to allow the machine to keep a running total of the adoption fees. Also changed the html for the dog photos. Each dog image has the dogImage() function with each dog's name and breed, parameters are different for each dog

--- dog-types.js
+++ dog-types.js
@@ -1,41 +1,19 @@
-function MurphyDog() {alert("Name: Murphy\nBreed: American FoxHound\nAdoption Fees: $123.45");}
-function PoppyDog() {alert("Name: Poppy\nBreed: Bichon Frise Mix\nAdoption Fees: $123.45");}
-function JackDog() {alert("Name: Jack\nBreed: Beagle \nAdoption Fees: $123.45");}
-function DuffyDog() {alert("Name: Duffy\nBreed: Staffordshire Bull Terrier Mix \nAdoption Fees: $123.45");}
-function LucasDog() {alert("Name: Lucas\nBreed: German Shepherd Mix \nAdoption Fees: $123.45");}
-function JakeDog() {alert("Name: Jake\nBreed: Golden Labrador Retriever  \nAdoption Fees: $123.45");}
-function AngusDog() {alert("Name: Angus\nBreed: Boxer  \nAdoption Fees: $123.45");}
-function VioletDog() {alert("Name: Violet \nBreed: Chocolate Labrador Retriever  \nAdoption Fees: $123.45");}
 
 
-
-function AdoptionFee(){
-    let FeeAmount = 123.45;
-    let result = 0;
-    result = FeeAmount 
-    alert(result);
-    
-   
-   
-   var total = 0;
-    function AddFee() {
-     total += 123.45;
-     console.log(total);
-   }
-   
-   AddFee()
-   
-   alert (total)
-   
-   
+function dogImage(name, breed, fees) {
+  alert('Name: ' + name + ' Breed: ' + breed + ' Adoption Fees: $ ' + fees);
 }
+
+  
    
-
-
-function TotalCost(){
-// count the number of AdoptionFee clicks and for each click which occurs then add 123.45 to a running total 
-
+let total = 0;
+function addDogCost(cost) {
+  total = total + Number(cost);
+  alert(total)
 }
+
+
+
 
 
 
