2025e5bdc462c94fc0b51f5b2b3d8924e5649a86
KAnderson <kylaa2@uw.edu>
2023-11-15 19:07:49 -0800

    created javascript coding inside my dog-blog.js and I was able to figure out how to place an image in my webpage using javascript.Also I created several h2 elements. I created several paragraph elements.

---
+++ dog-blog.js
@@ -0,0 +1,58 @@
+
+let img = document.createElement('img');
+img.src = 'images./blog-1.jpg';
+document.body.appendChild(img);
+  
+
+const title1 = document.createElement('h2');
+title1.textContent = 'Traveling With Your Dog';
+
+document.body.appendChild(title1);
+
+const p1 = document.createElement('p');
+p1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+
+document.body.appendChild(p1);
+
+
+const p2 = document.createElement('p');
+p2.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
+document.body.appendChild(p2);
+
+const title2 = document.createElement('h2');
+title2.textContent = 'How to Walk Multiple Dogs';
+
+document.body.appendChild(title2);
+
+const p3 = document.createElement('p');
+p3.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+
+document.body.appendChild(p3);
+
+const p4 = document.createElement('p');
+p4.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
+document.body.appendChild(p4);
+
+
+
+const title3 = document.createElement('h2');
+title3.textContent = 'Teach Your Dog to Fetch!';
+
+document.body.appendChild(title3);
+
+const p5 = document.createElement('p');
+p5.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+
+document.body.appendChild(p5);
+
+const p6 = document.createElement('p');
+p6.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
+document.body.appendChild(p6);
+
+
+
+
+
+
+
+
