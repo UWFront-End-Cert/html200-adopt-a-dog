b32b44913316064c65749809f15df0e6ffa31ed4
KAnderson <kylaa2@uw.edu>
2023-11-16 17:54:28 -0800

    used javascript to create form fields. worked on figuring out the possible methods to console.log values from my dog-checkout.html webpage using javascript

--- dog-form.js
+++ dog-form.js
@@ -1,4 +1,16 @@
 const form = document.createElement('form');
+
+const fields = ['Your Name Here', 'Your Email Here'];
+for (let i = 0; i < fields.length; i += 1) {
+const label = document.createElement('label');
+label.textContent = fields[i];
+label.setAttribute('for', fields[i]);
+const input = document.createElement('input');
+input.setAttribute('id', fields[i]);
+form.appendChild(label);
+form.appendChild(input);
+}
+
 document.body.appendChild(form);
 
 const submitButton = document.createElement('button');
@@ -9,19 +21,34 @@ form.appendChild(submitButton);
 
 form.addEventListener('submit' , function(event) {
     event.preventDefault();
-    alert('Form submitted' );
+    alert('Form submitted');
 });
 
 
-let infoAdoption = ['Name' ,'Email' ,'AddressHome','City', 'State' ,'ZipcodeHome' , 'FirstTimeNo', 'FirstTimeYes'] ;
-console.log(infoAdoption);
 
-let Name = document.getElementsByName('Name');
-console.log(Name);
+let fullName = document.getElementById('Name').value
+console.log(fullName);
 
-let adopterEmail = document.getElementById('Email');
-console.log(adopterEmail);
+let fullEmail = document.getElementById('Email').value
+console.log(fullEmail);
 
+let streetAddress = document.getElementById('AddressHome').value
+console.log(AddressHome);
+
+let cityHome = document.getElementById('City').value
+console.log(cityHome);
+
+let stateName = document.querySelector('#State').value;
+console.log(stateName);
+
+let zipCode = document.getElementById('ZipcodeHome').value
+console.log(zipCode);
+
+let adopterStatus = document.querySelector('input[ name="special"]:checked').value;
+console.log(adopterStatus);
+
+let pickupLocation = document.querySelector('#Pickup').value;
+console.log(pickupLocation);
 
 
 
