6676eadd36cb242c9aa58b3aad6724d8562a5e75
KAnderson <kylaa2@uw.edu>
2023-11-22 17:49:41 -0800

    made changes to my javascript so that I have an array and also I have for each loops. Also made some changes to my html inside my dog-blog.html including the class of some items so instead certain html elments have been assigned id names to allow me to be more specific with my css styling. I did make changes to my dog-blog.css external css stylesheet to make sure my id and class names are changed in the css styling too.

--- dog-blog.js
+++ dog-blog.js
@@ -1,58 +1,62 @@
+const blogPosts = [
+    {
+        imgSrc: "images/blog-1.jpg",
+        imgAlt: "person hugging a dog",
+        heading: "Traveling with Your Dog",
+        paragraph:"Lorem Ipsum" 
+    },
+    {
+        imgSrc: "images/blog-2.jpg",
+        imgAlt: "a person holding multiple dogs",
+        heading: "How to Walk Multiple Dogs",
+        paragraph:"Lorem Ipsum"
+    },
+    {
+        imgSrc: "images/blog-3.jpg",
+        imgAlt: "a person throwing a ball",
+        heading: "Teach Your Dog To Fetch",
+        paragraph:"Lorem Ipsum"
+    }
+]
+const blogs = document.getElementById("blogs-container");
 
-const img = document.createElement('img');
-img.src = 'images/blog-1.jpg';
-document.body.appendChild(img);
-  
-const title1 = document.createElement('h2');
-title1.textContent = 'Traveling With Your Dog';
+blogPosts.forEach((blog) => {
 
-document.body.appendChild(title1);
+const blogArticle = document.createElement("article");
+    blogArticle.className = "filler-text"
+    blogs.appendChild(blogArticle);
 
-const p1 = document.createElement('p');
-p1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
+    const blogImg = document.createElement("img");
+    blogImg.setAttribute("src", blog.imgSrc);
+    blogImg.setAttribute("alt", blog.imgAlt);
+    blogArticle.appendChild(blogImg);
 
-document.body.appendChild(p1);
+    const blogDiv = document.createElement("div");
+    blogArticle.appendChild(blogDiv);
+
+    const blogHeading = document.createElement("h2");
+    blogHeading.className = "med-blue-header"
+    blogHeading.textContent = blog.heading;
+    blogDiv.appendChild(blogHeading);
+
+    const blogText = document.createElement("p");
+    blogText.textContent = blog.paragraph;
+    blogDiv.appendChild(blogText);
+});
 
 
-const p2 = document.createElement('p');
-p2.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
-document.body.appendChild(p2);
 
-const img2 = document.createElement('img');
-img2.src = 'images/blog-2.jpg';
-document.body.appendChild(img2);
 
-const title2 = document.createElement('h2');
-title2.textContent = 'How to Walk Multiple Dogs';
 
-document.body.appendChild(title2);
 
-const p3 = document.createElement('p');
-p3.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
 
-document.body.appendChild(p3);
 
-const p4 = document.createElement('p');
-p4.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
-document.body.appendChild(p4);
 
-const img3 = document.createElement('img');
-img3.src = 'images/blog-3.jpg';
-document.body.appendChild(img3);
 
-const title3 = document.createElement('h2');
-title3.textContent = 'Teach Your Dog to Fetch!';
 
-document.body.appendChild(title3);
 
-const p5 = document.createElement('p');
-p5.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.';
 
-document.body.appendChild(p5);
 
-const p6 = document.createElement('p');
-p6.textContent = 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.';
-document.body.appendChild(p6);
 
 
 
