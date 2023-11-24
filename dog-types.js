91360f91298138f75e1702c0e32679b326a44361
KAnderson <kylaa2@uw.edu>
2023-11-24 15:13:16 -0800

    I properly included the jQuery <script> inside my index.html and I properly included the jQuery <script> inside my dog-checkout.html. Also I used jQuery to create a hover effect on the dog tiles on my index.html webpage. Then I used jQuery to create a click event to hide the green adopt buttons on my index.html webpage.

--- dog-types.js
+++ dog-types.js
@@ -17,3 +17,23 @@ function addDogCost(cost) {
 
 
 
+$(function() {
+  console.log("Document ready; javascript running")
+  $('#show').click(function(){
+    $('.dog-box').show(500)
+  })
+  $('.dog-box').mouseenter(function(){
+    $(this).fadeTo("slow", .7)
+  }).mouseleave(function(){
+    $(this).fadeTo("fast", 1)
+  })
+});
+
+$(function() {
+  $('.green-button-adopt').click(function(){
+       $('.green-button-adopt').hide()
+       console.log("an adopt button was clicked!")
+     })
+  })
+  
+
